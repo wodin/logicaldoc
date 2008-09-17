679d3dfded12c7c41aabe22884feea0b9543b8a9
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2008-09-17 10:00:44 +0000

    fixed checkout scripts

--- community/checkout.bat
+++ community/checkout.bat
@@ -1,10 +1,10 @@
-svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/documentation/ documentation
-svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/logicaldoc/trunk logicaldoc
-svn checkout --depth=files https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/modules/ modules
+svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/documentation/ documentation
+svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/logicaldoc/trunk logicaldoc
+svn checkout --depth=files https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/modules/ modules
 cd modules 
 .\checkout.bat
 cd..
-svn checkout --depth=files https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/build/ build
+svn checkout --depth=files https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/build/ build
 cd build
 .\checkout.bat
 cd..
--- community/checkout.sh
+++ community/checkout.sh
@@ -1,10 +1,10 @@
-svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/documentation/ documentation
-svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/logicaldoc/trunk logicaldoc
-svn checkout --depth=files https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/modules/ modules
+svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/documentation/ documentation
+svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/logicaldoc/trunk logicaldoc
+svn checkout --depth=files https://logicaldoc.svn.sourceforge.net/svnroot/community/logicaldoc/modules/ modules
 cd modules 
 ./checkout.sh
 cd..
-svn checkout --depth=files https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/build/ build
+svn checkout --depth=files https://logicaldoc.svn.sourceforge.net/svnroot/community/logicaldoc/build/ build
 cd build
 ./checkout.sh
 cd..
