9774ec25a0ef0ae3d911d4127af86977e2cf77be
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2008-09-17 10:50:46 +0000

    fixed build checkout scripts

--- build/checkout.bat
+++ build/checkout.bat
@@ -1,1 +1,3 @@
 svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/build/poms/trunk poms
+svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/build/ant/trunk ant
+
--- build/checkout.sh
+++ build/checkout.sh
@@ -1,1 +1,2 @@
 svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/build/poms/trunk poms
+svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/build/ant/trunk ant
