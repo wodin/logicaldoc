8fee0aecebaad1e2a34f100510e4d8049a3151c7
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2008-09-17 11:15:51 +0000

    Fixed docs checkout scripts

--- docs/checkout.bat
+++ docs/checkout.bat
@@ -1,3 +1,2 @@
 svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/docs/models/trunk models
-svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/docs/conventions/trunk conventions
 svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/docs/devel/trunk devel
