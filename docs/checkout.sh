a6c4bd29d4199f5107dfabb233613bb144b75cfe
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2008-09-17 11:06:11 +0000

    docs checkout scripts import

---
+++ docs/checkout.sh
@@ -0,0 +1,3 @@
+svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/docs/models/trunk models
+svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/docs/conventions/trunk conventions
+svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/docs/devel/trunk devel
