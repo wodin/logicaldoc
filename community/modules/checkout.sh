a26e3eec9727d2615cfe135bec47b14859eb00cc
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2008-09-17 09:31:06 +0000

    modules import

---
+++ community/modules/checkout.sh
@@ -0,0 +1,2 @@
+svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/modules/logicaldoc-lang-nl/trunk logicaldoc-lang-nl
+svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/modules/logicaldoc-lang-pt/trunk logicaldoc-lang-pt
