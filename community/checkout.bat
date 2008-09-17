bbf89aa826e0f84e2745baa9d0d96acde3b3be65
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2008-09-17 11:28:57 +0000

    Fixed community checkout scripts

--- community/checkout.bat
+++ community/checkout.bat
@@ -1,10 +1,5 @@
-svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/documentation/ documentation
 svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/logicaldoc/trunk logicaldoc
 svn checkout --depth=files https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/modules/ modules
 cd modules 
-.\checkout.bat
-cd..
-svn checkout --depth=files https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/build/ build
-cd build
 .\checkout.bat
 cd..
--- community/checkout.sh
+++ community/checkout.sh
@@ -1,11 +1,5 @@
-svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/documentation/ documentation
 svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/logicaldoc/trunk logicaldoc
 svn checkout --depth=files https://logicaldoc.svn.sourceforge.net/svnroot/community/logicaldoc/modules/ modules
 cd modules 
 ./checkout.sh
 cd..
-svn checkout --depth=files https://logicaldoc.svn.sourceforge.net/svnroot/community/logicaldoc/build/ build
-cd build
-./checkout.sh
-cd..
-
