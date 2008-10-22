74982999296e062a0dedab2332dc15e4e7dff5df
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2008-10-22 13:01:01 +0000

    Added logicaldoc-webdav module

---
+++ community/logicaldoc/trunk/logicaldoc-webdav/src/main/java/com/logicaldoc/webdav/WebDAVPlugin.java
@@ -0,0 +1,22 @@
+package com.logicaldoc.webdav;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.logicaldoc.util.plugin.LogicalDOCPlugin;
+
+/**
+ * This class provides initialisations needed by this plug-in
+ * 
+ * @author Marco Meschieri - Logical Objects
+ * @since 4.0
+ */
+public class WebDAVPlugin extends LogicalDOCPlugin {
+	protected static Log log = LogFactory.getLog(WebDAVPlugin.class);
+
+	@Override
+	protected void doStart() throws Exception {
+		// TODO Insert here needed initializations(servlet registration and so
+		// on)
+	}
+}
