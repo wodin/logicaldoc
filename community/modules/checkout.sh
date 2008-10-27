b8dea0d60c91cf46df047ffdbe8bcc1e12b1a532
mcaruso <mcaruso@bae09422-6297-422f-b3ee-419521344c47>
2008-10-27 08:48:47 +0000

    Added logicaldoc-email module in checkout scripts

--- community/modules/checkout.sh
+++ community/modules/checkout.sh
@@ -1,2 +1,3 @@
 svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/modules/logicaldoc-lang-nl/trunk logicaldoc-lang-nl
 svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/modules/logicaldoc-lang-pt/trunk logicaldoc-lang-pt
+svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/modules/logicaldoc-email/trunk logicaldoc-email
