15fd986e8676317f0f43bb293b7de17fb23b365c
blucecio <blucecio@bae09422-6297-422f-b3ee-419521344c47>
2008-11-11 16:38:55 +0000

    Upgraded logicaldoc-core to latest version of lucene 2.4.0 (2)

--- community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/text/analyzer/Stemmer.java
+++ community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/text/analyzer/Stemmer.java
@@ -4,10 +4,10 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Locale;
 
-import net.sf.snowball.SnowballProgram;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.tartarus.snowball.SnowballProgram;
 
 /**
  * @author Michael Scholz
@@ -26,14 +26,14 @@ class Stemmer {
 	public Stemmer(String lang) throws SecurityException, NoSuchMethodException {
         String language = new Locale(lang).getDisplayLanguage(Locale.ENGLISH);
         try {
-			Class stemClass = Class.forName("net.sf.snowball.ext." + language + "Stemmer");
+			Class stemClass = Class.forName("org.tartarus.snowball.ext." + language + "Stemmer");
 			stemmer = (SnowballProgram) stemClass.newInstance();
 			stemMethod = stemmer.getClass().getMethod("stem", new Class[0]);
 		} catch (Exception e) {
 			log.info("Error instantiating stemmer for language:" + language);
 			log.info("Trying with English Stemmer.");
 			// Default with English stemmer
-			stemmer = new net.sf.snowball.ext.EnglishStemmer();
+			stemmer = new org.tartarus.snowball.ext.EnglishStemmer();
 			stemMethod = stemmer.getClass().getMethod("stem", new Class[0]);			
 		} 
     }
