ba94f10f9346ed1c8ce32c25ec6dd277fa05e51f
blucecio <blucecio@bae09422-6297-422f-b3ee-419521344c47>
2008-11-12 17:35:29 +0000

    Added sorting on column of the tag-documents table

--- community/logicaldoc/trunk/logicaldoc-web/src/main/java/com/logicaldoc/web/search/KeywordsBean.java
+++ community/logicaldoc/trunk/logicaldoc-web/src/main/java/com/logicaldoc/web/search/KeywordsBean.java
@@ -2,9 +2,13 @@ package com.logicaldoc.web.search;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.logging.Log;
@@ -13,6 +17,7 @@ import org.apache.commons.logging.LogFactory;
 import com.logicaldoc.core.document.dao.DocumentDAO;
 import com.logicaldoc.util.Context;
 import com.logicaldoc.web.SessionManagement;
+import com.logicaldoc.web.components.SortableList;
 import com.logicaldoc.web.document.DocumentRecord;
 
 /**
@@ -23,20 +28,27 @@ import com.logicaldoc.web.document.DocumentRecord;
  * @version $Id: KeywordsBean.java,v 1.2 2007/08/22 14:12:20 marco Exp $
  * @since ###release###
  */
-public class KeywordsBean {
-	
+public class KeywordsBean extends SortableList {
+
 	protected static Log log = LogFactory.getLog(KeywordsBean.class);
 
 	private Collection<Letter> letters = new ArrayList<Letter>();
 
-	private Collection<Keyword> keywords = new ArrayList<Keyword>();
+	private List<Keyword> keywords = new ArrayList<Keyword>();
 
-	private Collection<DocumentRecord> documents = new ArrayList<DocumentRecord>();
+	private List<DocumentRecord> documents = new ArrayList<DocumentRecord>();
 
 	private String selectedWord;
+	
+	private DocumentHandler dh = new DocumentHandler("xxx");
+	
+
+	// record the last operation requested: letter or keyword
 	private String reqop;
 
 	public KeywordsBean() {
+		// We don't sort by default
+		super("xxx");
 		String str = "abcdefghijklmnopqrstuvwxyz";
 
 		for (int i = 0; i < str.length(); i++) {
@@ -75,7 +87,8 @@ public class KeywordsBean {
 	/**
 	 * Set selectedWord and show documents marked with that word.
 	 * 
-	 * @param keyword The word of where we want to show documents
+	 * @param keyword
+	 *            The word of where we want to show documents
 	 */
 	public void select(String keyword) {
 		selectedWord = keyword;
@@ -192,7 +205,7 @@ public class KeywordsBean {
 					long userId = SessionManagement.getUserId();
 					DocumentDAO docDao = (DocumentDAO) Context.getInstance().getBean(DocumentDAO.class);
 					Set<Long> docIds = docDao.findDocIdByUserIdAndKeyword(userId, word);
-					
+
 					reqop = "keyword";
 					documents.clear();
 					keywords.clear();
@@ -202,7 +215,7 @@ public class KeywordsBean {
 
 						if (!documents.contains(record)) {
 							documents.add(record);
-						} 
+						}
 					}
 				} catch (Exception e) {
 					log.error(e.getMessage(), e);
@@ -218,4 +231,90 @@ public class KeywordsBean {
 	public String getReqop() {
 		return reqop;
 	}
+
+	@Override
+	protected boolean isDefaultAscending(String sortColumn) {
+		return true;
+	}
+
+	/**
+	 * Sorts the list of Keyword data.
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void sort(final String column, final boolean ascending) {
+
+		Comparator comparator = new Comparator() {
+			public int compare(Object o1, Object o2) {
+
+				Keyword c1 = (Keyword) o1;
+				Keyword c2 = (Keyword) o2;
+				if (column == null) {
+					return 0;
+				}
+				if (column.equals("tagName")) {
+					return ascending ? c1.getWord().compareTo(c2.getWord()) : c2.getWord().compareTo(c1.getWord());
+				} else if (column.equals("documentsCount")) {
+					return ascending ? new Integer(c1.getCount()).compareTo(c2.getCount()) : new Integer(c2.getCount())
+							.compareTo(c1.getCount());
+				} else
+					return 0;
+			}
+		};
+
+		Collections.sort(keywords, comparator);
+	}
+
+	public class DocumentHandler extends SortableList {
+
+		protected DocumentHandler(String defaultSortColumn) {
+			// We don't sort by default
+			super("xxx");
+		}
+
+		@Override
+		protected boolean isDefaultAscending(String sortColumn) {
+			return true;
+		}
+
+		@Override
+		protected void sort(final String column, final boolean ascending) {
+			
+			log.debug("invoked DocumentsRecordsManager.sort()");
+			log.debug("sort on column: " + column);
+			log.debug("sort ascending: " + ascending);
+			
+			Comparator comparator = new Comparator() {
+				public int compare(Object o1, Object o2) {
+
+					DocumentRecord c1 = (DocumentRecord) o1;
+					DocumentRecord c2 = (DocumentRecord) o2;
+					if (column == null) {
+						return 0;
+					}
+					if (column.equals("displayDescription")) {
+						return ascending ? c1.getDisplayTitle().compareTo(c2.getDisplayTitle()) : c2.getDisplayTitle()
+								.compareTo(c1.getDisplayTitle());
+					} else if (column.equals("date")) {
+						Date d1 = c1.getSourceDate() != null ? c1.getSourceDate() : new Date(0);
+						Date d2 = c2.getSourceDate() != null ? c2.getSourceDate() : new Date(0);
+						return ascending ? d1.compareTo(d2) : d2.compareTo(d1);
+					} else if (column.equals("size")) {
+						Long s1 = new Long(c1.getDocument().getFileSize());
+						Long s2 = new Long(c2.getDocument().getFileSize());
+						return ascending ? s1.compareTo(s2) : s2.compareTo(s1);
+					} else
+						return 0;
+				}
+			};
+
+			Collections.sort(documents, comparator);
+		}
+
+	}
+
+	public DocumentHandler getDh() {
+		return dh;
+	}
+
 }
