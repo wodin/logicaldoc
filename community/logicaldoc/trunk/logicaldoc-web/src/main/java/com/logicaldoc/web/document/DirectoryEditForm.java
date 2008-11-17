9187e0a081449fae8023426c3670bbb4c3da899b
mcaruso <mcaruso@bae09422-6297-422f-b3ee-419521344c47>
2008-11-17 16:52:16 +0000

    Modified folder rights management on folder creation

--- community/logicaldoc/trunk/logicaldoc-web/src/main/java/com/logicaldoc/web/document/DirectoryEditForm.java
+++ community/logicaldoc/trunk/logicaldoc-web/src/main/java/com/logicaldoc/web/document/DirectoryEditForm.java
@@ -2,10 +2,12 @@ package com.logicaldoc.web.document;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import com.logicaldoc.core.security.Menu;
+import com.logicaldoc.core.security.MenuGroup;
+import com.logicaldoc.core.security.dao.GroupDAO;
 import com.logicaldoc.core.security.dao.MenuDAO;
 import com.logicaldoc.util.Context;
-
 import com.logicaldoc.web.SessionManagement;
 import com.logicaldoc.web.i18n.Messages;
 
@@ -72,7 +74,7 @@ public class DirectoryEditForm {
 	}
 
 	public String insert() {
-		long menuparent = documentNavigation.getSelectedDir().getMenuId();
+		Menu parent = documentNavigation.getSelectedDir().getMenu();
 
 		if (SessionManagement.isValid()) {
 			MenuDAO dao = (MenuDAO) Context.getInstance().getBean(MenuDAO.class);
@@ -80,12 +82,26 @@ public class DirectoryEditForm {
 			try {
 				Menu menu = new Menu();
 				menu.setText(getFolderName());
-				menu.setParentId(menuparent);
+				menu.setParentId(parent.getId());
 				menu.setSort(1);
 				menu.setIcon("folder.png");
-				menu.setPath(documentNavigation.getSelectedDir().getMenu().getPath() + "/" + menuparent);
+				menu.setPath(parent.getPath() + "/" + parent.getId());
 				menu.setType(Menu.MENUTYPE_DIRECTORY);
-				menu.setMenuGroup(menuGroup);
+
+				for (MenuGroup mg : parent.getMenuGroups()) {
+					MenuGroup clone = mg.clone();
+					menu.getMenuGroups().add(clone);
+					if (clone.getGroupId() == SessionManagement.getUser().getUserGroup().getId()) {
+						clone.setManageSecurity(1);
+					}
+				}
+
+				MenuGroup mg = new MenuGroup(SessionManagement.getUser().getUserGroup().getId());
+				mg.setManageSecurity(1);
+
+				if (!menu.getMenuGroups().contains(mg)) {
+					menu.getMenuGroups().add(mg);
+				}
 
 				boolean stored = dao.store(menu);
 
