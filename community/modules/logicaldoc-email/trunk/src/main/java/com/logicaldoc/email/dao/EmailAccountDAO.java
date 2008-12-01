facfe6a8e09b0efdc43d12218b553dd3f846c634
mcaruso <mcaruso@bae09422-6297-422f-b3ee-419521344c47>
2008-12-01 09:39:46 +0000

    Maked Email plugin compliant with the new DAO API.

--- community/modules/logicaldoc-email/trunk/src/main/java/com/logicaldoc/email/dao/EmailAccountDAO.java
+++ community/modules/logicaldoc-email/trunk/src/main/java/com/logicaldoc/email/dao/EmailAccountDAO.java
@@ -1,7 +1,6 @@
 package com.logicaldoc.email.dao;
 
-import java.util.Collection;
-
+import com.logicaldoc.core.PersistentObjectDAO;
 import com.logicaldoc.email.EmailAccount;
 
 /**
@@ -11,32 +10,5 @@ import com.logicaldoc.email.EmailAccount;
  * @author Alessandro Gasparini - Logical Objects
  * @since 4.0
  */
-public interface EmailAccountDAO {
-
-	/**
-	 * This method persists an EmailAccount object.
-	 * 
-	 * @param account EmailAccount which should be store.
-	 * @return True if successfully stored in a database.
-	 */
-	public boolean store(EmailAccount account);
-
-	/**
-	 * This method deletes an EmailAccount.
-	 * 
-	 * @param accountId Id of the EmailAccount which should be delete.
-	 */
-	public boolean delete(long accountId);
-
-	/**
-	 * This method finds an EmailAccount by its accountId.
-	 */
-	public EmailAccount findById(long accountId);
-
-	/**
-	 * Loads all accounts
-	 * 
-	 * @return
-	 */
-	public Collection<EmailAccount> findAll();
+public interface EmailAccountDAO extends PersistentObjectDAO<EmailAccount> {
 }
