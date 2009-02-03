422ea25efc6cb023004616b933c9c3401bfa3b9d
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2009-02-03 08:06:08 +0000

    Fixed index recreation

--- community/logicaldoc/branches/logicaldoc-4.0/logicaldoc-web/src/main/java/com/logicaldoc/web/admin/IndexInfo.java
+++ community/logicaldoc/branches/logicaldoc-4.0/logicaldoc-web/src/main/java/com/logicaldoc/web/admin/IndexInfo.java
@@ -1,6 +1,5 @@
 package com.logicaldoc.web.admin;
 
-import java.io.File;
 import java.util.Collection;
 import java.util.Iterator;
 
@@ -97,14 +96,11 @@ public class IndexInfo {
 								.getInstance().getBean(DocumentDAO.class);
 						Collection<Document> documents = documentDao.findAll();
 						Iterator<Document> iter = documents.iterator();
-						Indexer indexer = (Indexer) Context.getInstance()
-								.getBean(Indexer.class);
-
 						while (iter.hasNext()) {
 							Document document = iter.next();
-							String dir = conf.getValue("docdir") + "/";
-							dir += document.getPath()+"/"+document.getId();
-							indexer.addFile(new File(dir), document);
+							documentDao.initialize(document);
+							document.setIndexed(0);
+							documentDao.store(document);
 						}
 					} catch (Exception e) {
 						log.error(e.getMessage(), e);
