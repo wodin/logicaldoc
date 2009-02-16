ece45f8c25d5d62af0a9e75578b5d0a93950bd79
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2009-02-16 08:25:55 +0000

    Javadoc fixes

--- community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/security/authentication/AuthenticationComponent.java
+++ community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/security/authentication/AuthenticationComponent.java
@@ -1,12 +1,14 @@
 package com.logicaldoc.core.security.authentication;
 
 /**
+ * Implementations of this interface must provide authentication of a user
+ * against a defined source.
  * 
  * @author Sebastian Wenzky
  * @since 4.5
  */
 public interface AuthenticationComponent {
-	
+
 	public boolean authenticate(String username, String password);
-	
+
 }
