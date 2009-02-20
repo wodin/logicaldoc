4404a59bd84646f4ec8d7d6584cc74ff5b45df43
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2009-02-20 17:17:08 +0000

    Import optimizations

--- community/logicaldoc/trunk/logicaldoc-core/src/test/java/com/logicaldoc/core/AbstractCoreTestCase.java
+++ community/logicaldoc/trunk/logicaldoc-core/src/test/java/com/logicaldoc/core/AbstractCoreTestCase.java
@@ -1,12 +1,7 @@
 package com.logicaldoc.core;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 
