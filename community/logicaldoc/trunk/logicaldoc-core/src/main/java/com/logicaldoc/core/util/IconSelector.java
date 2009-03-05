a9c11fada7ec74e4b59e49441aecaeffa5183adc
blucecio <blucecio@bae09422-6297-422f-b3ee-419521344c47>
2009-03-05 17:58:51 +0000

    Replaced document type icons

--- community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/util/IconSelector.java
+++ community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/util/IconSelector.java
@@ -12,29 +12,33 @@ public class IconSelector {
 		ext = ext.toLowerCase();
 
 		if (ext == null || ext.equalsIgnoreCase(""))
-			icon = "document.gif";
+			icon = "generic.png";
 		else if (ext.equals("pdf"))
-			icon = "pdf.gif";
-		else if (ext.equals("doc") || ext.equals("docx") || ext.equals("dot") || ext.equals("rtf") || ext.equals("sxw") || ext.equals("txt")
+			icon = "pdf.png";
+		else if (ext.equals("txt"))
+			icon = "text.png";
+		else if (ext.equals("doc") || ext.equals("docx") || ext.equals("dot") || ext.equals("rtf") || ext.equals("sxw")
 				|| ext.equals("wpd") || ext.equals("kwd") || ext.equals("abw") || ext.equals("zabw") || ext.equals("odt"))
-			icon = "textdoc.gif";
+			icon = "word.png";
 		else if (ext.equals("xls")|| ext.equals("xslx") || ext.equals("xlt") || ext.equals("sxc") || ext.equals("dbf") || ext.equals("ksp")
 				|| ext.equals("ods") || ext.equals("odb"))
-			icon = "tabledoc.gif";
+			icon = "excel.png";
 		else if (ext.equals("ppt") || ext.equals("pptx") || ext.equals("pps") || ext.equals("pot") || ext.equals("sxi") || ext.equals("kpr")
 				|| ext.equals("odp"))
-			icon = "presentdoc.gif";
+			icon = "powerpoint.png";
 		else if (ext.equals("apf") || ext.equals("bmp") || ext.equals("cur") || ext.equals("dib") || ext.equals("gif")
-				|| ext.equals("jpg") || ext.equals("psd") || ext.equals("tif") || ext.equals("tiff"))
-			icon = "picture.gif";
+				|| ext.equals("jpg") || ext.equals("psd") || ext.equals("tif") || ext.equals("tiff") || ext.equals("png"))
+			icon = "picture.png";
 		else if (ext.equals("htm") || ext.equals("html") || ext.equals("xml"))
-			icon = "internet.gif";
+			icon = "html.png";
         else if (ext.equals("mail"))       	
             icon = "mail.gif";
+        else if (ext.equals("zip"))       	
+            icon = "zip.png";
         else if (ext.equals("dwg"))       	
             icon = "dwg.gif";
         else
-			icon = "document.gif";
+			icon = "generic.png";
 		
 		return icon;
 	}
