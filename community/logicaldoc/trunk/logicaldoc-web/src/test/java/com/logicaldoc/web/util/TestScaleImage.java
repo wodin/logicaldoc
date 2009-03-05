cb9f58ec3c60822b158217cf08f883f04d305dd0
blucecio <blucecio@bae09422-6297-422f-b3ee-419521344c47>
2009-03-05 16:58:05 +0000

    Added document preview on document info page

--- community/logicaldoc/trunk/logicaldoc-web/src/test/java/com/logicaldoc/web/util/TestScaleImage.java
+++ community/logicaldoc/trunk/logicaldoc-web/src/test/java/com/logicaldoc/web/util/TestScaleImage.java
@@ -2,14 +2,14 @@ package com.logicaldoc.web.util;
 
 import junit.framework.TestCase;
 
-public class TestScaleJPG extends TestCase {
+public class TestScaleImage extends TestCase {
 
 	public void testScale() {
 		try {
 			String src = "C:/tmp/cucchiaio-legno.jpg";
 			int width = 150;
 			String dest = "C:/tmp/cucchiaio-legno-thumb.jpg";
-			ScaleJPG.scale(src, width, dest);
+			ScaleImage.scale(src, width, dest);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
