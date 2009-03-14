18a7fc7d289b6897c95d8033180a325c7663d7a9
blucecio <blucecio@bae09422-6297-422f-b3ee-419521344c47>
2009-03-14 11:03:39 +0000

    Backported LD 4.5 changes

--- community/logicaldoc/branches/logicaldoc-4.0/logicaldoc-web/src/main/java/com/logicaldoc/web/admin/GroupForm.java
+++ community/logicaldoc/branches/logicaldoc-4.0/logicaldoc-web/src/main/java/com/logicaldoc/web/admin/GroupForm.java
@@ -39,7 +39,7 @@ public class GroupForm {
 
 	private UIInput groupDesc = null;
 
-	private long parentGroup;
+	private Long parentGroup = null;
 
 	private String groupFilter = "";
 
@@ -57,11 +57,11 @@ public class GroupForm {
 		return group;
 	}
 
-	public long getParentGroup() {
+	public Long getParentGroup() {
 		return parentGroup;
 	}
 
-	public void setParentGroup(long parentGroup) {
+	public void setParentGroup(Long parentGroup) {
 		this.parentGroup = parentGroup;
 	}
 
@@ -234,15 +234,19 @@ public class GroupForm {
 				boolean stored = false;
 
 				if (recordsManager.getSelectedPanel().equals("create")) {
-					if (parentGroup <= 0) {
-						return null;
+					if (parentGroup == null || parentGroup.longValue() <= 0) {
+						stored = dao.store(group);
+					} else {
+						stored = dao.insert(group, parentGroup.longValue());
 					}
-					stored = dao.insert(group, parentGroup);
+
 				} else {
 					for (Preference preference : preferences) {
 						preference.updateGroup(group);
 					}
 					stored = dao.store(group);
+					if (parentGroup != null)
+						dao.inheritACLs(group.getId(), parentGroup.longValue());
 				}
 				if (!stored) {
 					Messages.addLocalizedError("errors.action.savegroup.notstored");
@@ -258,6 +262,7 @@ public class GroupForm {
 		recordsManager.setSelectedPanel("list");
 		return null;
 	}
+
 
 	/**
 	 * Inner class to define the Preference Bean user only by the Group
--- community/logicaldoc/branches/logicaldoc-4.0/logicaldoc-web/src/main/java/com/logicaldoc/web/admin/GroupsRecordsManager.java
+++ community/logicaldoc/branches/logicaldoc-4.0/logicaldoc-web/src/main/java/com/logicaldoc/web/admin/GroupsRecordsManager.java
@@ -105,7 +105,8 @@ public class GroupsRecordsManager {
 		if (mdao.isReadEnable(7, userId)) {
 			Group group = new Group();
 			groupForm.setGroup(group);
-			groupForm.setParentGroup(-1);
+			//groupForm.setParentGroup(-1);
+			groupForm.setParentGroup(null);
 			FacesUtil.clearAllMessages();
 		} else {
 			Messages.addLocalizedError("errors.noaccess");
