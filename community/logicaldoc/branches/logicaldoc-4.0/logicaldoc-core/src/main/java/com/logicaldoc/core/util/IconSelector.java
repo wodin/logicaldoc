2c4d7dca831e2f9481d1a55af0f354e007b9d2a8
blucecio <blucecio@bae09422-6297-422f-b3ee-419521344c47>
2009-03-15 16:49:54 +0000

    Changed document's icons in LD 4.0.2

--- community/logicaldoc/branches/logicaldoc-4.0/logicaldoc-core/src/main/java/com/logicaldoc/core/util/IconSelector.java
+++ community/logicaldoc/branches/logicaldoc-4.0/logicaldoc-core/src/main/java/com/logicaldoc/core/util/IconSelector.java
@@ -2,40 +2,46 @@ package com.logicaldoc.core.util;
 
 /**
  * utility class to select an icon based on a file extension
+ * 
  * @author Sebastian Stein
  */
 public class IconSelector {
-	
+
 	/** returns path to menu icon by parsing the provided file extension */
 	public static String selectIcon(String ext) {
 		String icon = "";
 		ext = ext.toLowerCase();
 
 		if (ext == null || ext.equalsIgnoreCase(""))
-			icon = "document.gif";
+			icon = "generic.png";
 		else if (ext.equals("pdf"))
-			icon = "pdf.gif";
-		else if (ext.equals("doc") || ext.equals("docx") || ext.equals("dot") || ext.equals("rtf") || ext.equals("sxw") || ext.equals("txt")
-				|| ext.equals("wpd") || ext.equals("kwd") || ext.equals("abw") || ext.equals("zabw") || ext.equals("odt"))
-			icon = "textdoc.gif";
-		else if (ext.equals("xls")|| ext.equals("xslx") || ext.equals("xlt") || ext.equals("sxc") || ext.equals("dbf") || ext.equals("ksp")
-				|| ext.equals("ods") || ext.equals("odb"))
-			icon = "tabledoc.gif";
-		else if (ext.equals("ppt") || ext.equals("pptx") || ext.equals("pps") || ext.equals("pot") || ext.equals("sxi") || ext.equals("kpr")
-				|| ext.equals("odp"))
-			icon = "presentdoc.gif";
-		else if (ext.equals("apf") || ext.equals("bmp") || ext.equals("cur") || ext.equals("dib") || ext.equals("gif")
-				|| ext.equals("jpg") || ext.equals("psd") || ext.equals("tif") || ext.equals("tiff"))
-			icon = "picture.gif";
-		else if (ext.equals("htm") || ext.equals("html") || ext.equals("xml"))
-			icon = "internet.gif";
-        else if (ext.equals("mail"))       	
-            icon = "mail.gif";
-        else if (ext.equals("dwg"))       	
-            icon = "dwg.gif";
-        else
-			icon = "document.gif";
-		
+			icon = "pdf.png";
+		else if (ext.equals("txt") || ext.equals("properties"))
+			icon = "text.png";
+		else if (ext.equals("doc") || ext.equals("odt") || ext.equals("docx") || ext.equals("rtf") || ext.equals("sxw")
+				|| ext.equals("wpd") || ext.equals("kwd") || ext.equals("dot"))
+			icon = "word.png";
+		else if (ext.equals("xls") || ext.equals("ods") || ext.equals("xslx") || ext.equals("xlt") || ext.equals("sxc")
+				|| ext.equals("dbf") || ext.equals("ksp") || ext.equals("odb"))
+			icon = "excel.png";
+		else if (ext.equals("ppt") || ext.equals("odp") || ext.equals("pptx") || ext.equals("pps") || ext.equals("pot")
+				|| ext.equals("sxi") || ext.equals("kpr"))
+			icon = "powerpoint.png";
+		else if (ext.equals("jpg") || ext.equals("gif") || ext.equals("png") || ext.equals("bmp") || ext.equals("tif")
+				|| ext.equals("tiff") || ext.equals("psd"))
+			icon = "picture.png";
+		else if (ext.equals("htm") || ext.equals("html") || ext.equals("xml") || ext.equals("xhtml"))
+			icon = "html.png";
+		else if (ext.equals("eml") || ext.equals("mail"))
+			icon = "email.png";
+		else if (ext.equals("zip") || ext.equals("rar") || ext.equals("gz") || ext.equals("tar") || ext.equals("jar")
+				|| ext.equals("7z"))
+			icon = "zip.png";
+		else if (ext.equals("dwg"))
+			icon = "dwg.gif";
+		else
+			icon = "generic.png";
+
 		return icon;
 	}
 }
