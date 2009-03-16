42978d7c93fd4f3150ce0e314b284633828d9e92
blucecio <blucecio@bae09422-6297-422f-b3ee-419521344c47>
2009-03-16 08:37:09 +0000

    test remove all users from group

--- community/logicaldoc/trunk/logicaldoc-core/src/test/java/com/logicaldoc/core/security/dao/SecurityManagerImplTest.java
+++ community/logicaldoc/trunk/logicaldoc-core/src/test/java/com/logicaldoc/core/security/dao/SecurityManagerImplTest.java
@@ -72,30 +72,74 @@ public class SecurityManagerImplTest extends AbstractCoreTestCase {
 		assertFalse(user.getGroups().contains(group));
 	}
 
+	public void testRemoveAllUsersFromGroup() {
+		// create a new group which extends author
+		Group authorGroup = groupDAO.findByName("author");
+
+		Group editorGroup = new Group();
+		editorGroup.setName("editors");
+		editorGroup.setDescription("Group for editors which extends author group.");
+
+		groupDAO.insert(editorGroup, authorGroup.getId());
+
+		// create 4 new users
+		// assign the newly created users to the editor group		
+		User user = new User();
+		user.setUserName("test1");
+		user.setDecodedPassword("xxxpwd");
+		userDAO.store(user);
+		manager.assignUserToGroup(user, editorGroup);
+
+		user = new User();
+		user.setUserName("test2");
+		user.setDecodedPassword("xxxpwd");
+		userDAO.store(user);
+		manager.assignUserToGroup(user, editorGroup);
+
+		user = new User();
+		user.setUserName("test3");
+		user.setDecodedPassword("xxxpwd");
+		userDAO.store(user);
+		manager.assignUserToGroup(user, editorGroup);
+
+		user = new User();
+		user.setUserName("test4");
+		user.setDecodedPassword("xxxpwd");
+		userDAO.store(user);
+		manager.assignUserToGroup(user, editorGroup);
+		
+		// remove all users from mthe group editors
+		manager.removeAllUsersFromGroup(editorGroup);
+		
+		// check
+		User userf = userDAO.findByUserName("test4");
+		assertFalse(userf.getGroups().contains(editorGroup));
+	}
+
 	public void testAssignUserToGroups() {
 		User user = new User();
 		user.setUserName("zzz");
 		user.setDecodedPassword("xxxpwd");
 		userDAO.store(user);
 	}
-	
+
 	public void testGetAllowedGroups() {
-		Set<Group> groups=manager.getAllowedGroups(5);
+		Set<Group> groups = manager.getAllowedGroups(5);
 		assertNotNull(groups);
 		assertTrue(groups.contains(groupDAO.findByName("admin")));
 		assertTrue(groups.contains(groupDAO.findByName("author")));
 		assertTrue(groups.contains(groupDAO.findByName("guest")));
 	}
-	
+
 	public void testIsMemberOfLongLong() {
-		assertTrue(manager.isMemberOf(1,1));
-		assertFalse(manager.isMemberOf(1,1234));
-		assertFalse(manager.isMemberOf(1,2));
+		assertTrue(manager.isMemberOf(1, 1));
+		assertFalse(manager.isMemberOf(1, 1234));
+		assertFalse(manager.isMemberOf(1, 2));
 	}
-	
+
 	public void testIsMemberOfLongString() {
-		assertTrue(manager.isMemberOf(1,"admin"));
-		assertFalse(manager.isMemberOf(1,"xyz"));
-		assertFalse(manager.isMemberOf(1,"author"));
+		assertTrue(manager.isMemberOf(1, "admin"));
+		assertFalse(manager.isMemberOf(1, "xyz"));
+		assertFalse(manager.isMemberOf(1, "author"));
 	}
 }
