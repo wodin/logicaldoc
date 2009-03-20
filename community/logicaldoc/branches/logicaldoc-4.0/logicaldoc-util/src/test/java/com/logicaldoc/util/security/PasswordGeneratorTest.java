99ae79b5b958c437a700193159f75362256aea7d
blucecio <blucecio@bae09422-6297-422f-b3ee-419521344c47>
2009-03-20 11:30:36 +0000

    Added test for passwordgenerator

---
+++ community/logicaldoc/branches/logicaldoc-4.0/logicaldoc-util/src/test/java/com/logicaldoc/util/security/PasswordGeneratorTest.java
@@ -0,0 +1,24 @@
+package com.logicaldoc.util.security;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PasswordGeneratorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGenerate() {
+//		PasswordGenerator pg = new PasswordGenerator();
+//		for(int i=0; i< 20; i++) {
+//			String xxx = pg.generate(8);
+//			System.out.println("passwd[" +i +"]: " + xxx);
+//		}
+		//fail("Not yet implemented");
+	}
+
+}
