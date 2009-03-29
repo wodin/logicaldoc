3cbd74d24e52b527942a23c1e75bf99873ee32a0
blucecio <blucecio@bae09422-6297-422f-b3ee-419521344c47>
2009-03-29 17:25:48 +0000

    changed task icon

--- community/logicaldoc/trunk/logicaldoc-core/src/main/resources/sql/logicaldoc-core.sql
+++ community/logicaldoc/trunk/logicaldoc-core/src/main/resources/sql/logicaldoc-core.sql
@@ -91,7 +91,7 @@ values     (-1,'2008-12-01',0,'admin.security',9,15,'password.png','/9',1,'admin
 
 insert into ld_menu
            (ld_id,ld_lastmodified,ld_deleted,ld_text,ld_parentid,ld_sort,ld_icon,ld_path,ld_type,ld_ref,ld_size)
-values     (14,'2008-10-22',0,'task.tasks',2,10,'thread.png','/2',1,'admin/tasks',0);
+values     (14,'2008-10-22',0,'task.tasks',2,10,'task_scheduler.png','/2',1,'admin/tasks',0);
 
 insert into ld_menu
            (ld_id,ld_lastmodified,ld_deleted,ld_text,ld_parentid,ld_sort,ld_icon,ld_path,ld_type,ld_ref,ld_size)
