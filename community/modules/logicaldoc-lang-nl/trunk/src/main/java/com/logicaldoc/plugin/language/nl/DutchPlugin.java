085519ff27ae799018163a1ec3acfca12f4df0e9
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2009-04-17 10:20:01 +0000

    Porting to 4.5 release

--- community/modules/logicaldoc-lang-nl/trunk/src/main/java/com/logicaldoc/plugin/language/nl/DutchPlugin.java
+++ community/modules/logicaldoc-lang-nl/trunk/src/main/java/com/logicaldoc/plugin/language/nl/DutchPlugin.java
@@ -1,25 +1,26 @@
 package com.logicaldoc.plugin.language.nl;
 
 import java.io.File;
-import java.util.Locale;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.logicaldoc.core.i18n.Language;
-import com.logicaldoc.core.searchengine.Indexer;
 import com.logicaldoc.util.config.FacesConfigurator;
-import com.logicaldoc.util.config.PropertiesBean;
 import com.logicaldoc.util.plugin.LogicalDOCPlugin;
 
+/**
+ * Language module plugin class.
+ * 
+ * @author Alessandro Gasparini - Logical Objects
+ * @since 4.0
+ */
 public class DutchPlugin extends LogicalDOCPlugin {
 
 	protected static Log log = LogFactory.getLog(DutchPlugin.class);
 
 	@Override
 	protected void install() throws Exception {
-		
 		String classesDir = getManager().getPathResolver().resolvePath(getDescriptor(), "classes").toString();
 		log.debug("classesDir: " + classesDir);
 		if (classesDir.startsWith("file:")) {
@@ -32,45 +33,10 @@ public class DutchPlugin extends LogicalDOCPlugin {
 
 		log.debug("Copy resources from " + src.getPath() + " to " + destClasses.getPath());
 		FileUtils.copyDirectory(src, destClasses);
-		
-		// Create Lucene Index
-		createLuceneIndex();
 
 		// Now add the message bundle
 		log.info("Add Dutch (nl) language to faces-config.xml");
 		FacesConfigurator facesConfig = new FacesConfigurator();
 		facesConfig.addLanguageToFacesConfig("nl");
 	}
-
-	private void createLuceneIndex() throws Exception {
-		try {
-			Class.forName("com.logicaldoc.core.i18n.Language");
-		} catch (ClassNotFoundException ex) {
-			// The core plugin was not ready, so avoid initializations
-			return;
-		}
-
-		log.info("Create Lucene Index");
-		PropertiesBean pbean = new PropertiesBean(getClass().getClassLoader().getResource("context.properties"));
-		String indexdir = pbean.getProperty("conf.indexdir");
-		log.info("indexdir = '" + indexdir + "'");
-		if (indexdir == null || indexdir.equals("")) {
-			throw new Exception("System un-setted up, impossible to create Lucene Index");
-		}
-
-		try {
-			Language ldLanguage = new Language(new Locale("nl"));
-			File indexPath = new File(indexdir, ldLanguage.getIndex());
-
-			// Prevent overwrite of an already present index
-			if (indexPath.exists())
-				return;
-
-			Indexer.createIndex(indexPath, "nl");
-		} catch (Exception e) {
-			log.error("Unable to: createLuceneIndex(): " + e.getMessage(), e);
-			throw e;
-		}
-	}
-
 }
