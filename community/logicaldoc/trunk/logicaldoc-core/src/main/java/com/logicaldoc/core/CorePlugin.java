35be3493e2cce5d18e3020b1d0c69976fbd8c39f
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2009-05-04 15:52:08 +0000

    Removed comments

--- community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/CorePlugin.java
+++ community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/CorePlugin.java
@@ -1,16 +1,9 @@
 package com.logicaldoc.core;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.java.plugin.registry.Extension;
-import org.java.plugin.registry.ExtensionPoint;
 
 import com.logicaldoc.core.task.TaskManager;
-import com.logicaldoc.util.config.ContextConfigurator;
 import com.logicaldoc.util.plugin.LogicalDOCPlugin;
 
 /**
@@ -21,7 +14,6 @@ import com.logicaldoc.util.plugin.LogicalDOCPlugin;
  */
 public class CorePlugin extends LogicalDOCPlugin {
 
-
 	protected static Log log = LogFactory.getLog(CorePlugin.class);
 
 	@Override
@@ -30,22 +22,5 @@ public class CorePlugin extends LogicalDOCPlugin {
 		// Register scheduled tasks
 		TaskManager manager = new TaskManager();
 		manager.registerTasks();
-
-		ContextConfigurator cfg = new ContextConfigurator();
-
-//		// AuthenticationChain
-//		List<String> authenticationComponents = new LinkedList<String>();
-//		authenticationComponents.add("BasicAuthentication");
-//		ExtensionPoint toolExtPoint = getManager().getRegistry().getExtensionPoint(getDescriptor().getId(),
-//				AUTHENTICATION_COMPONENTS_BEAN_ID);
-//		for (Iterator<Extension> it = toolExtPoint.getConnectedExtensions().iterator(); it.hasNext();) {
-//			Extension ext = it.next();
-//			authenticationComponents.add(ext.getParameter("beanId").valueAsString());
-//		}
-//		cfg.clearPropertyValue(AUTHENTICATION_CHAIN_BEAN_ID, AUTHENTICATION_COMPONENTS_BEAN_ID);
-//		cfg.addPropertyBeanRefList(AUTHENTICATION_CHAIN_BEAN_ID, AUTHENTICATION_COMPONENTS_BEAN_ID,
-//				authenticationComponents);
-//		System.out.println("*** authenticationComponents="+authenticationComponents);
-//		cfg.write();
 	}
 }
