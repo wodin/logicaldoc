6409764471745d79ba7f2b989abdeebddb9fa415
cheffilet <cheffilet@bae09422-6297-422f-b3ee-419521344c47>
2009-05-23 13:40:26 +0000

    Removing first-draft implementation of JBPM and replacing it with Spring-JBPM-Module.

--- community/logicaldoc/trunk/logicaldoc-workflow/src/main/java/com/logicaldoc/workflow/WorkflowPlugin.java
+++ community/logicaldoc/trunk/logicaldoc-workflow/src/main/java/com/logicaldoc/workflow/WorkflowPlugin.java
@@ -1,115 +1,60 @@
 package com.logicaldoc.workflow;
 
-
-import java.io.IOException;
-import java.util.Properties;
-
-import org.hibernate.MappingNotFoundException;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.java.plugin.PluginManager;
+import org.jbpm.JbpmConfiguration;
+import org.springmodules.workflow.jbpm31.LocalJbpmConfigurationFactoryBean;
 
-import com.logicaldoc.core.spring.PropertiesPlaceHolder;
 import com.logicaldoc.util.Context;
+import com.logicaldoc.util.config.WebConfigurator;
 import com.logicaldoc.util.event.SystemEvent;
 import com.logicaldoc.util.event.SystemEventStatus;
 import com.logicaldoc.util.plugin.LogicalDOCPlugin;
 
 @SuppressWarnings("unused")
-public class WorkflowPlugin extends LogicalDOCPlugin{
+public class WorkflowPlugin extends LogicalDOCPlugin {
 
-	@SuppressWarnings("serial")
-	public class ClassLoaderConfiguration extends Configuration {
-	
-		@Override
-		public Configuration addResource(String resourceName){
-			Configuration configuration = null;
-			try {
-				configuration = super.addResource(resourceName);
-			}catch(MappingNotFoundException e){System.out.println("WARNING: " + resourceName + " can not be found normally");}
-			
-			if(configuration == null){
-				
-				try {
-					//configuration =  addInputStream( null );
-					String s = "";
-				} catch (Exception e) {
-					throw new RuntimeException(e);
-				}
-			
-			}
-			return configuration;
-		}
-	}
-	
-	private class Event extends SystemEvent {
-		
+	private class InstallationEvent extends SystemEvent {
+
 		private PluginManager manager;
-		
-		public Event(PluginManager manager){
+
+		public InstallationEvent(PluginManager manager) {
 			this();
 			this.manager = manager;
 		}
-		
-		public Event() {
+
+		public InstallationEvent() {
 			super(SystemEventStatus.BEANS_AVAILABLE);
 		}
 
 		@Override
 		public void processEvent() {
+
 			Context ctx = Context.getInstance();
-			Configuration config = new Configuration();
-			PropertiesPlaceHolder cfgPlaceHolder = (PropertiesPlaceHolder)ctx.getBean("PropertyPlaceholderConfigurer");
-			Properties properties = null;
-			try {
-				properties = cfgPlaceHolder.mergeProperties();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			
+			JbpmConfiguration jbpmInstallConfig = (JbpmConfiguration) ctx
+					.getBean("jbpmConfiguration");
+			
+			jbpmInstallConfig.dropSchema();
+
+			jbpmInstallConfig.createSchema();
 			
 
-			/*
-			  		Setting up following properties:
-			 
-			      <property name="hibernate.dialect">org.hibernate.dialect.MySQLDialect</property>
-				  <property name="hibernate.connection.driver_class">com.mysql.jdbc.Driver</property>
-				  <property name="hibernate.connection.url">jdbc:mysql://localhost:3306/jbpm_test</property>
-				  <property name="hibernate.connection.username"></property>
-				  <property name="hibernate.connection.password"></property>
-			 */
-			
-			Configuration configuration = new Configuration();
-			configuration.configure(this.getClass().getResource("/workflow-hibernate.cfg.xml"));
-			configuration.setProperty("hibernate.dialect", properties.getProperty("hibernate.dialect"));
-			configuration.setProperty("hibernate.connection.driver_class", properties.getProperty("jdbc.driver"));
-			configuration.setProperty("hibernate.connection.url", properties.getProperty("jdbc.url"));
-			configuration.setProperty("hibernate.connection.username", properties.getProperty("jdbc.username"));
-			configuration.setProperty("hibernate.connection.password", properties.getProperty("jdbc.password"));
-			configuration.setProperty("hibernate.hbm2ddl.auto", "create");
-			
-
-			SchemaExport schema = new SchemaExport(configuration);
-			
-			schema.drop(true, true);
-			schema.create(true, true);
-			
-			if(properties == null)
-				throw new RuntimeException("Properties file must not be null");
-			
 		}
-		
+
 	}
-	
-	@Override
-	protected void start() throws Exception {
-		Context.addListener(new Event(getManager()));
-		
-	}
-	
+
 	protected void install() throws Exception {
-		
+
 		Configuration installConfiguration = new Configuration();
-		Context.addListener(new Event());
-		String s = "";
+		Context.addListener(new InstallationEvent());
+		
+		WebConfigurator config = new WebConfigurator();
+		config.addServlet("Workflow Servlet",
+				"com.logicaldoc.workflow.TestServlet", 4);
+		config.writeXMLDoc();
+	
+		config.addServletMapping("Workflow Servlet", "/webdav/*");
+		config.writeXMLDoc();
 	}
 }
