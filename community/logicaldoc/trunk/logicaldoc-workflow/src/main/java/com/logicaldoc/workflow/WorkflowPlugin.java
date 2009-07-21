5ede49927cc3008710ebbf602188d6065336003c
cheffilet <cheffilet@bae09422-6297-422f-b3ee-419521344c47>
2009-07-21 20:00:17 +0000

    File-based Management of the Workflowtemplates

--- community/logicaldoc/trunk/logicaldoc-workflow/src/main/java/com/logicaldoc/workflow/WorkflowPlugin.java
+++ community/logicaldoc/trunk/logicaldoc-workflow/src/main/java/com/logicaldoc/workflow/WorkflowPlugin.java
@@ -1,6 +1,7 @@
 package com.logicaldoc.workflow;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
 import org.hibernate.cfg.Configuration;
@@ -17,12 +18,15 @@ import com.logicaldoc.util.config.WebConfigurator;
 import com.logicaldoc.util.event.SystemEvent;
 import com.logicaldoc.util.event.SystemEventStatus;
 import com.logicaldoc.util.plugin.LogicalDOCPlugin;
+import com.logicaldoc.workflow.editor.WorkflowTemplateLoader;
 
 @SuppressWarnings("unused")
 public class WorkflowPlugin extends LogicalDOCPlugin {
 
 	private class InstallationEvent extends SystemEvent {
-
+		
+		private String pluginDirectory;
+		
 		private PluginManager manager;
 
 		public InstallationEvent(PluginManager manager) {
@@ -36,17 +40,68 @@ public class WorkflowPlugin extends LogicalDOCPlugin {
 
 		@Override
 		public void processEvent() {
+			
+			//Create a folder for the templates
+			File f = new File(this.pluginDirectory + "/templates/");
+			
+			if(f.exists() == false){
+				
+				try {
+					f.createNewFile();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+				
+			}
+			
 			Context ctx = Context.getInstance();
 			JbpmConfiguration jbpmInstallConfig = (JbpmConfiguration) ctx
 					.getBean("jbpmConfiguration");			
 			jbpmInstallConfig.dropSchema();
 			jbpmInstallConfig.createSchema();
 		}
+		
+		public void setPluginDirectory(String pluginDirectory) {
+			this.pluginDirectory = pluginDirectory;
+		}
+	}
+	
+	private class VariableEvent extends SystemEvent {
+
+		private String pluginDirectory;
+		
+		private PluginManager manager;
+
+		public VariableEvent(PluginManager manager) {
+			this();
+			this.manager = manager;
+		}
+
+		public VariableEvent() {
+			super(SystemEventStatus.BEANS_AVAILABLE);
+		}
+		
+		public void setPluginDirectory(String pluginDirectory) {
+			this.pluginDirectory = pluginDirectory;
+		}
+
+		@Override
+		public void processEvent() {
+			Context ctx = Context.getInstance();
+			WorkflowTemplateLoader workflowTemplateLoader = (WorkflowTemplateLoader) ctx
+					.getBean("WorkflowTemplateLoader");			
+		
+			workflowTemplateLoader.setPluginDirectory(this.pluginDirectory);
+		}
 	}
 
 	protected void install() throws Exception {
-		Configuration installConfiguration = new Configuration();
-		Context.addListener(new InstallationEvent());
+		
+		
+		InstallationEvent installationEvent = new InstallationEvent();
+		installationEvent.setPluginDirectory(getDataDirectory().getAbsolutePath());
+		Context.addListener(installationEvent);
+		
 		super.install();
 
 		String webappDir = resolvePath("webapp");
@@ -58,6 +113,10 @@ public class WorkflowPlugin extends LogicalDOCPlugin {
 	
 	@Override
 	protected void start() throws Exception {
-			
+		
+		VariableEvent variableEvent = new VariableEvent();
+		variableEvent.setPluginDirectory(getDataDirectory().getAbsolutePath() + "/templates/");
+		
+		Context.addListener(variableEvent);
 	}
 }
--- community/logicaldoc/trunk/logicaldoc-workflow/src/main/java/com/logicaldoc/workflow/editor/WorkflowTemplateLoader.java
+++ community/logicaldoc/trunk/logicaldoc-workflow/src/main/java/com/logicaldoc/workflow/editor/WorkflowTemplateLoader.java
@@ -1,14 +1,30 @@
 package com.logicaldoc.workflow.editor;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-import com.logicaldoc.workflow.model.WorkflowDefinition;
 import com.logicaldoc.workflow.persistence.WorkflowPersistenceTemplate;
 
 public class WorkflowTemplateLoader extends HibernateDaoSupport{
 	
+	private String pluginDirectory;
+	
+	private String resolveSystemPath(WorkflowPersistenceTemplate template){
+		return this.pluginDirectory + template.getName() + "-" + template.getId() + ".jbpm";
+	}
+	
+	public void setPluginDirectory(String pluginDirectory) {
+		this.pluginDirectory = pluginDirectory;
+	}
 	
 	@SuppressWarnings("unchecked")
 	public List<WorkflowPersistenceTemplate> getAvailableWorkflowTemplates(){
@@ -30,16 +46,63 @@ public class WorkflowTemplateLoader extends HibernateDaoSupport{
 			getHibernateTemplate().update(persistenceTemplate);
 		}
 		
+		//save IO 
+		
+		File workflowFile = new File(resolveSystemPath(persistenceTemplate));
+		try {
+			DataOutputStream os = new DataOutputStream(new FileOutputStream(
+			        workflowFile));
+			os.write(((String)persistenceTemplate.getXmldata()).getBytes("UTF-8"));
+			os.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
 		return (Long)id;
 		
 	}
 	
 	public void deleteWorkflowTemplate(WorkflowPersistenceTemplate persistenceTemplate){
+		
 		getHibernateTemplate().delete(persistenceTemplate);
+		
+		File workflowFile = new File(resolveSystemPath(persistenceTemplate));
+		try {
+			workflowFile.delete();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
 	}
 	
 	public WorkflowPersistenceTemplate loadWorkflowTemplate(Long id){
-		return (WorkflowPersistenceTemplate)getHibernateTemplate().load(WorkflowPersistenceTemplate.class, id);
+		WorkflowPersistenceTemplate workflowPersistenceTemplate = 
+		(WorkflowPersistenceTemplate)getHibernateTemplate().load(WorkflowPersistenceTemplate.class, id);
+		
+		File workflowFile = new File(resolveSystemPath(workflowPersistenceTemplate));
+		try {
+			FileInputStream is = new FileInputStream(
+			        workflowFile);
+			
+			int read = 0;
+			byte[] buf = new byte[4192];
+			
+			ByteArrayOutputStream bos = new ByteArrayOutputStream(buf.length);
+
+			
+			while ((read = is.read(buf)) != -1)
+				 bos.write(buf, 0, (int) read); 
+			 
+			is.close();
+			bos.close();
+			 
+			workflowPersistenceTemplate.setXmldata( bos.toString("UTF-8") );
+			 
+			return workflowPersistenceTemplate;
+			 
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 	public WorkflowPersistenceTemplate loaWorkflowTemplate(WorkflowPersistenceTemplate workflowTemplate){
