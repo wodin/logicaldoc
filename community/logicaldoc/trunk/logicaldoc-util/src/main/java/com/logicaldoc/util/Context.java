d17020bccc41b3bb32ef95a3963796e5e95d9628
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2009-07-22 09:39:34 +0000

    Catch exception during context refresh

--- community/logicaldoc/trunk/logicaldoc-util/src/main/java/com/logicaldoc/util/Context.java
+++ community/logicaldoc/trunk/logicaldoc-util/src/main/java/com/logicaldoc/util/Context.java
@@ -21,14 +21,13 @@ import com.logicaldoc.util.event.SystemEventStatus;
 /**
  * Utility class collecting static methods related to spring's context.
  * 
- * @author Marco Meschieri
- * @version $Id:$
+ * @author Marco Meschieri - Logical Objects
  * @since 3.0
  */
 public class Context implements ApplicationContextAware, ApplicationListener {
 
 	private static HashMap<SystemEventStatus, LinkedList<SystemEvent>> systemEvents = new HashMap<SystemEventStatus, LinkedList<SystemEvent>>();
-	
+
 	// Singleton instance
 	private static Context instance;
 
@@ -42,7 +41,7 @@ public class Context implements ApplicationContextAware, ApplicationListener {
 	public static Context getInstance() {
 		return instance;
 	}
-	
+
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		Context.applicationContext = applicationContext;
 	}
@@ -81,89 +80,95 @@ public class Context implements ApplicationContextAware, ApplicationListener {
 	 * changed
 	 */
 	public static void refresh() {
-		if (applicationContext != null){
-			((AbstractApplicationContext) applicationContext).stop();
-			((AbstractApplicationContext) applicationContext).start();
-			((AbstractApplicationContext) applicationContext).refresh();
+		if (applicationContext != null) {
+			try {
+				((AbstractApplicationContext) applicationContext).stop();
+				((AbstractApplicationContext) applicationContext).start();
+				((AbstractApplicationContext) applicationContext).refresh();
+			} catch (Throwable e) {
+//				e.printStackTrace();
+			}
 		}
 	}
 
 	/**
-	 * Adds an Listener to a particular Event given from 
+	 * Adds an Listener to a particular Event given from
+	 * 
 	 * @see {@link SystemEvent#getSystemStatus()}
 	 * @param evt
 	 */
 	public static void addListener(SystemEvent evt) {
-		
+
 		synchronized (systemEvents) {
 			LinkedList<SystemEvent> evts = Context.systemEvents.get(evt.getSystemStatus());
-			
-			//firstly we have to initialize every systemstatus
-			if(evts == null){
+
+			// firstly we have to initialize every systemstatus
+			if (evts == null) {
 				evts = new LinkedList<SystemEvent>();
 				Context.systemEvents.put(evt.getSystemStatus(), evts);
 			}
-			
+
 			evts.add(evt);
 		}
 	}
-	
+
 	/**
 	 * Removes a particular Listener from the list
+	 * 
 	 * @param evt
 	 */
-	public static void removeListener(SystemEvent evt){
+	public static void removeListener(SystemEvent evt) {
 		synchronized (systemEvents) {
 			LinkedList<SystemEvent> evts = Context.systemEvents.get(evt.getSystemStatus());
-			if(evts == null || evts.size() == 0)
+			if (evts == null || evts.size() == 0)
 				return;
-		
+
 			evts.remove(evt);
 		}
 	}
-	
-	public Resource[] getResources(String resourcePattern){
+
+	public Resource[] getResources(String resourcePattern) {
 		try {
 			return this.applicationContext.getResources(resourcePattern);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
+
 	/**
 	 * Returns a Spring-Resource
+	 * 
 	 * @return
 	 */
 	public Resource getResource(String resourceLocation) {
 		return this.applicationContext.getResource(resourceLocation);
 	}
-	
+
 	/**
-	 * Processes a newly incoming event on appropriated events that 
-	 * registered itself on it
+	 * Processes a newly incoming event on appropriated events that registered
+	 * itself on it
 	 */
 	@Override
 	public synchronized void onApplicationEvent(ApplicationEvent event) {
-		if((event instanceof ContextStartedEvent) || (event instanceof ContextRefreshedEvent)){
+		if ((event instanceof ContextStartedEvent) || (event instanceof ContextRefreshedEvent)) {
 			processEvents(SystemEventStatus.BEANS_AVAILABLE);
-		}
-		else if(event instanceof ContextClosedEvent){
+		} else if (event instanceof ContextClosedEvent) {
 			processEvents(SystemEventStatus.SYSTEM_DESTORY);
 		}
-		
+
 	}
-	
-	private void processEvents(SystemEventStatus st){
+
+	private void processEvents(SystemEventStatus st) {
 		LinkedList<SystemEvent> evts = Context.systemEvents.get(st);
 		
-		//surely its possible that no one event have been 
-		//registered to the current spring event
-		if(evts == null || evts.size() == 0)
+		// surely its possible that no one event have been
+		// registered to the current spring event
+		if (evts == null || evts.size() == 0)
 			return;
-		
-		for(SystemEvent evt : evts){
+
+		for (SystemEvent evt : evts) {
 			evt.processEvent();
 		}
 	}
