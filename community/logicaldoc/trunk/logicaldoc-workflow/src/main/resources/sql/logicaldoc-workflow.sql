6db85a83987277dc3520a199d549efeb8ab695e9
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2009-07-27 12:27:28 +0000

    First implementation of My Tasks page

--- community/logicaldoc/trunk/logicaldoc-workflow/src/main/resources/sql/logicaldoc-workflow.sql
+++ community/logicaldoc/trunk/logicaldoc-workflow/src/main/resources/sql/logicaldoc-workflow.sql
@@ -4,3 +4,14 @@ insert into ld_menu (ld_id,ld_lastmodified,ld_deleted,ld_text,ld_parentid,ld_sor
 values     (-10,'2009-07-21',0,'logicaldoc-workflow.workflow',2,4,'workflow.png','/2',1,'workflow/manage-workflowtemplates',0);
 insert into ld_menugroup(ld_menuid, ld_groupid, ld_write , ld_addchild, ld_managesecurity, ld_manageimmutability, ld_delete, ld_rename, ld_bulkimport, ld_bulkexport, ld_sign, ld_archive)
 values     (-10,1,1,1,1,1,1,1,1,1,1,1);
+
+
+insert into ld_menu (ld_id,ld_lastmodified,ld_deleted,ld_text,ld_parentid,ld_sort,ld_icon,ld_path,ld_type,ld_ref,ld_size)
+values     (-11,'2008-10-22',0,'logicaldoc-workflow.mytasks',4,10,'workflow.png','/4',1,'workflow/mytasks',0);
+
+insert into ld_menugroup(ld_menuid, ld_groupid, ld_write , ld_addchild, ld_managesecurity, ld_manageimmutability, ld_delete, ld_rename, ld_bulkimport, ld_bulkexport, ld_sign, ld_archive)
+values     (-11,1,1,1,1,1,1,1,1,1,1,1);
+insert into ld_menugroup(ld_menuid, ld_groupid, ld_write , ld_addchild, ld_managesecurity, ld_manageimmutability, ld_delete, ld_rename, ld_bulkimport, ld_bulkexport, ld_sign, ld_archive)
+values     (-11,2,1,1,1,1,1,1,1,1,1,1);
+insert into ld_menugroup(ld_menuid, ld_groupid, ld_write , ld_addchild, ld_managesecurity, ld_manageimmutability, ld_delete, ld_rename, ld_bulkimport, ld_bulkexport, ld_sign, ld_archive)
+values     (-11,3,1,1,1,1,1,1,1,1,1,1);
