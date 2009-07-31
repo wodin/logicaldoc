05c342bcd46648cedbb6aebee44472f89c9b9925
blucecio <blucecio@bae09422-6297-422f-b3ee-419521344c47>
2009-07-31 17:45:37 +0000

    Replaced HTMLParser with a more efficient one based on CyberNeko

--- community/logicaldoc/branches/logicaldoc-4.5/logicaldoc-core/src/main/java/com/logicaldoc/core/text/parser/HTMLParser.java
+++ community/logicaldoc/branches/logicaldoc-4.5/logicaldoc-core/src/main/java/com/logicaldoc/core/text/parser/HTMLParser.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.StringReader;
+import java.nio.charset.Charset;
 
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
@@ -21,6 +22,11 @@ import javax.xml.transform.sax.SAXSource;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.xerces.xni.parser.XMLDocumentFilter;
+import org.apache.xerces.xni.parser.XMLParserConfiguration;
+import org.cyberneko.html.HTMLConfiguration;
+import org.cyberneko.html.filters.DefaultFilter;
+import org.cyberneko.html.filters.ElementRemover;
 import org.xml.sax.InputSource;
 import org.xml.sax.helpers.DefaultHandler;
 
@@ -46,7 +52,19 @@ public class HTMLParser extends AbstractParser {
         try {
             TransformerFactory factory = TransformerFactory.newInstance();
             Transformer transformer = factory.newTransformer();
-            HTMLSAXParser parser = new HTMLSAXParser();
+            
+           	XMLParserConfiguration parserConfig = new HTMLConfiguration();
+           	
+           	ElementRemover remover = new ElementRemover();
+           	remover.removeElement("script");
+           	remover.removeElement("noscript");
+           	remover.removeElement("style");
+ 
+           	XMLDocumentFilter[] filters = { remover };
+           	parserConfig.setProperty("http://cyberneko.org/html/properties/filters", filters);
+
+            HTMLSAXParser parser = new HTMLSAXParser(parserConfig);
+            
             SAXResult result = new SAXResult(new DefaultHandler());
 
             Reader reader;
@@ -75,7 +93,10 @@ public class HTMLParser extends AbstractParser {
 		FileInputStream fis = null;
 		try {
 			fis = new FileInputStream(file);
-			Reader read = extractText(fis, null, "UTF-8");
+			String encoding = getCharEncoding(fis);
+			//System.out.println("encoding: " + encoding);
+			fis = new FileInputStream(file);
+			Reader read = extractText(fis, null, encoding);
 			
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			OutputStreamWriter osw = new OutputStreamWriter(baos, "UTF-8");
@@ -84,7 +105,7 @@ public class HTMLParser extends AbstractParser {
 			BufferedReader br = new BufferedReader(read);
 			String inputLine;
 			while ((inputLine = br.readLine()) != null) {
-				System.out.println(inputLine);
+				//System.out.println(inputLine);
 				bw.write(inputLine);
 				bw.newLine();
 			}
@@ -107,5 +128,30 @@ public class HTMLParser extends AbstractParser {
 				}
 			}
 		}
+	}
+
+	private String getCharEncoding(FileInputStream fis) throws IOException {
+		
+		Reader read = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(read);
+		String inputLine;
+		while ((inputLine = br.readLine()) != null) {
+			//System.out.println(inputLine);
+			if (inputLine.indexOf("charset") != -1) {
+				//System.out.println("inputLine: " + inputLine);
+				String chs = inputLine.substring(inputLine.indexOf("charset") + "charset=".length());
+				if (chs.indexOf("\"") != -1)
+					chs = chs.substring(0, chs.indexOf("\""));
+				if (chs.indexOf(">") != -1)
+					chs = chs.substring(0, chs.indexOf(">"));
+				//System.out.println("charset: " + chs);
+				// verify if is a valid charset
+				boolean supportedCharset = Charset.isSupported(chs);
+				if (supportedCharset == true)
+					return chs;
+			}
+		}
+
+		return "UTF-8";
 	}
 }
