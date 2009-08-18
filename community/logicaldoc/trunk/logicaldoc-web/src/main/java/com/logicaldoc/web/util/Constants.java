279184b898d8fb096e639664a310de6e857e5bec
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2009-08-18 07:11:39 +0000

    Added constants for operative system detection

--- community/logicaldoc/trunk/logicaldoc-web/src/main/java/com/logicaldoc/web/util/Constants.java
+++ community/logicaldoc/trunk/logicaldoc-web/src/main/java/com/logicaldoc/web/util/Constants.java
@@ -21,10 +21,10 @@ public final class Constants {
 
 	// The currenly logged user identifier
 	public static final String AUTH_USERID = "authUserId";
-	
+
 	// The currenly logged user password
 	public static final String AUTH_PASSWORD = "authPassword";
-	
+
 	// The language of the currently logged user
 	public static final String LANGUAGE = "language";
 
@@ -34,7 +34,40 @@ public final class Constants {
 	public static final String LOCALE = "locale";
 
 	public static final String TIMEZONE = "timezone";
-	
+
 	// Sessions the context key of the sessions map
 	public static final String SESSIONS = "sessions";
+
+	public static final String OSName = System.getProperty("os.name");
+
+	public static final boolean isOSX = OSName.toLowerCase().startsWith("mac os");
+
+	public static final boolean isLinux = OSName.equalsIgnoreCase("Linux");
+
+	public static final boolean isSolaris = OSName.equalsIgnoreCase("SunOS");
+
+	public static final boolean isFreeBSD = OSName.equalsIgnoreCase("FreeBSD");
+
+	public static final boolean isWindowsXP = OSName.equalsIgnoreCase("Windows XP");
+
+	public static final boolean isWindowsVista = OSName.equalsIgnoreCase("Windows Vista");
+
+	public static final boolean isWindows95 = OSName.equalsIgnoreCase("Windows 95");
+
+	public static final boolean isWindows98 = OSName.equalsIgnoreCase("Windows 98");
+
+	public static final boolean isWindows2000 = OSName.equalsIgnoreCase("Windows 2000");
+
+	public static final boolean isWindowsME = OSName.equalsIgnoreCase("Windows ME");
+
+	public static final boolean isWindows9598ME = isWindows95 || isWindows98 || isWindowsME;
+
+	public static boolean isSafeMode = false;
+
+	public static final boolean isWindows = OSName.toLowerCase().startsWith("windows");
+
+	// If it isn't windows or osx, it's most likely an unix flavor
+	public static final boolean isUnix = !isWindows && !isOSX;
+
+	public static final String JAVA_VERSION = System.getProperty("java.version");
 }
