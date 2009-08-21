dea16c46a01c2a43d6c8a3c2a4a359e8cc561f26
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2009-08-21 16:05:33 +0000

    label updates

--- community/logicaldoc/trunk/logicaldoc-i18n/src/main/java/com/logicaldoc/i18n/PurifyRB.java
+++ community/logicaldoc/trunk/logicaldoc-i18n/src/main/java/com/logicaldoc/i18n/PurifyRB.java
@@ -4,83 +4,102 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 public class PurifyRB {
 
 	/**
 	 * @param args
+	 * @throws IOException
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		ResourceBundle master = ResourceBundle.getBundle("i18n/application", Locale.ENGLISH);
-		
-		Locale[] locales = new Locale[]{Locale.GERMAN, Locale.FRENCH, Locale.ITALIAN, new Locale("es"), new Locale("pt"), new Locale("nl"), new Locale("pt", "BR"), Locale.CHINA, new Locale("ro"),new Locale("el")};
-		for (int i = 0; i < locales.length; i++) {
+		List<Locale> locales = new ArrayList<Locale>();
+		Properties loc = new Properties();
+		loc.load(PurifyRB.class.getResourceAsStream("/i18n/i18n.properties"));
+		StringTokenizer st = new StringTokenizer(loc.getProperty("locales"), ",", false);
+		while (st.hasMoreElements()) {
+			String elem = (String) st.nextElement();
+			locales.add(toLocale(elem));
+		}
+
+		List<String> bundles = new ArrayList<String>();
+		st = new StringTokenizer(loc.getProperty("bundles"), ",", false);
+		while (st.hasMoreElements()) {
+			String elem = (String) st.nextElement();
+			bundles.add(elem);
+		}
+
+		for (String bundle : bundles) {
+			System.out.println("\nInspecting bundle "+bundle);
 			
-			try {
-				System.out.println("\nAnalyzing language: "+ locales[i]);
-				writePurifiedRB(master, locales[i]);
-			} catch (Exception e) {
-                System.out.println("Locale: "+ locales[i] + ", NOT FOUND");
-                e.printStackTrace();
+			ResourceBundle master = ResourceBundle.getBundle("i18n/"+bundle, Locale.ENGLISH);
+			for (Locale locale : locales) {
+				try {
+					System.out.println("\nAnalyzing language: " + locale);
+					writePurifiedRB(master, "i18n/"+bundle, locale);
+				} catch (Exception e) {
+					System.out.println("Locale: " + locale + ", NOT FOUND");
+					e.printStackTrace();
+				}
 			}
 		}
-		
-		
+
 		System.out.println("Finished");
 	}
 
-	private static void writePurifiedRB(ResourceBundle master, Locale locale) {
-		
-		ResourceBundle translated = ResourceBundle.getBundle("i18n/application", locale);
-		
+	private static void writePurifiedRB(ResourceBundle master, String transalatedBundle, Locale locale) {
+		ResourceBundle translated = ResourceBundle.getBundle(transalatedBundle, locale);
 		if (locale != translated.getLocale()) {
-			System.out.println("Translation for locale: "+ locale + ", not Found; Skipped");
+			System.out.println("Translation for locale: " + locale + ", not Found; Skipped");
 			return;
 		}
-		
-		
+
 		Properties destprop = new Properties();
-	
-        // 1: controllare che nel bundle dest i valori delle chiavi siano diversi da quelli nel bundle source
+
+		// 1: controllare che nel bundle dest i valori delle chiavi siano
+		// diversi da quelli nel bundle source
 
 		int counter = 0;
 		int mre = 0;
-		
+
 		Set<String> keySet = translated.keySet();
 		for (String key : keySet) {
-	         String value = translated.getString(key);
-	         try {
+			String value = translated.getString(key);
+			try {
 				String masterValue = master.getString(key);
-				
-				 if (masterValue != null) {
-					 // la chiave esiste anche nel master
-					 if (!value.equals(masterValue)) {
-						 // il valore è diverso da quello iniziale
-						 // copio l'accoppiata chiave valore nel properties di destinazione
-						 destprop.put(key, value);
-						 //System.out.println("trans <> master");
-						 counter++;
-					 }
-				 }
+
+				if (masterValue != null) {
+					// la chiave esiste anche nel master
+					if (!value.equals(masterValue)) {
+						// il valore è diverso da quello iniziale
+						// copio l'accoppiata chiave valore nel properties di
+						// destinazione
+						destprop.put(key, value);
+						// System.out.println("trans <> master");
+						counter++;
+					}
+				}
 			} catch (MissingResourceException e) {
 				mre++;
 			}
 		}
-	
-		System.out.println("trans props n.: " +translated.keySet().size());
-		System.out.println("written props n.: " +counter);
-		System.out.println("exceding keywords n.: " +mre);
-		
+
+		System.out.println("trans props n.: " + translated.keySet().size());
+		System.out.println("written props n.: " + counter);
+		System.out.println("exceding keywords n.: " + mre);
+
 		try {
 			// scrivo il prop destinazione su FileSystem
 			System.out.println(locale);
-			File file = new File("src/main/resources/i18n/application_" +locale +".properties");
+			File file = new File("src/main/resources/"+transalatedBundle+"_" + locale + ".properties");
 			FileOutputStream out = new FileOutputStream(file);
 			destprop.store(out, "comments");
 			out.flush();
@@ -94,4 +113,16 @@ public class PurifyRB {
 		}
 	}
 
+	public static Locale toLocale(String str) {
+		String lang = "";
+		String country = "";
+		String variant = "";
+		StringTokenizer st = new StringTokenizer(str, "_", false);
+		lang = st.nextToken();
+		if (st.hasMoreTokens())
+			country = st.nextToken();
+		if (st.hasMoreTokens())
+			variant = st.nextToken();
+		return new Locale(lang, country, variant);
+	}
 }
