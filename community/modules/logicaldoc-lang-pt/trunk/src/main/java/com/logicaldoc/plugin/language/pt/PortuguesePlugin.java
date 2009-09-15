f100b280dbbffa88dda111e8af72d19c516934ce
blucecio <blucecio@bae09422-6297-422f-b3ee-419521344c47>
2009-09-15 08:04:29 +0000

    Upgraded PortuguesePlugin.java for LogicalDOC 4.5.x

--- community/modules/logicaldoc-lang-pt/trunk/src/main/java/com/logicaldoc/plugin/language/pt/PortuguesePlugin.java
+++ community/modules/logicaldoc-lang-pt/trunk/src/main/java/com/logicaldoc/plugin/language/pt/PortuguesePlugin.java
@@ -1,24 +1,19 @@
 package com.logicaldoc.plugin.language.pt;
 
 import java.io.File;
-import java.util.Locale;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.logicaldoc.core.i18n.Language;
-import com.logicaldoc.core.searchengine.Indexer;
 import com.logicaldoc.util.config.FacesConfigurator;
-import com.logicaldoc.util.config.PropertiesBean;
 import com.logicaldoc.util.plugin.LogicalDOCPlugin;
 
 /**
- * Web module plugin class. 
+ * Language module plugin class. 
  * 
  * @author Alessandro Gasparini
- * @version $Id$
- * @since ###release###
+ * @since 4.0.1
  */
 public class PortuguesePlugin extends LogicalDOCPlugin {
 
@@ -39,45 +34,11 @@ public class PortuguesePlugin extends LogicalDOCPlugin {
 
 		log.debug("Copy resources from " + src.getPath() + " to " + destClasses.getPath());
 		FileUtils.copyDirectory(src, destClasses);
-		
-		// Create Lucene Index
-		createLuceneIndex();
 
 		// Now add the message bundle
 		log.info("Add Portuguese (pt) language to faces-config.xml");
 		FacesConfigurator facesConfig = new FacesConfigurator();
 		facesConfig.addLanguageToFacesConfig("pt");
-	}
-
-	private void createLuceneIndex() throws Exception {
-		try {
-			Class.forName("com.logicaldoc.core.i18n.Language");
-		} catch (ClassNotFoundException ex) {
-			// The core plugin was not ready, so avoid initializations
-			return;
-		}
-		
-		log.info("Create Lucene Index");
-		PropertiesBean pbean = new PropertiesBean(getClass().getClassLoader().getResource("context.properties"));
-		String indexdir = pbean.getProperty("conf.indexdir");
-		log.info("indexdir = '" + indexdir + "'");
-		if (indexdir == null || indexdir.equals("")) {
-			throw new Exception("System un-setted up, impossible to create Lucene Index");
-		}
-		
-		try {
-			Language ldLanguage = new Language(new Locale("pt"));
-			File indexPath = new File(indexdir, ldLanguage.getIndex());
-			
-			// Prevent overwrite of an already present index
-			if (indexPath.exists())
-				return;
-			
-			Indexer.createIndex(indexPath, "pt");
-		} catch (Exception e) {
-			log.error("Unable to: createLuceneIndex(): " + e.getMessage(), e);
-			throw e;
-		}
 	}
 	
 }
