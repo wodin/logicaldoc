b42c996ff5f8c0eb6e7ea584bb1baeb46e1ce4e6
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2009-10-03 05:03:06 +0000

    Added logicaldoc-lang-ro module

--- community/modules/checkout.sh
+++ community/modules/checkout.sh
@@ -4,3 +4,4 @@ svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community
 svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/modules/logicaldoc-lang-el/trunk logicaldoc-lang-el
 svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/modules/logicaldoc-lang-pt_br/trunk logicaldoc-lang-pt_br
 svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/modules/logicaldoc-lang-zh/trunk logicaldoc-lang-zh
+svn checkout https://logicaldoc.svn.sourceforge.net/svnroot/logicaldoc/community/modules/logicaldoc-lang-ro/trunk logicaldoc-lang-ro
