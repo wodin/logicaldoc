85395a05dcce4dc0b80843cbce4171e089e7427f
mcaruso <mcaruso@bae09422-6297-422f-b3ee-419521344c47>
2009-10-30 16:35:46 +0000

    Modified the System Message settings. Added the Recipient management.

--- community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/communication/dao/SystemMessageDAO.java
+++ community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/communication/dao/SystemMessageDAO.java
@@ -14,14 +14,62 @@ import com.logicaldoc.core.communication.SystemMessage;
  */
 public interface SystemMessageDAO extends PersistentObjectDAO<SystemMessage> {
 
-	public List<SystemMessage> findByRecipient(String recipient);
-
-	public int getCount(String recipient);
+	/**
+	 * This method selects all the messages for the specified recipient and type
+	 * 
+	 * @param recipient The recipient name
+	 * @param type The message type
+	 * @return The messages list
+	 */
+	public List<SystemMessage> findByRecipient(String recipient, int type);
 
 	/**
-	 * Removes all expired messages for the specified recipient
+	 * This methods gets the number of messages for the specified recipient and
+	 * type
+	 * 
+	 * @param recipient The recipient name
+	 * @param type The message type
+	 * @return The number of messages
+	 */
+	public int getCount(String recipient, int type);
+
+	/**
+	 * Removes all system expired messages for the specified recipient
 	 * 
 	 * @param recipient The recipient
 	 */
 	public void deleteExpiredMessages(String recipient);
+
+	/**
+	 * Removes all expired messages for the specified type
+	 * 
+	 * @param type The message type
+	 */
+	public void deleteExpiredMessages(int type);
+
+	/**
+	 * This method selects all the messages for the specified type
+	 * 
+	 * @param recipient The recipient name
+	 * @return The list of messages with the given type
+	 */
+	public List<SystemMessage> findByType(int type);
+
+	/**
+	 * This method selects all the messages for the specified mode
+	 * 
+	 * @param mode The message mode
+	 * @return The list of messages with the given mode
+	 */
+	public List<SystemMessage> findByMode(String mode);
+
+	/**
+	 * This method selects all the messages for the specified type that are not
+	 * been already sent and for which the number of sending trials is less than
+	 * the maximum number (parameter 'notifier.maxtrials')
+	 * 
+	 * @param type The message type
+	 * @param maxTrials The maximum number of sending trials
+	 */
+	public List<SystemMessage> findMessagesToBeSent(int type, int maxTrials);
 }
