ced80c9c2f0a30cd7d5d2e2bd94274f1c48625df
mcaruso <mcaruso@bae09422-6297-422f-b3ee-419521344c47>
2009-11-10 15:10:19 +0000

    Fixed Fork and Join component definitions

--- community/modules/logicaldoc-workflow/trunk/src/main/java/com/logicaldoc/workflow/editor/model/BaseWorkflowModel.java
+++ community/modules/logicaldoc-workflow/trunk/src/main/java/com/logicaldoc/workflow/editor/model/BaseWorkflowModel.java
@@ -8,35 +8,32 @@ import java.util.UUID;
 import com.logicaldoc.workflow.editor.controll.EditController;
 import com.logicaldoc.workflow.editor.message.DeployMessage;
 
-public abstract class BaseWorkflowModel implements Serializable{
+public abstract class BaseWorkflowModel implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	private String id;
 
 	private String name;
-	
+
 	private List<Transition> transitions = new LinkedList<Transition>();
-	
+
 	public List<Transition> getTransitions() {
 		return transitions;
 	}
-	
+
 	public void setTransitions(List<Transition> transitions) {
 		this.transitions = transitions;
 	}
-	
+
 	public BaseWorkflowModel() {
 		id = UUID.randomUUID().toString();
 	}
 
-	public BaseWorkflowModel(BaseWorkflowModel workflowModel){
+	public BaseWorkflowModel(BaseWorkflowModel workflowModel) {
 		this.copy(workflowModel);
 	}
-	
+
 	public void setId(String id) {
 		this.id = id;
 	}
@@ -44,30 +41,31 @@ public abstract class BaseWorkflowModel implements Serializable{
 	public String getId() {
 		return id;
 	}
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public abstract String getType();
-	
+
 	public abstract String getImage();
 
 	public abstract String getTemplate();
-	
+
 	public abstract EditController getController();
-	
+
 	public abstract boolean isPossibleStartState();
-	
+
 	public abstract void checkForDeploy(List<DeployMessage> failures);
-	
+
 	public BaseWorkflowModel copy(BaseWorkflowModel baseWorkflowModel) {
 		this.id = baseWorkflowModel.id;
 		this.name = baseWorkflowModel.name;
-		
+
 		return this;
 	}
 
