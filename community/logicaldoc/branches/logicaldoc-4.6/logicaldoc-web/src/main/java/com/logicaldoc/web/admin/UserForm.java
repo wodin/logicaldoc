4d51db59cf546502ac8a1e8bdbd4b27aa3b6de37
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2009-11-12 16:02:03 +0000

    Fixed bug 149

--- community/logicaldoc/branches/logicaldoc-4.6/logicaldoc-web/src/main/java/com/logicaldoc/web/admin/UserForm.java
+++ community/logicaldoc/branches/logicaldoc-4.6/logicaldoc-web/src/main/java/com/logicaldoc/web/admin/UserForm.java
@@ -328,6 +328,7 @@ public class UserForm {
 	}
 
 	private String save(boolean withPassword) {
+		String decodedPassword=getPassword();
 		if (SessionManagement.isValid()) {
 			try {
 				UserDAO dao = (UserDAO) Context.getInstance().getBean(UserDAO.class);
@@ -355,7 +356,7 @@ public class UserForm {
 						user.setPasswordChanged(new Date());
 
 						// Notify the user by email
-						notifyAccount(user, getPassword());
+						notifyAccount(user, decodedPassword);
 					}
 
 					user.setRepass("");
@@ -365,10 +366,9 @@ public class UserForm {
 				manager.removeUserFromAllGroups(user);
 
 				if (createNew) {
-					
 					// Generate an initial password
-					String password = new PasswordGenerator().generate(getConfig().getInt("password.size"));
-					user.setDecodedPassword(password);
+					decodedPassword = new PasswordGenerator().generate(getConfig().getInt("password.size"));
+					user.setDecodedPassword(decodedPassword);
 					dao.store(user);
 				}
 
@@ -397,10 +397,10 @@ public class UserForm {
 					Messages.addLocalizedError("errors.action.saveuser.notstored");
 				} else {
 					Messages.addLocalizedInfo("msg.action.changeuser");
-					
+
 					// Notify the user by email
 					if (createNew)
-						notifyAccount(user, password);
+						notifyAccount(user, decodedPassword);
 				}
 
 				UsersRecordsManager recordsManager = ((UsersRecordsManager) FacesUtil.accessBeanFromFacesContext(
