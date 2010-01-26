ea43cf889f2e7ec743c5a7318876b6b814a66a37
mcaruso <mcaruso@bae09422-6297-422f-b3ee-419521344c47>
2010-01-26 12:20:06 +0000

    Fixed bug on workflow supervisor management.

--- community/modules/logicaldoc-workflow/trunk/src/main/java/com/logicaldoc/workflow/action/DefaultAssignmentHandler.java
+++ community/modules/logicaldoc-workflow/trunk/src/main/java/com/logicaldoc/workflow/action/DefaultAssignmentHandler.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
@@ -20,12 +19,9 @@ import com.logicaldoc.core.communication.SystemMessage;
 import com.logicaldoc.core.communication.dao.SystemMessageDAO;
 import com.logicaldoc.core.document.Document;
 import com.logicaldoc.core.document.dao.DocumentDAO;
-import com.logicaldoc.core.security.Group;
 import com.logicaldoc.core.security.User;
-import com.logicaldoc.core.security.dao.GroupDAO;
 import com.logicaldoc.core.security.dao.UserDAO;
 import com.logicaldoc.util.Context;
-import com.logicaldoc.web.i18n.Messages;
 import com.logicaldoc.workflow.TemplateService;
 import com.logicaldoc.workflow.WorkflowConstants;
 import com.logicaldoc.workflow.WorkflowFactory;
@@ -115,55 +111,7 @@ public class DefaultAssignmentHandler extends AbstractAssignmentHandler {
 			message.setRecipients(recipients);
 
 			systemMessageDAO.store(message);
-
-			notifySupervisor(workflowTemplate, assignmentText, subject);
 		}
-	}
-
-	private void notifySupervisor(WorkflowTemplate workflowTemplate, String assignmentText, String subject) {
-		UserDAO userDAO = (UserDAO) Context.getInstance().getBean(UserDAO.class);
-		SystemMessageDAO systemMessageDAO = (SystemMessageDAO) Context.getInstance().getBean(SystemMessageDAO.class);
-
-		// Add the supervisor/s
-		Recipient supervisorRecipient = null;
-		Set<Recipient> supervisorRecipients = new HashSet<Recipient>();
-		// Checks if the supervisor is a user
-		User supervisorUser = userDAO.findByUserName(workflowTemplate.getSupervisor());
-		Locale supervisorLocale = Locale.getDefault();
-		if (supervisorUser != null) {
-			supervisorRecipient = new Recipient();
-			supervisorRecipient.setName(supervisorUser.getUserName());
-			supervisorRecipient.setAddress(supervisorUser.getUserName());
-			supervisorRecipient.setType(SystemMessage.TYPE_SYSTEM);
-			supervisorRecipients.add(supervisorRecipient);
-			supervisorLocale = supervisorUser.getLocale();
-		} else {
-			// Checks if the supervisor is a group
-			GroupDAO groupDAO = (GroupDAO) Context.getInstance().getBean(GroupDAO.class);
-			Group supervisorGroup = groupDAO.findByName(workflowTemplate.getSupervisor());
-			if (supervisorGroup != null) {
-				for (User supervisorGroupUser : supervisorGroup.getUsers()) {
-					supervisorRecipient = new Recipient();
-					supervisorRecipient.setName(supervisorGroupUser.getUserName());
-					supervisorRecipient.setAddress(supervisorGroupUser.getUserName());
-					supervisorRecipient.setType(SystemMessage.TYPE_SYSTEM);
-					supervisorRecipients.add(supervisorRecipient);
-				}
-			}
-		}
-
-		SystemMessage supervisorMessage = new SystemMessage();
-		supervisorMessage.setAuthor("[BPM] - " + workflowTemplate.getName());
-		StringBuffer messageText = new StringBuffer();
-		messageText.append(Messages.getMessage("workflow.editor_settings_supervisors_message", supervisorLocale,
-				new Object[] { workflowTemplate.getName() }));
-		messageText.append("\n");
-		supervisorMessage.setMessageText(messageText.toString() + assignmentText);
-		supervisorMessage.setSentDate(new Date());
-		supervisorMessage.setSubject(subject);
-		supervisorMessage.setRecipients(supervisorRecipients);
-
-		systemMessageDAO.store(supervisorMessage);
 	}
 
 	private void preparteTemplateModelWithRecipient(User user, Map<String, Object> modelProperties) {
