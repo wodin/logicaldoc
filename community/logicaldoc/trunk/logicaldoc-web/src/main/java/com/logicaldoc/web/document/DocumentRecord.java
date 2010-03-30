1f8fcf42453381a83982d7a248fcd9806a15bb02
mcaruso <mcaruso@bae09422-6297-422f-b3ee-419521344c47>
2010-03-30 09:42:06 +0000

    Updated the maximum length of the document diplay name.

--- community/logicaldoc/trunk/logicaldoc-web/src/main/java/com/logicaldoc/web/document/DocumentRecord.java
+++ community/logicaldoc/trunk/logicaldoc-web/src/main/java/com/logicaldoc/web/document/DocumentRecord.java
@@ -124,11 +124,11 @@ public class DocumentRecord extends MenuBarBean {
 	}
 
 	public String getDisplayTitle() {
-		return StringUtils.abbreviate(getDocument().getTitle(), 68);
+		return StringUtils.abbreviate(getDocument().getTitle(), 58);
 	}
 
 	public String getDisplayFilename() {
-		return StringUtils.abbreviate(getDocument().getFileName(), 68);
+		return StringUtils.abbreviate(getDocument().getFileName(), 58);
 	}
 
 	/**
