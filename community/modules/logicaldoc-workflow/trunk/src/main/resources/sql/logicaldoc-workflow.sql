8bd5239ffcd5da116dd3e7d3c22f4a7e3079f72b
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2010-05-16 07:46:10 +0000

    Partial Dashboard implementation

--- community/modules/logicaldoc-workflow/trunk/src/main/resources/sql/logicaldoc-workflow.sql
+++ community/modules/logicaldoc-workflow/trunk/src/main/resources/sql/logicaldoc-workflow.sql
@@ -1,5 +1,5 @@
 create table ld_workflowtemplate (ld_id bigint not null, ld_lastmodified timestamp not null, ld_deleted int not null, ld_name varchar(255) not null, ld_description varchar(255), ld_deployed int, ld_startstate varchar(40), primary key (ld_id));
-create table ld_workflowhistory (ld_id bigint not null, ld_lastmodified timestamp not null, ld_deleted int not null, ld_date timestamp, ld_event varchar(255), ld_comment varchar(4000), ld_docid bigint, ld_userid bigint, ld_username varchar(255), ld_templateid bigint not null, ld_instanceid varchar(255), ld_sessionid varchar(255), primary key (ld_id));
+create table ld_workflowhistory (ld_id bigint not null, ld_lastmodified timestamp not null, ld_deleted int not null, ld_date timestamp, ld_event varchar(255), ld_comment varchar(4000), ld_docid bigint, ld_userid bigint, ld_username varchar(255), ld_templateid bigint not null, ld_instanceid varchar(255), ld_sessionid varchar(255), ld_new int, ld_filename varchar(255), primary key (ld_id));
 
 insert into ld_menu (ld_id,ld_lastmodified,ld_deleted,ld_text,ld_parentid,ld_sort,ld_icon,ld_type,ld_ref,ld_size)
 values     (-10,CURRENT_TIMESTAMP,0,'workflow',2,4,'workflow.png',1,'workflow/manage-workflowtemplates',0);
