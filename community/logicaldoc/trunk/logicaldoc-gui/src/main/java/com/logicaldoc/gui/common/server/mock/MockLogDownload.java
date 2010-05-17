0d407ba57bcc5beae37e810caf4d0ac6f5b08c50
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2010-05-17 16:43:29 +0000

    Added Log page

--- community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/server/mock/MockLogDownload.java
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/server/mock/MockLogDownload.java
@@ -1,18 +1,34 @@
 package com.logicaldoc.gui.common.server.mock;
 
 import java.io.IOException;
+import java.io.InputStream;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MockServlet extends HttpServlet {
-	
+public class MockLogDownload extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().print("Feature not available");
+		String sid = request.getParameter("sid");
+		if (sid == null)
+			throw new IOException("Invalid session");
+		try {
+			InputStream is = this.getClass().getResourceAsStream("/dms.log.html");
+			byte[] buf = new byte[1024];
+			int read = 1;
+			while (read > 0) {
+				read = is.read(buf, 0, buf.length);
+				if (read > 0)
+					response.getOutputStream().write(buf, 0, read);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/system/LogPanel.java
@@ -0,0 +1,50 @@
+package com.logicaldoc.gui.frontend.client.system;
+
+import com.google.gwt.core.client.GWT;
+import com.logicaldoc.gui.common.client.I18N;
+import com.logicaldoc.gui.common.client.Session;
+import com.smartgwt.client.types.ContentsType;
+import com.smartgwt.client.widgets.HTMLPane;
+import com.smartgwt.client.widgets.IButton;
+import com.smartgwt.client.widgets.events.ClickEvent;
+import com.smartgwt.client.widgets.events.ClickHandler;
+import com.smartgwt.client.widgets.layout.HStack;
+import com.smartgwt.client.widgets.layout.VLayout;
+
+/**
+ * Main log panel
+ * 
+ * @author Marco Meschieri - Logical Objects
+ * @since 6.0
+ */
+public class LogPanel extends VLayout {
+	public LogPanel() {
+		setHeight100();
+
+		final HTMLPane htmlPane = new HTMLPane();
+		htmlPane.setWidth100();
+		htmlPane.setHeight100();
+		htmlPane.setShowEdges(true);
+		htmlPane.setContentsURL(GWT.getHostPageBaseURL() + "log?sid=" + Session.get().getSid() + "&appender=DMS");
+		htmlPane.setContentsType(ContentsType.PAGE);
+
+		HStack hStack = new HStack();
+		hStack.setHeight(50);
+		hStack.setLayoutMargin(10);
+		hStack.setMembersMargin(10);
+
+		IButton refresh = new IButton(I18N.getMessage("refresh"));
+		refresh.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				htmlPane.redraw();
+				htmlPane.setWidth100();
+				htmlPane.setHeight100();
+			}
+		});
+		hStack.addMember(refresh);
+
+		addMember(hStack);
+		addMember(htmlPane);
+		draw();
+	}
+}
