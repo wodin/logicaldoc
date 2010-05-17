c3955d1f84a80e267a3194ee1db00e7357863a48
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2010-05-17 16:43:29 +0000

    Added Log page

--- community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/system/SystemMenu.java
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/system/SystemMenu.java
@@ -1,7 +1,12 @@
 package com.logicaldoc.gui.frontend.client.system;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.logicaldoc.gui.common.client.I18N;
+import com.logicaldoc.gui.common.client.Session;
+import com.logicaldoc.gui.common.client.beans.GUISecuritySettings;
+import com.logicaldoc.gui.common.client.log.Log;
 import com.logicaldoc.gui.frontend.client.administration.AdminPanel;
+import com.logicaldoc.gui.frontend.client.security.SecuritySettingsPanel;
 import com.smartgwt.client.widgets.Button;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
@@ -47,24 +52,12 @@ public class SystemMenu extends VLayout {
 		// }
 		// });
 		//
-		// log.addClickHandler(new ClickHandler() {
-		// @Override
-		// public void onClick(ClickEvent event) {
-		// service.loadSettings(Session.get().getSid(), new
-		// AsyncCallback<GUISecuritySettings>() {
-		//
-		// @Override
-		// public void onFailure(Throwable caught) {
-		// Log.serverError(caught);
-		// }
-		//
-		// @Override
-		// public void onSuccess(GUISecuritySettings settings) {
-		// AdminPanel.get().setContent(new SecuritySettingsPanel(settings));
-		// }
-		//
-		// });
-		// }
-		// });
+
+		log.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				AdminPanel.get().setContent(new LogPanel());
+			}
+		});
 	}
 }
