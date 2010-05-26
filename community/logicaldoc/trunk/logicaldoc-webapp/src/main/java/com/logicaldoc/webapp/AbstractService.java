cee19765e98766e2c193bde5d70f7d1ae0fc8782
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2010-05-26 19:51:09 +0000

    Partial Security Service implementation

---
+++ community/logicaldoc/trunk/logicaldoc-webapp/src/main/java/com/logicaldoc/webapp/AbstractService.java
@@ -0,0 +1,26 @@
+package com.logicaldoc.webapp;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.logicaldoc.core.security.SessionManager;
+import com.logicaldoc.core.security.UserSession;
+import com.logicaldoc.gui.common.client.I18N;
+
+/**
+ * Base class for services implementation
+ * 
+ * @author Marco Meschieri - Logical Objects
+ * @since 6.0
+ */
+public abstract class AbstractService extends RemoteServiceServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 *  Throws a runtime exception id the given session is invalid
+	 */
+	protected void validateSession(String sid) {
+		UserSession session = SessionManager.getInstance().get(sid);
+		if (session == null)
+			throw new RuntimeException(I18N.getMessage("invalidsession"));
+	}
+}
--- community/logicaldoc/trunk/logicaldoc-webapp/src/main/java/com/logicaldoc/webapp/security/SecurityServiceImpl.java
+++ community/logicaldoc/trunk/logicaldoc-webapp/src/main/java/com/logicaldoc/webapp/security/SecurityServiceImpl.java
@@ -7,22 +7,26 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.logicaldoc.core.security.Group;
+import com.logicaldoc.core.security.SecurityManager;
 import com.logicaldoc.core.security.SessionManager;
 import com.logicaldoc.core.security.User;
 import com.logicaldoc.core.security.UserHistory;
 import com.logicaldoc.core.security.UserSession;
 import com.logicaldoc.core.security.authentication.AuthenticationChain;
+import com.logicaldoc.core.security.dao.GroupDAO;
 import com.logicaldoc.core.security.dao.UserDAO;
+import com.logicaldoc.gui.common.client.beans.GUIADSettings;
 import com.logicaldoc.gui.common.client.beans.GUIGroup;
-import com.logicaldoc.gui.common.client.beans.GUIRight;
+import com.logicaldoc.gui.common.client.beans.GUILdapSettings;
+import com.logicaldoc.gui.common.client.beans.GUISecuritySettings;
 import com.logicaldoc.gui.common.client.beans.GUISession;
 import com.logicaldoc.gui.common.client.beans.GUIUser;
 import com.logicaldoc.gui.frontend.client.services.SecurityService;
 import com.logicaldoc.util.Context;
 import com.logicaldoc.util.config.SettingsConfig;
 import com.logicaldoc.util.io.CryptUtil;
+import com.logicaldoc.webapp.AbstractService;
 
 /**
  * Implementation of the SecurityService
@@ -30,16 +34,11 @@ import com.logicaldoc.util.io.CryptUtil;
  * @author Marco Meschieri - Logical Objects
  * @since 6.0
  */
-public class SecurityServiceImpl extends RemoteServiceServlet implements SecurityService {
+public class SecurityServiceImpl extends AbstractService implements SecurityService {
 
 	private static final long serialVersionUID = 1L;
 
 	private static Log log = LogFactory.getLog(SecurityServiceImpl.class);
-
-	@Override
-	public GUIRight[] getSecurityEntities(String sid) {
-		return null;
-	}
 
 	@Override
 	public GUISession login(String username, String password) {
@@ -147,49 +146,178 @@ public class SecurityServiceImpl extends RemoteServiceServlet implements Securit
 
 	@Override
 	public void addUserToGroup(String sid, long groupId, long userId) {
-		// TODO Auto-generated method stub
+		validateSession(sid);
 
+		SecurityManager manager = (SecurityManager) Context.getInstance().getBean(SecurityManager.class);
+		UserDAO userDao = (UserDAO) Context.getInstance().getBean(UserDAO.class);
+		GroupDAO groupDao = (GroupDAO) Context.getInstance().getBean(GroupDAO.class);
+		manager.assignUserToGroup(userDao.findById(userId), groupDao.findById(groupId));
 	}
 
 	@Override
 	public void deleteGroup(String sid, long groupId) {
-		// TODO Auto-generated method stub
-
+		validateSession(sid);
+		GroupDAO groupDao = (GroupDAO) Context.getInstance().getBean(GroupDAO.class);
+		groupDao.delete(groupId);
 	}
 
 	@Override
 	public void deleteUser(String sid, long userId) {
-		// TODO Auto-generated method stub
-
+		validateSession(sid);
+		UserDAO userDao = (UserDAO) Context.getInstance().getBean(UserDAO.class);
+		userDao.delete(userId);
 	}
 
 	@Override
 	public GUIGroup getGroup(String sid, long groupId) {
-		// TODO Auto-generated method stub
-		return null;
+		validateSession(sid);
+		GroupDAO groupDao = (GroupDAO) Context.getInstance().getBean(GroupDAO.class);
+		Group group = groupDao.findById(groupId);
+
+		GUIGroup grp = new GUIGroup();
+		grp.setId(groupId);
+		grp.setDescription(group.getDescription());
+		grp.setName(group.getName());
+		return grp;
 	}
 
 	@Override
 	public GUIUser getUser(String sid, long userId) {
-		// TODO Auto-generated method stub
-		return null;
+		validateSession(sid);
+		UserDAO userDao = (UserDAO) Context.getInstance().getBean(UserDAO.class);
+		User user = userDao.findById(userId);
+		userDao.initialize(user);
+
+		GUIUser usr = new GUIUser();
+		usr.setId(userId);
+		usr.setAddress(user.getStreet());
+		usr.setCell(user.getTelephone2());
+		usr.setPhone(user.getTelephone());
+		usr.setCity(user.getCity());
+		usr.setCountry(user.getCountry());
+		usr.setEmail(user.getEmail());
+		usr.setEnabled(user.getEnabled() == 1);
+		usr.setFirstName(user.getFirstName());
+		usr.setLanguage(user.getLanguage());
+		usr.setName(user.getName());
+		usr.setPostalCode(user.getPostalcode());
+		usr.setState(user.getState());
+		usr.setUserName(user.getUserName());
+
+		GUIGroup[] grps = new GUIGroup[user.getGroups().size()];
+
+		int i = 0;
+		for (Group group : user.getGroups()) {
+			grps[i] = new GUIGroup();
+			grps[i].setId(group.getId());
+			grps[i].setName(group.getName());
+			grps[i].setDescription(group.getDescription());
+			grps[i].setName(group.getName());
+			i++;
+		}
+
+		return usr;
 	}
 
 	@Override
-	public void removeFromGroup(String sid, long groupId, long[] docIds) {
-		// TODO Auto-generated method stub
+	public void removeFromGroup(String sid, long groupId, long[] userIds) {
+		validateSession(sid);
 
+		SecurityManager manager = (SecurityManager) Context.getInstance().getBean(SecurityManager.class);
+		UserDAO userDao = (UserDAO) Context.getInstance().getBean(UserDAO.class);
+		GroupDAO groupDao = (GroupDAO) Context.getInstance().getBean(GroupDAO.class);
+		Group group = groupDao.findById(groupId);
+		for (long id : userIds) {
+			User user = userDao.findById(id);
+			manager.removeUserFromGroup(user, group);
+		}
 	}
 
 	@Override
 	public GUIGroup saveGroup(String sid, GUIGroup group) {
+		validateSession(sid);
+
+		GroupDAO groupDao = (GroupDAO) Context.getInstance().getBean(GroupDAO.class);
+		Group grp;
+		if (group.getId() != 0) {
+			grp = groupDao.findById(group.getId());
+			groupDao.initialize(grp);
+		} else
+			grp = new Group();
+
+		grp.setName(group.getName());
+		grp.setDescription(group.getDescription());
+		groupDao.store(grp);
+
+		group.setId(grp.getId());
+		return group;
+	}
+
+	@Override
+	public GUIUser saveUser(String sid, GUIUser user) {
+		validateSession(sid);
+
+		UserDAO userDao = (UserDAO) Context.getInstance().getBean(UserDAO.class);
+		User usr;
+		if (user.getId() != 0) {
+			usr = userDao.findById(user.getId());
+			userDao.initialize(usr);
+		} else
+			usr = new User();
+
+		usr.setCity(user.getCity());
+		usr.setCountry(user.getCountry());
+		usr.setEmail(user.getEmail());
+		usr.setFirstName(user.getFirstName());
+		usr.setName(user.getName());
+		usr.setLanguage(user.getLanguage());
+		usr.setPostalcode(user.getPostalCode());
+		usr.setState(user.getState());
+		usr.setStreet(user.getAddress());
+		usr.setTelephone(user.getPhone());
+		usr.setTelephone2(user.getCell());
+		usr.setUserName(user.getUserName());
+
+		userDao.store(usr);
+		user.setId(usr.getId());
+
+		SecurityManager manager = (SecurityManager) Context.getInstance().getBean(SecurityManager.class);
+		GroupDAO groupDao = (GroupDAO) Context.getInstance().getBean(GroupDAO.class);
+		manager.removeUserFromAllGroups(usr);
+		for (GUIGroup grp : user.getGroups()) {
+			Group g = groupDao.findById(grp.getId());
+			manager.assignUserToGroup(usr, g);
+		}
+
+		return user;
+	}
+
+	@Override
+	public void kill(String sid) {
+		SessionManager.getInstance().kill(sid);
+	}
+
+	@Override
+	public GUILdapSettings[] loadExtAuthSettings(String sid) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public GUIUser saveUser(String sid, GUIUser user) {
+	public GUISecuritySettings loadSettings(String sid) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public void saveExtAuthSettings(String sid, GUILdapSettings ldapSettings, GUIADSettings adSettings) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void saveSettings(String sid, GUISecuritySettings settings) {
+		// TODO Auto-generated method stub
+
 	}
 }
