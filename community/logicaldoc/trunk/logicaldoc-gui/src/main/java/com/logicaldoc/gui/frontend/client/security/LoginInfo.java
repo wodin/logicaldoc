358ec82274351669149037af74b0d2670ee9b2a5
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2010-06-17 19:47:47 +0000

    Application main menu

--- community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/security/LoginInfo.java
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/security/LoginInfo.java
@@ -1,18 +1,8 @@
 package com.logicaldoc.gui.frontend.client.security;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.logicaldoc.gui.common.client.Session;
-import com.logicaldoc.gui.common.client.i18n.I18N;
-import com.logicaldoc.gui.common.client.log.Log;
-import com.logicaldoc.gui.common.client.util.Util;
-import com.logicaldoc.gui.frontend.client.services.SecurityService;
-import com.logicaldoc.gui.frontend.client.services.SecurityServiceAsync;
 import com.smartgwt.client.types.Alignment;
-import com.smartgwt.client.types.Cursor;
 import com.smartgwt.client.widgets.Label;
-import com.smartgwt.client.widgets.events.ClickEvent;
-import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 /**
@@ -22,8 +12,6 @@ import com.smartgwt.client.widgets.layout.VLayout;
  * @since 6.0
  */
 public class LoginInfo extends VLayout {
-	private SecurityServiceAsync securityService = (SecurityServiceAsync) GWT.create(SecurityService.class);
-
 	public LoginInfo(String stylePrefix) {
 		setHeight(30);
 		setAlign(Alignment.RIGHT);
@@ -34,35 +22,7 @@ public class LoginInfo extends VLayout {
 		user.setMargin(2);
 		user.setStyleName(stylePrefix + "loginInfo");
 
-		Label logout = new Label(I18N.message("logout"));
-		logout.setHeight(10);
-		logout.setAlign(Alignment.RIGHT);
-		logout.setStyleName(stylePrefix + "logout");
-		logout.setCursor(Cursor.HAND);
-		logout.setMargin(2);
-		logout.addClickHandler(new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				securityService.logout(Session.get().getSid(), new AsyncCallback<Void>() {
-					public void onFailure(Throwable caught) {
-						Log.serverError(caught);
-					}
-
-					@Override
-					public void onSuccess(Void arg0) {
-						Session.get().close();
-						String base = GWT.getHostPageBaseURL();
-						Util.redirect(base
-								+ (base.endsWith("/") ? GWT.getModuleName() + ".jsp" : "/" + GWT.getModuleName()
-										+ ".jsp"));
-					}
-				});
-			}
-
-		});
-	
-
 		addMember(user);
-		addMember(logout);
+
 	}
 }
