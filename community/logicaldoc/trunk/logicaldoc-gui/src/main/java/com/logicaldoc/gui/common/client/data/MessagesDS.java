dc6929972f40b0d9ab9fa293c5bf814cc150bf0f
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2010-06-30 19:53:53 +0000

    Partial messages implementation

---
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/data/MessagesDS.java
@@ -0,0 +1,43 @@
+package com.logicaldoc.gui.common.client.data;
+
+import com.logicaldoc.gui.common.client.Session;
+import com.smartgwt.client.data.DataSource;
+import com.smartgwt.client.data.fields.DataSourceDateTimeField;
+import com.smartgwt.client.data.fields.DataSourceImageField;
+import com.smartgwt.client.data.fields.DataSourceIntegerField;
+import com.smartgwt.client.data.fields.DataSourceTextField;
+
+/**
+ * Datasource to retrieve the system messages the current user.
+ * 
+ * @author Marco Meschieri - Logical Objects
+ * @since 6.0
+ */
+public class MessagesDS extends DataSource {
+
+	private static MessagesDS instance;
+
+	public static MessagesDS get() {
+		if (instance == null)
+			instance = new MessagesDS();
+		return instance;
+	}
+
+	private MessagesDS() {
+		setTitleField("name");
+		setRecordXPath("/list/message");
+		DataSourceTextField id = new DataSourceTextField("id");
+		id.setPrimaryKey(true);
+		id.setRequired(true);
+
+		DataSourceTextField subject = new DataSourceTextField("subject");
+		DataSourceImageField priority = new DataSourceImageField("priority");
+		DataSourceTextField from = new DataSourceTextField("from");
+		DataSourceDateTimeField sent = new DataSourceDateTimeField("sent");
+		DataSourceIntegerField status = new DataSourceIntegerField("status");
+
+		setFields(id, subject, priority, from, sent, status);
+		setClientOnly(true);
+		setDataURL("data/messages.xml?sid=" + Session.get().getSid());
+	}
+}
