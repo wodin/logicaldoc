abccfece060b828866fc091bf63f2aebbe4d44b4
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2010-08-09 07:36:13 +0000

    partial digital sign porting

--- community/logicaldoc/trunk/logicaldoc-webapp/src/main/java/com/logicaldoc/web/data/DocumentsDataServlet.java
+++ community/logicaldoc/trunk/logicaldoc-webapp/src/main/java/com/logicaldoc/web/data/DocumentsDataServlet.java
@@ -139,7 +139,7 @@ public class DocumentsDataServlet extends HttpServlet {
 				writer.print("<signed>blank</signed>");
 			else if (Integer.parseInt(cols[17].toString()) == 1)
 				writer.print("<signed>sign</signed>");
-
+			
 			writer.print("</document>");
 		}
 
