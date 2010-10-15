f3ad1559a9e4c13c04bba99b1a2ace1381b5a834
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2010-10-15 19:27:04 +0000

    Porting to new 6.0 plugin system

--- community/modules/logicaldoc-lang-fa/trunk/src/main/java/com/logicaldoc/plugin/language/fa/ConvertCh.java
+++ community/modules/logicaldoc-lang-fa/trunk/src/main/java/com/logicaldoc/plugin/language/fa/ConvertCh.java
@@ -1,12 +1,10 @@
 package com.logicaldoc.plugin.language.fa;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 
 import org.apache.commons.io.IOUtils;
 
