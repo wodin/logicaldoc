0f2793b05e36e36699f9863012cbfbaede8f3fdc
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2010-10-15 16:10:49 +0000

    Porting to new 6.0 plugin system

--- community/modules/logicaldoc-lang-pt/trunk/src/main/java/com/logicaldoc/plugin/language/pt/PortuguesePlugin.java
+++ community/modules/logicaldoc-lang-pt/trunk/src/main/java/com/logicaldoc/plugin/language/pt/PortuguesePlugin.java
@@ -1,16 +1,12 @@
 package com.logicaldoc.plugin.language.pt;
 
-import java.io.File;
-
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.logicaldoc.util.config.FacesConfigurator;
 import com.logicaldoc.util.plugin.LogicalDOCPlugin;
 
 /**
- * Language module plugin class. 
+ * Language module plugin class.
  * 
  * @author Alessandro Gasparini
  * @since 4.0.1
@@ -21,24 +17,7 @@ public class PortuguesePlugin extends LogicalDOCPlugin {
 
 	@Override
 	protected void install() throws Exception {
-
-		String classesDir = getManager().getPathResolver().resolvePath(getDescriptor(), "classes").toString();
-		log.debug("classesDir: " + classesDir);
-		if (classesDir.startsWith("file:")) {
-			classesDir = classesDir.substring(5);
-		}
-
-		File src = new File(classesDir, "i18n");
-		File destRoot = new File(System.getProperty("logicaldoc.app.rootdir"));
-		File destClasses = new File(destRoot, "WEB-INF/classes/i18n");
-
-		log.debug("Copy resources from " + src.getPath() + " to " + destClasses.getPath());
-		FileUtils.copyDirectory(src, destClasses);
-
-		// Now add the message bundle
-		log.info("Add Portuguese (pt) language to faces-config.xml");
-		FacesConfigurator facesConfig = new FacesConfigurator();
-		facesConfig.addLanguageToFacesConfig("pt");
+		// Do nothing
 	}
-	
+
 }
