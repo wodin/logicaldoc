b22a3bfed7c3b55e3fdcdc71882be7b69458ff3d
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2010-10-16 11:44:23 +0000

    Fixed statistics screen layout

--- community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/system/SystemMenu.java
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/system/SystemMenu.java
@@ -7,12 +7,18 @@ import com.logicaldoc.gui.common.client.Session;
 import com.logicaldoc.gui.common.client.beans.GUISearchEngine;
 import com.logicaldoc.gui.common.client.i18n.I18N;
 import com.logicaldoc.gui.common.client.log.Log;
+import com.logicaldoc.gui.common.client.util.ItemFactory;
 import com.logicaldoc.gui.frontend.client.administration.AdminPanel;
 import com.logicaldoc.gui.frontend.client.services.SearchEngineService;
 import com.logicaldoc.gui.frontend.client.services.SearchEngineServiceAsync;
+import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.types.TitleOrientation;
 import com.smartgwt.client.widgets.Button;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
+import com.smartgwt.client.widgets.form.DynamicForm;
+import com.smartgwt.client.widgets.form.fields.LinkItem;
+import com.smartgwt.client.widgets.form.fields.StaticTextItem;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 /**
@@ -53,6 +59,8 @@ public class SystemMenu extends VLayout {
 		log.setWidth100();
 		log.setHeight(25);
 		addMember(log);
+		
+		addInformations();
 		
 		general.addClickHandler(new ClickHandler() {
 			@Override
@@ -100,5 +108,68 @@ public class SystemMenu extends VLayout {
 				});
 			}
 		});
+	}
+	
+	private void addInformations(){
+		DynamicForm systemForm = new DynamicForm();
+		systemForm.setWidth(300);
+		systemForm.setColWidths(1, "*");
+
+		StaticTextItem productName = ItemFactory.newStaticTextItem("productName", "", "<b>"
+				+ Session.get().getInfo().getProductName() + "</b>");
+		productName.setShouldSaveValue(false);
+		productName.setWrapTitle(false);
+
+		StaticTextItem version = ItemFactory.newStaticTextItem("version", "", I18N.message("version") + " "
+				+ Session.get().getInfo().getRelease());
+		version.setShouldSaveValue(false);
+
+		StaticTextItem vendor = ItemFactory.newStaticTextItem("vendor", "", "&copy; "
+				+ Session.get().getInfo().getVendor());
+		vendor.setShouldSaveValue(false);
+
+		StaticTextItem address = ItemFactory.newStaticTextItem("address", "", Session.get().getInfo()
+				.getVendorAddress());
+		address.setShouldSaveValue(false);
+
+		StaticTextItem capAndCity = ItemFactory.newStaticTextItem("capAndCity", "", Session.get().getInfo()
+				.getVendorCap()
+				+ "  " + Session.get().getInfo().getVendorCity());
+		capAndCity.setShouldSaveValue(false);
+
+		StaticTextItem country = ItemFactory.newStaticTextItem("country", "", Session.get().getInfo()
+				.getVendorCountry());
+		country.setShouldSaveValue(false);
+
+		DynamicForm supportForm = new DynamicForm();
+		supportForm.setAlign(Alignment.LEFT);
+		supportForm.setTitleOrientation(TitleOrientation.TOP);
+		supportForm.setColWidths(1);
+		supportForm.setWrapItemTitles(false);
+		supportForm.setMargin(8);
+		supportForm.setNumCols(1);
+
+		LinkItem support = new LinkItem();
+		support.setName(I18N.message("support"));
+		support.setLinkTitle(Session.get().getInfo().getSupport());
+		support.setValue("mailto:" + Session.get().getInfo().getSupport() + "?subject="
+				+ Session.get().getInfo().getProductName() + " Support - ID("
+				+ Session.get().getInfo().getInstallationId() + ")");
+		support.setRequired(true);
+		support.setShouldSaveValue(false);
+
+		StaticTextItem installationID = ItemFactory.newStaticTextItem("", "installid", Session.get().getInfo()
+				.getInstallationId());
+		installationID.setRequired(true);
+		installationID.setShouldSaveValue(false);
+		installationID.setWrap(false);
+		installationID.setWrapTitle(false);
+
+		systemForm.setItems(productName, version, vendor, address, capAndCity, country);
+
+		supportForm.setItems(support, installationID);
+		
+		addMember(systemForm);
+		addMember(supportForm);
 	}
 }
