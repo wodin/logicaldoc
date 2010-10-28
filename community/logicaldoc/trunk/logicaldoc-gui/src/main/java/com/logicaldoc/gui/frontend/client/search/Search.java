609864fc3901aa009909710fa57dc60dcbb3ed98
mcaruso <mcaruso@bae09422-6297-422f-b3ee-419521344c47>
2010-10-28 10:10:10 +0000

    Added 'Contacting Server' window.

--- community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/search/Search.java
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/search/Search.java
@@ -11,6 +11,7 @@ import com.logicaldoc.gui.common.client.beans.GUIResult;
 import com.logicaldoc.gui.common.client.beans.GUISearchOptions;
 import com.logicaldoc.gui.common.client.i18n.I18N;
 import com.logicaldoc.gui.common.client.log.Log;
+import com.logicaldoc.gui.common.client.widgets.ContactingServer;
 import com.logicaldoc.gui.frontend.client.services.SearchService;
 import com.logicaldoc.gui.frontend.client.services.SearchServiceAsync;
 import com.smartgwt.client.widgets.grid.ListGridRecord;
@@ -62,42 +63,47 @@ public class Search {
 	}
 
 	public void search() {
+		ContactingServer.get().show();
 		options.setExpressionLanguage(I18N.getLocale());
 		service.search(Session.get().getSid(), options, new AsyncCallback<GUIResult>() {
 
 			@Override
 			public void onFailure(Throwable caught) {
+				ContactingServer.get().hide();
 				Log.serverError(caught);
 			}
 
 			@Override
 			public void onSuccess(GUIResult result) {
-				time = result.getTime();
-				lastResult = new ListGridRecord[result.getHits().length];
-				hasMore = result.isHasMore();
-				for (int i = 0; i < result.getHits().length; i++) {
-					GUIHit hit = result.getHits()[i];
-					ListGridRecord record = new ListGridRecord();
-					lastResult[i] = record;
-					record.setAttribute("title", hit.getTitle());
-					record.setAttribute("id", hit.getId());
-					record.setAttribute("docRef", hit.getDocRef());
-					record.setAttribute("score", hit.getScore());
-					record.setAttribute("summary", hit.getSummary());
-					record.setAttribute("creation", hit.getCreation());
-					record.setAttribute("date", hit.getDate());
-					record.setAttribute("customId", hit.getCustomId());
-					record.setAttribute("folderId", hit.getFolderId());
-					record.setAttribute("size", hit.getSize());
-					record.setAttribute("icon", hit.getIcon());
-					record.setAttribute("type", hit.getType());
-				}
+				try {
+					time = result.getTime();
+					lastResult = new ListGridRecord[result.getHits().length];
+					hasMore = result.isHasMore();
+					for (int i = 0; i < result.getHits().length; i++) {
+						GUIHit hit = result.getHits()[i];
+						ListGridRecord record = new ListGridRecord();
+						lastResult[i] = record;
+						record.setAttribute("title", hit.getTitle());
+						record.setAttribute("id", hit.getId());
+						record.setAttribute("docRef", hit.getDocRef());
+						record.setAttribute("score", hit.getScore());
+						record.setAttribute("summary", hit.getSummary());
+						record.setAttribute("creation", hit.getCreation());
+						record.setAttribute("date", hit.getDate());
+						record.setAttribute("customId", hit.getCustomId());
+						record.setAttribute("folderId", hit.getFolderId());
+						record.setAttribute("size", hit.getSize());
+						record.setAttribute("icon", hit.getIcon());
+						record.setAttribute("type", hit.getType());
+					}
 
-				for (SearchObserver observer : observers) {
-					observer.onSearchArrived();
+					for (SearchObserver observer : observers) {
+						observer.onSearchArrived();
+					}
+				} finally {
+					ContactingServer.get().hide();
 				}
 			}
-
 		});
 	}
 
