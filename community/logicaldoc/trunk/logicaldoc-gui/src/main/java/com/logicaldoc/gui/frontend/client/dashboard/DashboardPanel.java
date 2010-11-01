0b2604ed073ffa86c5b9568aa966bceaac4ab7fb
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2010-11-01 08:22:59 +0000

    Catch exceptions when subscribing a folder or document

--- community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/dashboard/DashboardPanel.java
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/dashboard/DashboardPanel.java
@@ -116,14 +116,18 @@ public class DashboardPanel extends VLayout implements UserObserver {
 
 	@Override
 	public void onUserChanged(GUIUser user, String attribute) {
-		if (attribute.equals(GUIUser.CHECKED_OUT_DOCS) || attribute.equals(GUIUser.LOCKED_DOCS)) {
-			updateUserTab();
-		} else if (attribute.equals(GUIUser.ALL_MESSAGES) || attribute.equals(GUIUser.UNREAD_MESSAGES)) {
-			updateMessageTab();
-		} else if (attribute.equals(GUIUser.ALL_SUBSCRIPTIONS)) {
-			updateSubscriptionsTab();
-		} else if (attribute.equals(GUIUser.ALL_TASKS)) {
-			updateWorkflowTab();
+		try {
+			if (attribute.equals(GUIUser.CHECKED_OUT_DOCS) || attribute.equals(GUIUser.LOCKED_DOCS)) {
+				updateUserTab();
+			} else if (attribute.equals(GUIUser.ALL_MESSAGES) || attribute.equals(GUIUser.UNREAD_MESSAGES)) {
+				updateMessageTab();
+			} else if (attribute.equals(GUIUser.ALL_SUBSCRIPTIONS)) {
+				updateSubscriptionsTab();
+			} else if (attribute.equals(GUIUser.ALL_TASKS)) {
+				updateWorkflowTab();
+			}
+		} catch (Throwable t) {
+
 		}
 	}
 }
