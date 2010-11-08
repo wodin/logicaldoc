e3abeeac878c5ae8a0ad11b63fb9d09b4b2a6cf8
blucecio <blucecio@bae09422-6297-422f-b3ee-419521344c47>
2010-11-08 10:36:07 +0000

    Simplified display of filesize

--- community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/document/VersionsDiff.java
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/document/VersionsDiff.java
@@ -60,8 +60,8 @@ public class VersionsDiff extends Window {
 		records.add(new DiffRecord(I18N.message("object"), version1.getObject(), version2.getObject()));
 		records.add(new DiffRecord(I18N.message("coverage"), version1.getCoverage(), version2.getCoverage()));
 		records.add(new DiffRecord(I18N.message("filename"), version1.getFileName(), version2.getFileName()));
-		records.add(new DiffRecord(I18N.message("size"), Util.formatSize(version1.getFileSize()), Util
-				.formatSize(version2.getFileSize())));
+		records.add(new DiffRecord(I18N.message("size"), Util.formatSizeKB(version1.getFileSize()), Util
+				.formatSizeKB(version2.getFileSize())));
 		records.add(new DiffRecord(I18N.message("recipient"), version1.getRecipient(), version2.getRecipient()));
 		records.add(new DiffRecord(I18N.message("folder"),
 				version1.getFolder().getId() == Constants.DOCUMENTS_FOLDERID ? "/" : version1.getFolder().getName(),
