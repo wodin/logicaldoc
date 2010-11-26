3932f498a170a7532e0252e5b26028e6780c39e8
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2010-11-26 17:09:10 +0000

    Automatically generate a unique installation indentifier

--- community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/CorePlugin.java
+++ community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/CorePlugin.java
@@ -1,9 +1,13 @@
 package com.logicaldoc.core;
 
+import java.util.UUID;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.logicaldoc.core.task.TaskManager;
+import com.logicaldoc.util.config.ContextProperties;
 import com.logicaldoc.util.plugin.LogicalDOCPlugin;
 
 /**
@@ -18,7 +22,12 @@ public class CorePlugin extends LogicalDOCPlugin {
 
 	@Override
 	protected void doStart() throws Exception {
-
+		ContextProperties pbean = new ContextProperties();
+		if (StringUtils.isEmpty(pbean.getProperty("id"))){
+			pbean.setProperty("id", UUID.randomUUID().toString());
+			pbean.write();
+		}
+		
 		// Register scheduled tasks
 		TaskManager manager = new TaskManager();
 		manager.registerTasks();
