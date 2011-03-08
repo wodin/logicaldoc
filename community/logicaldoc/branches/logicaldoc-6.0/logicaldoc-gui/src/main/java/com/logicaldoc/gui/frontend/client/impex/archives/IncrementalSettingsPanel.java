0e4a415ec9b776080f95cca3c0655939833a37e0
mcaruso <mcaruso@bae09422-6297-422f-b3ee-419521344c47>
2011-03-08 11:23:27 +0000

    Fixed bug saving an incremental archive.

--- community/logicaldoc/branches/logicaldoc-6.0/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/impex/archives/IncrementalSettingsPanel.java
+++ community/logicaldoc/branches/logicaldoc-6.0/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/impex/archives/IncrementalSettingsPanel.java
@@ -1,6 +1,8 @@
 package com.logicaldoc.gui.frontend.client.impex.archives;
 
-import com.google.gwt.core.client.JavaScriptObject;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.logicaldoc.gui.common.client.Feature;
 import com.logicaldoc.gui.common.client.beans.GUIIncrementalArchive;
 import com.logicaldoc.gui.common.client.beans.GUITemplate;
@@ -8,7 +10,6 @@ import com.logicaldoc.gui.common.client.util.ItemFactory;
 import com.logicaldoc.gui.common.client.widgets.FolderChangeListener;
 import com.logicaldoc.gui.common.client.widgets.FolderSelector;
 import com.smartgwt.client.types.TitleOrientation;
-import com.smartgwt.client.util.JSOHelper;
 import com.smartgwt.client.widgets.form.DynamicForm;
 import com.smartgwt.client.widgets.form.ValuesManager;
 import com.smartgwt.client.widgets.form.fields.IntegerItem;
@@ -33,14 +34,15 @@ public class IncrementalSettingsPanel extends VLayout {
 
 	private FolderSelector folderSelector = new FolderSelector("folder", false);
 
-	public IncrementalSettingsPanel(GUIIncrementalArchive incremental, ChangedHandler changedHandler,
-			FolderChangeListener folderListener) {
+	public IncrementalSettingsPanel(GUIIncrementalArchive incremental,
+			ChangedHandler changedHandler, FolderChangeListener folderListener) {
 		this.incremental = incremental;
 
 		form.setValuesManager(vm);
 		form.setTitleOrientation(TitleOrientation.TOP);
 
-		TextItem prefix = ItemFactory.newSimpleTextItem("prefix", "prefix", incremental.getPrefix());
+		TextItem prefix = ItemFactory.newSimpleTextItem("prefix", "prefix",
+				incremental.getPrefix());
 		prefix.setRequired(true);
 		prefix.addChangedHandler(changedHandler);
 
@@ -48,7 +50,8 @@ public class IncrementalSettingsPanel extends VLayout {
 		type.setValue(Integer.toString(incremental.getType()));
 		type.addChangedHandler(changedHandler);
 
-		IntegerItem frequency = ItemFactory.newIntegerItem("frequency", "frequency", incremental.getFrequency());
+		IntegerItem frequency = ItemFactory.newIntegerItem("frequency",
+				"frequency", incremental.getFrequency());
 		IntegerRangeValidator min = new IntegerRangeValidator();
 		min.setMin(1);
 		frequency.setValidators(min);
@@ -62,7 +65,8 @@ public class IncrementalSettingsPanel extends VLayout {
 		templates.addChangedHandler(changedHandler);
 		templates.setValues(incremental.getTemplateIds());
 
-		if (Feature.visible(Feature.PAPER_DEMATERIALIZATION) && incremental.getId() == 0) {
+		if (Feature.visible(Feature.PAPER_DEMATERIALIZATION)
+				&& incremental.getId() == 0) {
 			form.setFields(prefix, type, frequency, folderSelector, templates);
 			if (!Feature.enabled(Feature.PAPER_DEMATERIALIZATION))
 				type.setDisabled(true);
@@ -78,22 +82,26 @@ public class IncrementalSettingsPanel extends VLayout {
 		if (!vm.hasErrors()) {
 			incremental.setPrefix(vm.getValueAsString("prefix").toString());
 			if (vm.getValueAsString("archivetype") != null)
-				incremental.setType(Integer.parseInt(vm.getValueAsString("archivetype")));
-			incremental.setFrequency(Integer.parseInt(vm.getValueAsString("frequency")));
+				incremental.setType(Integer.parseInt(vm
+						.getValueAsString("archivetype")));
+			incremental.setFrequency(Integer.parseInt(vm
+					.getValueAsString("frequency")));
 			incremental.setFolder(folderSelector.getFolder());
 
 			if (vm.getValues().get("template") != null) {
-				String templateIdString = vm.getValues().get("template").toString().trim().replace("[", "")
-						.replace("]", "");
+				String templateIdString = vm.getValues().get("template")
+						.toString().trim().replace("[", "").replace("]", "");
 				if (!templateIdString.isEmpty()) {
 					String[] selection = templateIdString.split(",");
 					List<GUITemplate> templates = new ArrayList<GUITemplate>();
 					for (String selectionId : selection) {
 						GUITemplate currentTemplate = new GUITemplate();
-						currentTemplate.setId(Long.parseLong(selectionId.trim()));
+						currentTemplate
+								.setId(Long.parseLong(selectionId.trim()));
 						templates.add(currentTemplate);
 					}
-					incremental.setTemplates(templates.toArray(new GUITemplate[0]));
+					incremental.setTemplates(templates
+							.toArray(new GUITemplate[0]));
 				}
 			}
 
