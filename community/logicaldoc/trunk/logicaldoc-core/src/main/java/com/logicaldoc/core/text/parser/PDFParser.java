52918c5f71905879330e0c72cf0b20494bd19802
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2011-03-08 10:55:06 +0000

    #22 PDF Parser don't extract the text in forms

--- community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/text/parser/PDFParser.java
+++ community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/text/parser/PDFParser.java
@@ -9,11 +9,19 @@ import java.io.Writer;
 import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.pdfbox.exceptions.InvalidPasswordException;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
 import org.apache.pdfbox.pdmodel.PDDocumentInformation;
+import org.apache.pdfbox.pdmodel.common.COSObjectable;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.apache.pdfbox.util.PDFTextStripper;
 
 /**
@@ -79,6 +87,9 @@ public class PDFParser extends AbstractParser {
 		content = "";
 		PDDocument pdfDocument = null;
 
+		// Will store the extracted text
+		StringBuffer buffer = new StringBuffer();
+
 		try {
 			org.apache.pdfbox.pdfparser.PDFParser parser = new org.apache.pdfbox.pdfparser.PDFParser(input);
 
@@ -90,67 +101,24 @@ public class PDFParser extends AbstractParser {
 					throw new Exception("Can not get pdf document for parsing");
 				}
 
-				try {
-					PDDocumentInformation information = pdfDocument.getDocumentInformation();
-					if (information == null) {
-						throw new Exception("Can not get information from pdf document");
-					}
-
-					author = information.getAuthor();
-					if (author == null) {
-						author = "";
-					}
-
-					title = information.getTitle();
-					if (title == null) {
-						title = "";
-					}
-
+				if (pdfDocument.isEncrypted()) {
 					try {
-						Calendar calendar = information.getCreationDate();
-						Date date = calendar.getTime();
-						sourceDate = DateFormat.getDateInstance().format(date);
-						// In Italian it will be like 27-giu-2007
-						// sourceDate =
-						// DateFormat.getDateInstance(DateFormat.SHORT,
-						// Locale.ENGLISH).format(date);
-					} catch (Throwable e) {
-						log.error("Bad date format " + e.getMessage());
-						sourceDate = "";
-					}
-
-					tags = information.getKeywords();
-					if (tags == null) {
-						tags = "";
-					}
-				} catch (Exception e) {
-					log.error(e.getMessage(), e);
-					e.printStackTrace();
-				}
-
-				Writer writer = new CharArrayWriter();
-				PDFTextStripper stripper = new PDFTextStripper("UTF-8");
-				try {
-					if (pdfDocument.isEncrypted()) {
-						writer.write("encrypted document");
-						log.warn("Unable to decrypt pdf document");
-						throw new IOException("Encrypted document");
-					}
-
-					stripper.writeText(pdfDocument, writer);
-					writer.flush();
-					content = writer.toString();
-				} catch (Throwable tw) {
-					log.error("Exception reading pdf document: " + tw.getMessage());
-					author = "";
-				} finally {
-					try {
-						writer.close();
-					} catch (Throwable e) {
-						log.error(e.getMessage(), e);
+						pdfDocument.decrypt("");
+					} catch (InvalidPasswordException e) {
+						log.error("Error: The document is encrypted.");
+						buffer.append("The document is encrypted");
+						return;
 					}
 				}
 
+				// Strip text from the entire document
+				parseDocument(pdfDocument, buffer);
+
+				// Now parse the forms
+				parseForm(pdfDocument, buffer);
+
+				// Store all the extracted contents
+				content = buffer.toString();
 			}
 		} catch (Throwable ex) {
 			log.error(ex.getMessage(), ex);
@@ -170,6 +138,65 @@ public class PDFParser extends AbstractParser {
 		count++;
 	}
 
+	/**
+	 * Extract text and metadata from the main document
+	 */
+	protected void parseDocument(PDDocument pdfDocument, StringBuffer buffer) {
+		try {
+			PDDocumentInformation information = pdfDocument.getDocumentInformation();
+			if (information == null) {
+				throw new Exception("Can not get information from pdf document");
+			}
+
+			author = information.getAuthor();
+			if (author == null) {
+				author = "";
+			}
+
+			title = information.getTitle();
+			if (title == null) {
+				title = "";
+			}
+
+			try {
+				Calendar calendar = information.getCreationDate();
+				Date date = calendar.getTime();
+				sourceDate = DateFormat.getDateInstance().format(date);
+				// In Italian it will be like 27-giu-2007
+				// sourceDate =
+				// DateFormat.getDateInstance(DateFormat.SHORT,
+				// Locale.ENGLISH).format(date);
+			} catch (Throwable e) {
+				log.error("Bad date format " + e.getMessage());
+				sourceDate = "";
+			}
+
+			tags = information.getKeywords();
+			if (tags == null) {
+				tags = "";
+			}
+
+			Writer writer = new CharArrayWriter();
+			PDFTextStripper stripper = new PDFTextStripper("UTF-8");
+			try {
+				stripper.writeText(pdfDocument, writer);
+				writer.flush();
+				buffer.append(writer.toString());
+			} catch (Throwable tw) {
+				log.error("Exception reading pdf document: " + tw.getMessage());
+				author = "";
+			} finally {
+				try {
+					writer.close();
+				} catch (Throwable e) {
+					log.error(e.getMessage(), e);
+				}
+			}
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+	}
+
 	@Override
 	public void parse(File file) {
 
@@ -186,6 +213,57 @@ public class PDFParser extends AbstractParser {
 				is.close();
 			} catch (Exception e) {
 				log.error(e.getMessage(), e);
+			}
+		}
+	}
+
+	/**
+	 * Extract the text from the form fields
+	 */
+	@SuppressWarnings("rawtypes")
+	public void parseForm(PDDocument pdfDocument, StringBuffer buffer) throws IOException {
+		PDDocumentCatalog docCatalog = pdfDocument.getDocumentCatalog();
+		PDAcroForm acroForm = docCatalog.getAcroForm();
+
+		if (acroForm == null)
+			return;
+
+		buffer.append("\n");
+
+		List fields = acroForm.getFields();
+		Iterator fieldsIter = fields.iterator();
+
+		log.debug(new Integer(fields.size()).toString() + " top-level fields were found on the form");
+
+		while (fieldsIter.hasNext()) {
+			PDField field = (PDField) fieldsIter.next();
+			parseField(field, "|--", field.getPartialName(), buffer);
+		}
+	}
+
+	/**
+	 * Extracts the field contents populating the given buffer. This method is
+	 * recursive, use carefully.
+	 */
+	private void parseField(PDField field, String sLevel, String sParent, StringBuffer buffer) throws IOException {
+		List<COSObjectable> kids = field.getKids();
+		if (kids != null) {
+			Iterator<COSObjectable> kidsIter = kids.iterator();
+			if (!sParent.equals(field.getPartialName())) {
+				sParent = sParent + "." + field.getPartialName();
+			}
+
+			while (kidsIter.hasNext()) {
+				Object pdfObj = kidsIter.next();
+				if (pdfObj instanceof PDField) {
+					PDField kid = (PDField) pdfObj;
+					parseField(kid, "|  " + sLevel, sParent, buffer);
+				}
+			}
+		} else {
+			if (StringUtils.isNotEmpty(field.getValue())) {
+				buffer.append(" ");
+				buffer.append(field.getValue());
 			}
 		}
 	}
