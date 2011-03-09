f21e115ad3f2f8855806382c7c26f48782e2448a
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2011-03-09 18:55:11 +0000

    Trapped OutOfBound exceptions

--- community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/document/dao/HibernateRatingDAO.java
+++ community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/document/dao/HibernateRatingDAO.java
@@ -53,10 +53,10 @@ public class HibernateRatingDAO extends HibernatePersistentObjectDAO<Rating> imp
 			};
 
 			coll = (List<Rating>) query(query, new Object[] {}, ratingMapper, null);
-			if (coll.get(0).getCount() != 0)
+			if (!coll.isEmpty() && coll.get(0).getCount() != 0)
 				return coll.get(0);
 
-		} catch (Exception e) {
+		} catch (Throwable e) {
 			if (log.isErrorEnabled())
 				log.error(e.getMessage(), e);
 		}
