904522459752c100ac5c67f0d70b1b1982865aa7
mcaruso <mcaruso@bae09422-6297-422f-b3ee-419521344c47>
2011-03-10 17:14:45 +0000

    Listing of all storage archives and incremental archives completed. Started the signing and marking of closed storage archive.

--- community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/document/SignClosureDialog.java
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/document/SignClosureDialog.java
@@ -1,15 +1,32 @@
 package com.logicaldoc.gui.frontend.client.document;
 
-import com.google.gwt.user.client.ui.HTML;
+import gwtupload.client.IUploadStatus.Status;
+import gwtupload.client.IUploader;
+import gwtupload.client.MultiUploader;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.logicaldoc.gui.common.client.Session;
-import com.logicaldoc.gui.common.client.beans.GUIArchive;
 import com.logicaldoc.gui.common.client.i18n.I18N;
-import com.logicaldoc.gui.common.client.util.Util;
+import com.logicaldoc.gui.common.client.log.Log;
+import com.logicaldoc.gui.common.client.util.ItemFactory;
 import com.logicaldoc.gui.frontend.client.impex.archives.ExportArchivesList;
+import com.logicaldoc.gui.frontend.client.services.SignService;
+import com.logicaldoc.gui.frontend.client.services.SignServiceAsync;
+import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.types.HeaderControls;
+import com.smartgwt.client.types.TitleOrientation;
+import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.Window;
 import com.smartgwt.client.widgets.events.CloseClickHandler;
 import com.smartgwt.client.widgets.events.CloseClientEvent;
+import com.smartgwt.client.widgets.form.DynamicForm;
+import com.smartgwt.client.widgets.form.ValuesManager;
+import com.smartgwt.client.widgets.form.fields.LinkItem;
+import com.smartgwt.client.widgets.form.fields.StaticTextItem;
+import com.smartgwt.client.widgets.form.fields.SubmitItem;
+import com.smartgwt.client.widgets.form.fields.events.ClickEvent;
+import com.smartgwt.client.widgets.form.fields.events.ClickHandler;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 /**
@@ -19,9 +36,21 @@ import com.smartgwt.client.widgets.layout.VLayout;
  * @since 6.0
  */
 public class SignClosureDialog extends Window {
-	private HTML applet = new HTML();
-
 	private ExportArchivesList archivesList = null;
+
+	private SignServiceAsync signService = (SignServiceAsync) GWT.create(SignService.class);
+
+	private SubmitItem sendButton;
+
+	private MultiUploader uploader;
+
+	private ValuesManager vm;
+
+	private DynamicForm form;
+
+	private VLayout layout = new VLayout();
+
+	private long docId;
 
 	public SignClosureDialog(ExportArchivesList list, String id, String name) {
 		this.archivesList = list;
@@ -34,41 +63,145 @@ public class SignClosureDialog extends Window {
 			@Override
 			public void onCloseClick(CloseClientEvent event) {
 				destroy();
-				archivesList.refresh(GUIArchive.TYPE_DEFAULT, false);
+				archivesList.refresh(archivesList.getArchivesType(), false);
 				archivesList.showDetails(Long.parseLong(archiveId), false);
 			}
 		});
 
 		setHeaderControls(HeaderControls.HEADER_LABEL, HeaderControls.CLOSE_BUTTON);
 
-		setTitle(I18N.message("signdocuments"));
-		setWidth(460);
-		setHeight(340);
+		setTitle(I18N.message("signdocument"));
+		setWidth(550);
+		setHeight(230);
 		setCanDragResize(true);
 		setIsModal(true);
 		setShowModalMask(true);
 		centerInPage();
 
-		String tmp = "<applet name=\"SignApplet\" archive=\""
-				+ Util.contextPath()
-				+ "applet/logicaldoc-sign.jar\"  code=\"com.logicaldoc.sign.applet.Signer\" width=\"400\" height=\"300\">";
-		tmp += "<param name=\"lang\" value=\"" + I18N.getLocale() + "\" />";
-		tmp += "<param name=\"uploadUrl\" value=\"" + Util.contextPath() + "closearchive?archiveId={id}&sid="
-				+ Session.get().getSid() + "\" />";
-		tmp += "<param name=\"downloadUrl\" value=\"" + Util.contextPath() + "downloadarchive?archiveId={id}&sid="
-				+ Session.get().getSid() + "\" />";
-		tmp += "<param name=\"ids\" value=\"" + id + "\" />";
-		tmp += "<param name=\"names\" value=\"" + name + "\" />";
-		tmp += "<param name=\"forceCRLDownload\" value=\"false\" />";
-		tmp += "<param name=\"timestampRequested\" value=\"true\" />";
-		tmp += "<param name=\"checkcrl\" value=\"" + Session.get().getInfo().getConfig("checkcrl") + "\" />";
-		tmp += "</applet>";
+		DynamicForm urlForm = new DynamicForm();
+		urlForm.setMargin(3);
+		LinkItem downloadUrl = ItemFactory.newLinkItem("", "<b>(1)</b> " + I18N.message("downloadfiletosign"));
+		downloadUrl.setTitleOrientation(TitleOrientation.LEFT);
+		downloadUrl.setWrapTitle(false);
+		downloadUrl.setValue(GWT.getHostPageBaseURL() + "downloadarchive?archiveId=" + id + "&sid="
+				+ Session.get().getSid());
+		downloadUrl.setLinkTitle(name + ".zip");
 
-		applet.setHTML(tmp);
-		applet.setWidth("420px");
-		applet.setHeight("320px");
+		urlForm.setItems(downloadUrl);
+		layout.addMember(urlForm, 0);
 
-		layout.addMember(applet);
+		DynamicForm messageForm = new DynamicForm();
+		messageForm.setMargin(3);
+		StaticTextItem uploadMessage = ItemFactory.newStaticTextItem(I18N.message(""),
+				I18N.message("signmarkandupload", name), null);
+		uploadMessage.setTitle("<b>(2)</b> " + uploadMessage.getTitle());
+		uploadMessage.setWidth(500);
+		uploadMessage.setWrapTitle(false);
+		messageForm.setItems(uploadMessage);
+		layout.addMember(messageForm, 1);
+
+		reloadForm();
+
+		// Create a new uploader panel and attach it to the window
+		uploader = new MultiUploader();
+		uploader.setMaximumFiles(1);
+		uploader.setStyleName("upload");
+		uploader.setFileInputPrefix("LDOC");
+		uploader.setHeight("40px");
+		uploader.reset();
+
+		// Add a finish handler which will load the image once the upload
+		// finishes
+		uploader.addOnFinishUploadHandler(onFinishUploaderHandler);
+		uploader.addOnCancelUploadHandler(onCancelUploaderHandler);
+
+		layout.addMember(uploader, 2);
+		layout.setMembersMargin(10);
+		layout.setTop(25);
+		layout.setMargin(5);
+
 		addChild(layout);
+	}
+
+	private void reloadForm() {
+		if (form != null) {
+			layout.removeChild(form);
+		}
+
+		form = new DynamicForm();
+		vm = new ValuesManager();
+		form.setValuesManager(vm);
+
+		sendButton = new SubmitItem();
+		sendButton.setTitle(I18N.message("send"));
+		sendButton.setDisabled(true);
+		sendButton.setAlign(Alignment.RIGHT);
+		sendButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				onSend();
+			}
+		});
+
+		form.setItems(sendButton);
+
+		layout.addMember(form, 3);
+	}
+
+	// Load the image in the document and in the case of success attach it to
+	// the viewer
+	private IUploader.OnFinishUploaderHandler onFinishUploaderHandler = new IUploader.OnFinishUploaderHandler() {
+		public void onFinish(IUploader uploader) {
+			if (uploader.getStatus() == Status.SUCCESS) {
+				sendButton.setDisabled(false);
+			}
+		}
+	};
+
+	private IUploader.OnCancelUploaderHandler onCancelUploaderHandler = new IUploader.OnCancelUploaderHandler() {
+		@Override
+		public void onCancel(IUploader uploader) {
+			cancel();
+		}
+	};
+
+	public void onSend() {
+		if (uploader.getSuccessUploads() <= 0) {
+			SC.warn(I18N.message("filerequired"));
+			return;
+		}
+		if (!vm.validate())
+			return;
+
+		// TODO See the method 'signDocument' of the SignService. Here we have
+		// to upload the .m7m file uploaded by the user, but only after the
+		// original file digest verification with the file contained
+		// into the uploaded file (inside the .p7m file contained into the .m7m
+		// file).
+		// See the 'CloseArchive servlet' for the archive closure.
+
+		signService.signDocument(Session.get().getSid(), Session.get().getUser().getId(), docId,
+				new AsyncCallback<String>() {
+
+					@Override
+					public void onFailure(Throwable caught) {
+						Log.serverError(caught);
+						destroy();
+					}
+
+					@Override
+					public void onSuccess(String result) {
+						if (result == "ok") {
+							DocumentsPanel.get().refresh();
+							destroy();
+						} else {
+							SC.warn(I18N.message(result));
+						}
+					}
+				});
+	}
+
+	private void cancel() {
+		sendButton.setDisabled(true);
 	}
 }
