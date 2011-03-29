95be448d6de34b17adc7a032554c9d4243209584
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2011-03-29 20:47:41 +0000

    new translations

--- community/logicaldoc/trunk/logicaldoc-i18n/src/main/java/com/logicaldoc/i18n/PurifyRB.java
+++ community/logicaldoc/trunk/logicaldoc-i18n/src/main/java/com/logicaldoc/i18n/PurifyRB.java
@@ -115,10 +115,8 @@ public class PurifyRB {
 			out.close();
 			ow.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
