758a72f87aad7ca26c3c5bc85a966450ce5b6629
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2011-08-11 07:57:38 +0000

    Migration to GWT 2.3 and SmartGWT 2.5

---
+++ community/logicaldoc/branches/logicaldoc-6.2/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/document/ThumbnailPanel.java
@@ -0,0 +1,27 @@
+package com.logicaldoc.gui.frontend.client.document;
+
+import com.google.gwt.user.client.ui.Image;
+import com.logicaldoc.gui.common.client.Session;
+import com.logicaldoc.gui.common.client.beans.GUIDocument;
+import com.logicaldoc.gui.common.client.util.Util;
+
+/**
+ * This panel shows the notes on a document
+ * 
+ * @author Matteo Caruso - Logical Objects
+ * @since 6.2
+ */
+public class ThumbnailPanel extends DocumentDetailTab {
+
+	public ThumbnailPanel(final GUIDocument document) {
+		super(document, null);
+		setMembersMargin(1);
+
+		String url = Util.contextPath() + "/thumbnail?docId=" + document.getId() + "&sid=" + Session.get().getSid();
+		int size = Integer.parseInt(Session.get().getInfo().getConfig("gui.thumbnail.size"));
+		Image image = new Image(url);
+		image.setWidth(size + "px");
+
+		addMember(image);
+	}
+}
