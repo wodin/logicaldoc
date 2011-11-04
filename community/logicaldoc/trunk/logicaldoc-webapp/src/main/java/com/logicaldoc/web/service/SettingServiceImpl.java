0b9d55d2f350c4713036cb4e526e3cc27dd3b5eb
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2011-11-04 07:55:15 +0000

    don't load command. settings

--- community/logicaldoc/trunk/logicaldoc-webapp/src/main/java/com/logicaldoc/web/service/SettingServiceImpl.java
+++ community/logicaldoc/trunk/logicaldoc-webapp/src/main/java/com/logicaldoc/web/service/SettingServiceImpl.java
@@ -111,7 +111,8 @@ public class SettingServiceImpl extends RemoteServiceServlet implements SettingS
 					|| name.startsWith("index") || name.startsWith("proxy") || name.equals("id")
 					|| name.startsWith("lang") || name.startsWith("reg.") || name.startsWith("ocr.")
 					|| name.startsWith("barcode.") || name.startsWith("task.") || name.startsWith("quota")
-					|| name.startsWith("store") || name.startsWith("flexpaperviewer") || name.startsWith("omnipage."))
+					|| name.startsWith("store") || name.startsWith("flexpaperviewer") || name.startsWith("omnipage.")
+					|| name.startsWith("command."))
 				continue;
 
 			sortedSet.add(key.toString());
