82190044e8b1b4cae51875b354508cedc5807003
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2012-02-08 13:32:56 +0000

    #189 	HTML formatting when sending documents in emails

---
+++ community/logicaldoc/trunk/logicaldoc-core/src/test/java/com/logicaldoc/core/EmailDummy.java
@@ -0,0 +1,49 @@
+package com.logicaldoc.core;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.logicaldoc.core.communication.EMail;
+import com.logicaldoc.core.communication.EMailSender;
+import com.logicaldoc.core.communication.Recipient;
+
+public class EmailDummy {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		EMailSender sender = prepareEmailSender();
+
+		EMail eml = new EMail();
+		eml.setSubject("HTML test");
+		eml.setHtml(true);
+		Set<Recipient> recipients = new HashSet<Recipient>();
+
+		eml.getImages().add("http://www.logicaldoc.com/templates/theme326/images/LogicalDOC_logo_website.gif");
+
+		
+		// Notify the general report
+		Recipient rec = new Recipient();
+		rec.setAddress("m.meschieri@logicalobjects.com");
+		recipients.add(rec);
+		eml.setRecipients(recipients);
+		eml.setMessageText("<span style='color:red'>Test</span> di <b>HTML</b>\ncippa"
+				+"<br/><image src='cid:image_1' />");
+		
+		sender.send(eml);
+	}
+
+	protected static EMailSender prepareEmailSender() {
+		EMailSender sender = new EMailSender();
+		sender.setHost("smtp.logicalobjects.com");
+		sender.setPort(25);
+		sender.setUsername("m.meschieri@logicalobjects.com");
+		sender.setPassword("paf75peR");
+		sender.setAuthEncripted(false);
+		sender.setConnectionSecurity(0);
+		sender.setSender("m.meschieri@logicalobjects.com");
+		return sender;
+	}
+}
