1617779fbb5c2c2df0d7e666e0573007d6222a1d
shatzing5 <shatzing5@bae09422-6297-422f-b3ee-419521344c47>
2012-02-09 08:38:05 +0000

    Fixed web-service exceptions

--- community/logicaldoc/trunk/logicaldoc-webservice/src/test/java/com/logicaldoc/webservice/document/DocumentServiceImplTest.java
+++ community/logicaldoc/trunk/logicaldoc-webservice/src/test/java/com/logicaldoc/webservice/document/DocumentServiceImplTest.java
@@ -86,7 +86,7 @@ public class DocumentServiceImplTest extends AbstractWebServiceTestCase {
 		doc.setIndexed(0);
 		docDao.store(doc);
 		docServiceImpl.move("", doc.getId(), newFolder.getId()); 
-		// attenzione: errore optimistic lock
+		// NOTA: attenzione errore optimistic lock
 		Assert.assertSame(1L, doc.getId());
 		docDao.initialize(doc);
 		Assert.assertEquals(newFolder, doc.getFolder());
@@ -153,16 +153,9 @@ public class DocumentServiceImplTest extends AbstractWebServiceTestCase {
 	@Test
 	public void testDelete() throws Exception {
 		Document doc = docDao.findById(1);
-		System.err.println("doc: " + doc);
 		Assert.assertNotNull(doc);
-		System.err.println("doc.getId(): " + doc.getId());
-		docServiceImpl.delete("", doc.getId());
-		// NOTE: you can see an exception of this type: 
-		//1    [main] ERROR com.logicaldoc.core.document.dao.HibernateDocumentDAO  - 
-		//java.lang.NullPointerException
-        // this is due since we are trying to find a deleted document		
+		docServiceImpl.delete("", doc.getId());	
 		doc = docDao.findById(1);	
-		System.err.println("doc: " + doc);
 		Assert.assertNull(doc);
 	}
 
@@ -172,13 +165,7 @@ public class DocumentServiceImplTest extends AbstractWebServiceTestCase {
 		Assert.assertNotNull(doc);
 		Assert.assertEquals("testDocname", doc.getTitle());
 		docDao.initialize(doc);
-
-		// TODO: fails to initialize a lazy collection (Tags)
-		// TODO: this test may fails; file may not exists
-		// you can see an exception of this type: 
-		// failed to lazily initialize a collection of role: com.logicaldoc.core.document.Document.tags, no session or session was closed		
 		docServiceImpl.rename("", 1, "pippo");
-
 		docDao.initialize(doc);
 		Assert.assertEquals("pippo", doc.getTitle());
 		Assert.assertEquals("sourceauthor1", doc.getSourceAuthor());
@@ -192,12 +179,7 @@ public class DocumentServiceImplTest extends AbstractWebServiceTestCase {
 		Assert.assertNotNull(doc);
 		Assert.assertEquals("testDocname", doc.getTitle());
 		docDao.initialize(doc);
-
-		// TODO: this test may fails; file may not exists
-		// you can see an exception of this type: 
-		// failed to lazily initialize a collection of role: com.logicaldoc.core.document.Document.tags, no session or session was closed
 		docServiceImpl.renameFile("", 1, "pippo.doc");
-
 		docDao.initialize(doc);
 		Assert.assertEquals("pippo.doc", doc.getFileName());
 		Assert.assertEquals("doc", doc.getType());
