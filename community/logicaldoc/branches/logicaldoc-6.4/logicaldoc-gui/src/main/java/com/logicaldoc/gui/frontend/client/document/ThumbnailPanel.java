383ab7ad214397b5d8d6fe0d50d3d2fdf8c409c4
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2012-04-25 07:34:29 +0000

    Preview button in image viewer

--- community/logicaldoc/branches/logicaldoc-6.4/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/document/ThumbnailPanel.java
+++ community/logicaldoc/branches/logicaldoc-6.4/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/document/ThumbnailPanel.java
@@ -1,8 +1,6 @@
 package com.logicaldoc.gui.frontend.client.document;
 
-import com.logicaldoc.gui.common.client.Session;
 import com.logicaldoc.gui.common.client.beans.GUIDocument;
-import com.logicaldoc.gui.common.client.util.Util;
 import com.logicaldoc.gui.common.client.widgets.ImageViewer;
 
 /**
@@ -23,9 +21,7 @@ public class ThumbnailPanel extends DocumentDetailTab {
 	@Override
 	protected void onTabSelected() {
 		if (viewer == null) {
-			String url = Util.contextPath() + "/thumbnail?docId=" + document.getId() + "&sid=" + Session.get().getSid();
-
-			ImageViewer viewer = new ImageViewer(url);
+			ImageViewer viewer = new ImageViewer(document);
 			setMembers(viewer);
 		}
 	}
