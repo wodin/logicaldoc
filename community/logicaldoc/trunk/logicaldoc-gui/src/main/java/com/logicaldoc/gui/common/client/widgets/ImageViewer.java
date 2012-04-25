4ecd80f0471a527cd67e8ca2153f58439b1f1da1
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2012-04-25 07:36:24 +0000

    Preview button in image viewer

--- community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/widgets/ImageViewer.java
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/widgets/ImageViewer.java
@@ -1,6 +1,10 @@
 package com.logicaldoc.gui.common.client.widgets;
 
+import com.logicaldoc.gui.common.client.Session;
+import com.logicaldoc.gui.common.client.beans.GUIDocument;
 import com.logicaldoc.gui.common.client.i18n.I18N;
+import com.logicaldoc.gui.common.client.util.Util;
+import com.logicaldoc.gui.frontend.client.document.PreviewPopup;
 import com.reveregroup.gwt.imagepreloader.FitImage;
 import com.reveregroup.gwt.imagepreloader.ImageLoadEvent;
 import com.reveregroup.gwt.imagepreloader.ImageLoadHandler;
@@ -31,19 +35,19 @@ public class ImageViewer extends VLayout {
 
 	private int maxSize = 600;
 
-	private int width;
-
-	private int height;
-
 	private String url;
+
+	private GUIDocument document;
 
 	/**
 	 * Constructs the image respecting the original image size
 	 * 
 	 * @param url the image URL
 	 */
-	public ImageViewer(String url) {
-		this.url = url;
+	public ImageViewer(GUIDocument document) {
+		this.document = document;
+		this.url = Util.contextPath() + "/thumbnail?docId=" + document.getId() + "&sid=" + Session.get().getSid();
+
 		setWidth100();
 		setHeight100();
 
@@ -75,39 +79,7 @@ public class ImageViewer extends VLayout {
 		});
 	}
 
-	public ImageViewer(String url, int width, int height) {
-		this.width = width;
-		this.height = height;
-		this.url = url;
-		setWidth100();
-		setHeight100();
-
-		setupToolbar();
-
-		ImagePreloader.load(url, new ImageLoadHandler() {
-			@Override
-			public void imageLoaded(ImageLoadEvent event) {
-				// ImageViewer.this.baseSize = event.getDimensions().getWidth()
-				// < 600 ? event.getDimensions().getWidth()
-				// : 600;
-
-				image = new FitImage(ImageViewer.this.url, ImageViewer.this.width, ImageViewer.this.height);
-				imageContainer = new VLayout();
-				imageContainer.setMargin(0);
-				imageContainer.setPadding(0);
-				imageContainer.setWidth100();
-				imageContainer.setHeight100();
-				imageContainer.setOverflow(Overflow.SCROLL);
-				imageContainer.addMember(image);
-
-				addMember(toolBar);
-				addMember(imageContainer);
-			}
-		});
-	}
-
 	private void setupToolbar() {
-
 		toolBar = new ToolStrip();
 		toolBar.setHeight(20);
 		toolBar.setWidth100();
@@ -145,7 +117,33 @@ public class ImageViewer extends VLayout {
 			}
 		});
 
+		ToolStripButton preview = new ToolStripButton();
+		preview.setTitle(I18N.message("preview"));
+		toolBar.addButton(preview);
+		preview.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				onPreview();
+			}
+		});
+
 		toolBar.addFill();
+	}
+
+	protected void onPreview() {
+		try {
+			String filename = document.getFileName();
+			String version = document.getVersion();
+
+			// In the search hitlist we don't have the filename
+			if (filename == null)
+				filename = document.getTitle() + "." + document.getType();
+
+			PreviewPopup iv = new PreviewPopup(document.getId(), version, filename);
+			iv.show();
+		} catch (Throwable t) {
+
+		}
 	}
 
 	protected void onEffectiveSize() {
@@ -162,7 +160,6 @@ public class ImageViewer extends VLayout {
 	}
 
 	private void refreshImage() {
-
 		if (contains(imageContainer))
 			removeMember(imageContainer);
 
@@ -180,7 +177,6 @@ public class ImageViewer extends VLayout {
 	}
 
 	private void refreshImageEffectiveSize() {
-
 		if (contains(imageContainer))
 			removeMember(imageContainer);
 
