aed7387b47b4d5333f9a0413f04a98c9bc2056b5
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2012-08-18 10:11:00 +0000

    First packaging of the plugin and partial implementation of the protocol

--- community/logicaldoc/trunk/logicaldoc-cmis/src/main/java/org/apache/chemistry/opencmis/fileshare/FileShareRepository.java
+++ community/logicaldoc/trunk/logicaldoc-cmis/src/main/java/org/apache/chemistry/opencmis/fileshare/FileShareRepository.java
@@ -1,4 +1,22 @@
-package com.logicaldoc.opencmis.server;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.chemistry.opencmis.fileshare;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -106,7 +124,6 @@ import org.apache.chemistry.opencmis.commons.impl.server.ObjectInfoImpl;
 import org.apache.chemistry.opencmis.commons.server.CallContext;
 import org.apache.chemistry.opencmis.commons.server.ObjectInfoHandler;
 import org.apache.chemistry.opencmis.commons.spi.Holder;
-import org.apache.chemistry.opencmis.fileshare.TypeManager;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -114,7 +131,7 @@ import org.apache.commons.logging.LogFactory;
 /**
  * File system back-end for CMIS server.
  */
-public class LogicaldocRepository {
+public class FileShareRepository {
 
     private static final String ROOT_ID = "@root@";
     private static final String SHADOW_EXT = ".cmis.xml";
@@ -128,7 +145,7 @@ public class LogicaldocRepository {
 
     private static final int BUFFER_SIZE = 64 * 1024;
 
-    private static final Log log = LogFactory.getLog(LogicaldocRepository.class);
+    private static final Log log = LogFactory.getLog(FileShareRepository.class);
 
     /** Repository id */
     private final String repositoryId;
@@ -151,7 +168,7 @@ public class LogicaldocRepository {
      * @param types
      *            type manager object
      */
-    public LogicaldocRepository(String repId, String rootPath, TypeManager types) {
+    public FileShareRepository(String repId, String rootPath, TypeManager types) {
         // check repository id
         if ((repId == null) || (repId.trim().length() == 0)) {
             throw new IllegalArgumentException("Invalid repository id!");
