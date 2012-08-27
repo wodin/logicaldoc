28a7270f55d2da6d3dfe2faba09382270dc14d85
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2012-08-27 13:31:27 +0000

    reload documents list on folder refresh

--- community/logicaldoc/branches/logicaldoc-6.5/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/folder/Navigator.java
+++ community/logicaldoc/branches/logicaldoc-6.5/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/folder/Navigator.java
@@ -658,6 +658,7 @@ public class Navigator extends TreeGrid implements FolderObserver {
 	private void onReload() {
 		TreeNode selectedNode = (TreeNode) getSelectedRecord();
 		getTree().reloadChildren(selectedNode);
+		selectFolder(Long.parseLong(selectedNode.getAttributeAsString("folderId")));
 	}
 
 	private void onCreate() {
