de31e0f54e2a3daaed4d8050af561487a814ed63
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2012-10-15 12:22:05 +0000

    Nice contacting server dialog

--- community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/widgets/ContactingServer.java
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/widgets/ContactingServer.java
@@ -4,6 +4,7 @@ import com.logicaldoc.gui.common.client.i18n.I18N;
 import com.logicaldoc.gui.common.client.util.Util;
 import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.types.Overflow;
+import com.smartgwt.client.types.VerticalAlignment;
 import com.smartgwt.client.widgets.Dialog;
 import com.smartgwt.client.widgets.Label;
 
@@ -32,17 +33,22 @@ public class ContactingServer extends Dialog {
 		setVertical(true);
 		setAlign(Alignment.CENTER);
 		setMargin(2);
+		setMembersMargin(0);
 		setBackgroundColor("white");
 		setBorder("1px solid DarkBlue");
 		setOverflow(Overflow.HIDDEN);
-
+		setHeight(55);
+		
 		Label message = new Label(I18N.message("contactingserver") + "...");
 		message.setWrap(false);
 		message.setAlign(Alignment.CENTER);
 		message.setIcon(Util.imageUrl("loading32.gif"));
 		message.setIconSize(32);
 		message.setStyleName("contactingserver");
+		message.setLayoutAlign(Alignment.CENTER);
+		message.setLayoutAlign(VerticalAlignment.TOP);
+		message.setHeight(50);
 		
-		addChild(message);
+		addMember(message);
 	}
 }
