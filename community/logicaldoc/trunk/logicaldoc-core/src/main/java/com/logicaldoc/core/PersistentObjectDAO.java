013341bc479b9e553b91cb0c616eab2332c808df
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2013-04-11 06:47:07 +0000

    imports optimization

--- community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/PersistentObjectDAO.java
+++ community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/PersistentObjectDAO.java
@@ -6,9 +6,6 @@ import java.util.List;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
-import com.logicaldoc.util.Context;
-import com.logicaldoc.util.config.ContextProperties;
-
 /**
  * Interface for DAOs that operate on persistent objects
  * 
