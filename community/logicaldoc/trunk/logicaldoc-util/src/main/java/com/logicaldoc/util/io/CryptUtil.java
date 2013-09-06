fb32eb2953866c9c9cdcd896980f3c4c7a548306
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2013-09-06 14:07:16 +0000

    Added MD4 crypt method

--- community/logicaldoc/trunk/logicaldoc-util/src/main/java/com/logicaldoc/util/io/CryptUtil.java
+++ community/logicaldoc/trunk/logicaldoc-util/src/main/java/com/logicaldoc/util/io/CryptUtil.java
@@ -1,8 +1,10 @@
 package com.logicaldoc.util.io;
 
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import org.bouncycastle.crypto.digests.MD4Digest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,6 +35,32 @@ public class CryptUtil {
 			logError(nsae.getMessage());
 		}
 
+		return copy;
+	}
+
+	/**
+	 * Converts a string into a MD4 hash.
+	 * 
+	 * @param original the original string to be encrypted.
+	 * @return the returned hash as bytes.
+	 */
+	public static String ctyptStringMD4(String original) {
+		String copy = "";
+		String pwd = original;
+		if (pwd == null) {
+			pwd = "";
+		}
+
+		try {
+			MD4Digest md4 = new MD4Digest();
+			byte[] pwdBytes = original.getBytes();
+			md4.update(pwdBytes, 0, pwdBytes.length);
+			byte[] encPwd = new byte[md4.getDigestSize()];
+			md4.doFinal(encPwd, 0);
+			copy = new BigInteger(1, encPwd).toString(16);
+		} catch (Throwable nsae) {
+			logError(nsae.getMessage());
+		}
 		return copy;
 	}
 
