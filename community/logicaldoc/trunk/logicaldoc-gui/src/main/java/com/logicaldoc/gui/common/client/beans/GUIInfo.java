b690711aaef22f8abad4f0c59590b00805cdb680
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2014-01-16 15:32:58 +0000

    Updated version to 6.9

--- community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/beans/GUIInfo.java
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/beans/GUIInfo.java
@@ -17,7 +17,7 @@ public class GUIInfo implements Serializable {
 
 	private String product = "LogicalDOC";
 
-	private String release = "6.8.2";
+	private String release = "6.9";
 
 	private String year = "2006-2014";
 
