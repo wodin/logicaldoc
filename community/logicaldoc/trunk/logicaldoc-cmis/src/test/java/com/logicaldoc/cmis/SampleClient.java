d6b1c069f78e8bec2ec4b04014408fe6e2498721
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2014-02-05 17:47:16 +0000

    Fixed permission check on checkin and download

--- community/logicaldoc/trunk/logicaldoc-cmis/src/test/java/com/logicaldoc/cmis/SampleClient.java
+++ community/logicaldoc/trunk/logicaldoc-cmis/src/test/java/com/logicaldoc/cmis/SampleClient.java
@@ -1,6 +1,9 @@
 package com.logicaldoc.cmis;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -9,6 +12,7 @@ import java.util.Map;
 import org.apache.chemistry.opencmis.client.api.CmisObject;
 import org.apache.chemistry.opencmis.client.api.Document;
 import org.apache.chemistry.opencmis.client.api.Folder;
+import org.apache.chemistry.opencmis.client.api.ObjectId;
 import org.apache.chemistry.opencmis.client.api.Session;
 import org.apache.chemistry.opencmis.client.api.SessionFactory;
 import org.apache.chemistry.opencmis.client.runtime.SessionFactoryImpl;
@@ -19,7 +23,9 @@ import org.apache.chemistry.opencmis.commons.enums.BaseTypeId;
 import org.apache.chemistry.opencmis.commons.enums.BindingType;
 import org.apache.chemistry.opencmis.commons.enums.UnfileObject;
 import org.apache.chemistry.opencmis.commons.enums.VersioningState;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisBaseException;
 import org.apache.chemistry.opencmis.commons.exceptions.CmisObjectNotFoundException;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 
 public class SampleClient {
@@ -32,24 +38,18 @@ public class SampleClient {
 	// org.apache.chemistry.opencmis.password=12345678
 	// org.apache.chemistry.opencmis.binding.compression=true
 	// org.apache.chemistry.opencmis.binding.cookies=true
-	//
-	//
-	// org.apache.chemistry.opencmis.binding.spi.type=atompub
-	// org.apache.chemistry.opencmis.binding.atompub.url=http://192.168.2.6:8080/alfresco/service/cmis
-	// org.apache.chemistry.opencmis.user=admin
-	// org.apache.chemistry.opencmis.password=admin
-	// org.apache.chemistry.opencmis.binding.compression=true
-	// org.apache.chemistry.opencmis.binding.cookies=true
 
-	private static final String CONNECTION_URL = "http://localhost:9080/logicaldoc/service/cmis";
+	private static final String CONNECTION_URL = "http://localhost:8080/service/cmis";
 
-	private static final String TEST_FOLDER_NAME = "/Default/pippo";
+	private static final String TEST_FOLDER_NAME = "/Default/testcmis";
 
-	private static final String TEST_DOCUMENT_NAME_1 = "Piano 2014.odt";
+	private static final String TEST_DOCUMENT_NAME_1 = "business plan 2014.docx";
 
-	public static void main(String[] args) {
-		Folder root = connect();
-		root.getName();
+	public static void main(String[] args) throws IOException {
+		
+//		Folder root = connect();
+//		root.getName();
+		
 		// cleanup(root, TEST_FOLDER_NAME);
 		// Folder newFolder = createFolder(root, TEST_FOLDER_NAME);
 		// createDocument(newFolder, TEST_DOCUMENT_NAME_1);
@@ -59,12 +59,87 @@ public class SampleClient {
 		// DeleteDocument(newFolder, "/" + TEST_DOCUMENT_NAME_2);
 		// System.out.println("+++ List Folder +++");
 		// listFolder(0, newFolder);
+		
+//		CmisObject object = session.getObjectByPath(TEST_FOLDER_NAME);
+//		System.out.println(object.getId() + " " + object.getName());
+//		
+//		object = session.getObjectByPath(TEST_FOLDER_NAME + "/" + TEST_DOCUMENT_NAME_1);
+//		System.out.println(object.getId() + " " + object.getProperty(TypeManager.PROP_TITLE).getValues().get(0));
+		
+		checkoutCheckin();
+	}
+	
+	
+	private static void checkoutCheckin() throws IOException {
+		
+		//Folder root = connect("admin", "admin");
+		//Folder root = connect("manager", "12345678"); // user solely in group author
+		Folder root = connect("dotNET2WSClient", "12345678"); // user in groups author and admin
+		root.getName();
+
 		CmisObject object = session.getObjectByPath(TEST_FOLDER_NAME);
 		System.out.println(object.getId() + " " + object.getName());
 		
 		object = session.getObjectByPath(TEST_FOLDER_NAME + "/" + TEST_DOCUMENT_NAME_1);
 		System.out.println(object.getId() + " " + object.getProperty(TypeManager.PROP_TITLE).getValues().get(0));
-	}
+		
+		Document pwc = (Document) object;
+//		ObjectId oid = pwc.checkOut();
+//		System.out.println("oid: "  +oid);
+//		System.out.println("oid.getId(): "  +oid.getId());
+		
+		 // default values if the document has no content
+		String filename = "business plan 2014.docx";
+	    String mimetype = "text/plain; charset=UTF-8";
+	    String content = "";
+		
+		// get the orginal content
+	    ContentStream contentStream = pwc.getContentStream();
+	    if (contentStream != null) {
+	        filename = contentStream.getFileName();
+	        mimetype = contentStream.getMimeType();
+	        System.out.println("mimetype: " +mimetype);
+	        //content = getContentAsString(contentStream);
+	    }
+		
+//	    String updatedContents = content + "\nLine added in new version";
+//
+//	    byte[] buf = updatedContents.getBytes("UTF-8");
+//	    
+//	    ByteArrayInputStream input = new ByteArrayInputStream(buf);
+	    
+	    
+	    File myFile = new File("C:/Users/alle/Desktop/business plan 2014.docx");
+	    	    
+	    FileInputStream fis = new FileInputStream(myFile);
+	    
+	    byte[] buf = IOUtils.toByteArray(fis);
+	    ByteArrayInputStream input = new ByteArrayInputStream(buf);
+
+	    contentStream = session.getObjectFactory().createContentStream(filename, buf.length, mimetype, input);
+	    
+	    boolean major = false;
+	    Map<String,?> properties = null;
+		
+	    // Check in the pwc
+	    try {
+	    	if (true)
+	    		throw new RuntimeException("fghfgh");
+	    	// I can do the checkout only if I own the checkout or if I'm in admin group
+	        pwc.checkIn(major, properties, contentStream, "minor version");
+	        System.out.println("checkin completed!");
+	    } catch (RuntimeException e) {
+	        e.printStackTrace();
+	        System.out.println("checkin failed, trying to cancel the checkout");
+	        try {
+				pwc.cancelCheckOut(); // this can also generate a permission exception
+				System.out.println("checkout status canceled");
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	    }
+	}	
 
 	/**
 	 * Clean up test folder before executing test
@@ -156,20 +231,29 @@ public class SampleClient {
 	}
 
 	/**
-	 * Connect to alfresco repository
+	 * Connect to logicaldoc repository
 	 * 
 	 * @return root folder object
 	 */
 	private static Folder connect() {
+		return connect("admin", "12345678");
+	}
+	
+	/**
+	 * Connect to logicaldoc repository
+	 * 
+	 * @return root folder object
+	 */
+	private static Folder connect(String username, String password) {
 		SessionFactory sessionFactory = SessionFactoryImpl.newInstance();
 		Map<String, String> parameter = new HashMap<String, String>();
-		parameter.put(SessionParameter.USER, "admin");
-		parameter.put(SessionParameter.PASSWORD, "12345678");
+		parameter.put(SessionParameter.USER, username);
+		parameter.put(SessionParameter.PASSWORD, password);
 		parameter.put(SessionParameter.ATOMPUB_URL, CONNECTION_URL);
 		parameter.put(SessionParameter.BINDING_TYPE, BindingType.ATOMPUB.value());
 		parameter.put(SessionParameter.REPOSITORY_ID, Long.toString(5L));
 
 		session = sessionFactory.createSession(parameter);
 		return session.getRootFolder();
-	}
+	}	
 }
