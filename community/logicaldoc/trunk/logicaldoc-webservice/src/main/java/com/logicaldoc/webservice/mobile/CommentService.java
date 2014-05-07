754ad5e0d1b56ffb5054d67d8e49e93990e9ed38
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2014-05-07 10:02:27 +0000

    javadoc fixes

--- community/logicaldoc/trunk/logicaldoc-webservice/src/main/java/com/logicaldoc/webservice/mobile/CommentService.java
+++ community/logicaldoc/trunk/logicaldoc-webservice/src/main/java/com/logicaldoc/webservice/mobile/CommentService.java
@@ -28,79 +28,81 @@ import com.logicaldoc.util.Context;
 import com.logicaldoc.webservice.AbstractService;
 
 /**
- * User: Alessandro Gasparini
- * Date: 06/05/14
+ * Adds new comments on a document.
+ * 
+ * @author Alessandro Gasparini - Logical Objects
+ * @since 6.8.4
  */
 public class CommentService extends AbstractService {
 
-protected final Logger log = LoggerFactory.getLogger(CommentService.class);
+	protected final Logger log = LoggerFactory.getLogger(CommentService.class);
 
-    @GET
-    @Path("/getcomments/{sid}/{docid}")
-    @Produces({"application/json"})
-    public Response getComments(@PathParam("sid") String sid, @PathParam("docid") String docid) throws Exception {
-    	
+	@GET
+	@Path("/getcomments/{sid}/{docid}")
+	@Produces({ "application/json" })
+	public Response getComments(@PathParam("sid") String sid, @PathParam("docid") String docid) throws Exception {
+
 		User user = validateSession(sid);
-		
+
 		DocumentDAO ddao = (DocumentDAO) Context.getInstance().getBean(DocumentDAO.class);
 		Long docId = new Long(docid);
 		Document document = ddao.findById(docId);
-		
+
 		checkReadEnable(user, document.getFolder().getId());
 		boolean writeEnabled = isWriteEnabled(user, document.getFolder().getId());
-		
+
 		DocumentNoteDAO dndao = (DocumentNoteDAO) Context.getInstance().getBean(DocumentNoteDAO.class);
 
-		List<DocumentNote> notes = dndao.findByDocId(docId);		
-		
-        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd yyyy HH:mm:ss Z", Locale.ENGLISH);
-        
-        CommentListVO comments = new CommentListVO();
-        for (DocumentNote note : notes) {
-        	 CommentVO cvo = new CommentVO();
-        	 cvo.setName(String.valueOf(note.getId()));
-             cvo.setContent(note.getMessage());
-             cvo.setAuthor(note.getUsername());
+		List<DocumentNote> notes = dndao.findByDocId(docId);
 
-             String formattedDate = sdf.format(note.getLastModified());
-             
-             cvo.setModifiedOn(formattedDate); 	
-             comments.getItems().add(cvo);
-        }
-        
-        if (writeEnabled) {
-        	comments.nodePermissions.create = true;
-        	comments.nodePermissions.delete = true;
-        	comments.nodePermissions.edit = true;
-        }
-        
-        return Response.ok(comments).build();
-    }
+		SimpleDateFormat sdf = new SimpleDateFormat("MMM dd yyyy HH:mm:ss Z", Locale.ENGLISH);
 
-    private boolean isWriteEnabled(User user, long folderId) {
+		CommentListVO comments = new CommentListVO();
+		for (DocumentNote note : notes) {
+			CommentVO cvo = new CommentVO();
+			cvo.setName(String.valueOf(note.getId()));
+			cvo.setContent(note.getMessage());
+			cvo.setAuthor(note.getUsername());
+
+			String formattedDate = sdf.format(note.getLastModified());
+
+			cvo.setModifiedOn(formattedDate);
+			comments.getItems().add(cvo);
+		}
+
+		if (writeEnabled) {
+			comments.nodePermissions.create = true;
+			comments.nodePermissions.delete = true;
+			comments.nodePermissions.edit = true;
+		}
+
+		return Response.ok(comments).build();
+	}
+
+	private boolean isWriteEnabled(User user, long folderId) {
 		try {
 			FolderDAO dao = (FolderDAO) Context.getInstance().getBean(FolderDAO.class);
 			if (dao.isPermissionEnabled(Permission.READ, folderId, user.getId())) {
 				return true;
 			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}		
+			log.error(e.getMessage(), e);
+		}
 		return false;
 	}
 
 	@POST
-    @Path("/addcommentform/{sid}/{docid}")
-    @Produces({"application/xml", "application/json"})
-    @Consumes({"application/x-www-form-urlencoded"})
-    public Response addCommentForm(@PathParam("sid") String sid, @PathParam("docid") String docid, @FormParam("content") String content) throws Exception {
-    	
-        CommentVO comment = new CommentVO();
-        comment.setContent(content);
-       		
+	@Path("/addcommentform/{sid}/{docid}")
+	@Produces({ "application/xml", "application/json" })
+	@Consumes({ "application/x-www-form-urlencoded" })
+	public Response addCommentForm(@PathParam("sid") String sid, @PathParam("docid") String docid,
+			@FormParam("content") String content) throws Exception {
+
+		CommentVO comment = new CommentVO();
+		comment.setContent(content);
+
 		User user = validateSession(sid);
-        
+
 		DocumentNote note = new DocumentNote();
 		note.setDocId(Long.parseLong(docid));
 		note.setUserId(user.getId());
@@ -109,19 +111,20 @@ protected final Logger log = LoggerFactory.getLogger(CommentService.class);
 		note.setMessage(content);
 
 		DocumentNoteDAO dndao = (DocumentNoteDAO) Context.getInstance().getBean(DocumentNoteDAO.class);
-		dndao.store(note);		
+		dndao.store(note);
 
-        return Response.ok(comment).build();
-    }
-	
+		return Response.ok(comment).build();
+	}
+
 	@POST
-    @Path("/addcomment/{sid}/{docid}")
-    @Produces({"application/xml", "application/json"})
-    @Consumes({"application/xml", "application/json"})
-    public Response addComment(@PathParam("sid") String sid, @PathParam("docid") String docid, CommentVO comment) throws Exception {
-       	
+	@Path("/addcomment/{sid}/{docid}")
+	@Produces({ "application/xml", "application/json" })
+	@Consumes({ "application/xml", "application/json" })
+	public Response addComment(@PathParam("sid") String sid, @PathParam("docid") String docid, CommentVO comment)
+			throws Exception {
+
 		User user = validateSession(sid);
-        
+
 		DocumentNote note = new DocumentNote();
 		note.setDocId(Long.parseLong(docid));
 		note.setUserId(user.getId());
@@ -130,8 +133,8 @@ protected final Logger log = LoggerFactory.getLogger(CommentService.class);
 		note.setMessage(comment.getContent());
 
 		DocumentNoteDAO dndao = (DocumentNoteDAO) Context.getInstance().getBean(DocumentNoteDAO.class);
-		dndao.store(note);		
+		dndao.store(note);
 
-        return Response.ok(comment).build();
-    }	
+		return Response.ok(comment).build();
+	}
 }
