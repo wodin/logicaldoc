e6d7deb6280cabb8e6a933f3028b5162043ab30d
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2014-07-01 09:35:31 +0000

    Updated POMs to release 7.0.1

--- community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/beans/GUIInfo.java
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/beans/GUIInfo.java
@@ -19,7 +19,7 @@ public class GUIInfo implements Serializable {
 
 	private String product = "LogicalDOC";
 
-	private String release = "7.0";
+	private String release = "7.0.1";
 
 	private String year = "2006-2014";
 
