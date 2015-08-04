b6fea1a3c5ef8f2f457edbee7633901841422436
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2015-08-04 15:50:12 +0000

    tst commit

---
+++ community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/sequence/HibernateSequenceDAO.java
@@ -0,0 +1,95 @@
+package com.logicaldoc.core.sequence;
+
+import java.util.Date;
+import java.util.List;
+
+import org.slf4j.LoggerFactory;
+
+import com.logicaldoc.core.HibernatePersistentObjectDAO;
+import com.logicaldoc.util.sql.SqlUtil;
+
+/**
+ * Hibernate implementation of <code>SequenceDAO</code>. </p> Sequences are
+ * implemented ad Generics whose type is 'sequence' and subtype is the sequence
+ * name.
+ * 
+ * @author Marco Meschieri - Logical Objects
+ * @since 4.0
+ */
+@SuppressWarnings("unchecked")
+public class HibernateSequenceDAO extends HibernatePersistentObjectDAO<Sequence> implements SequenceDAO {
+
+	private HibernateSequenceDAO() {
+		super(Sequence.class);
+		super.log = LoggerFactory.getLogger(HibernateSequenceDAO.class);
+	}
+
+	@Override
+	public synchronized void reset(String sequence, long objectId, long tenantId, long value) {
+		synchronized (SequenceDAO.class) {
+			flush();
+			Sequence seq = findByAlternateKey(sequence, objectId, tenantId);
+			if (seq == null) {
+				seq = new Sequence();
+				seq.setName(sequence);
+				seq.setObjectId(objectId);
+				seq.setTenantId(tenantId);
+			}
+			seq.setLastReset(new Date());
+			seq.setValue(value);
+			store(seq);
+		}
+	}
+
+	@Override
+	public synchronized long next(String sequence, long objectId, long tenantId, long increment) {
+		synchronized (SequenceDAO.class) {
+			flush();
+			Sequence seq = findByAlternateKey(sequence, objectId, tenantId);
+			if (seq == null) {
+				seq = new Sequence();
+				seq.setName(sequence);
+				seq.setObjectId(objectId);
+				seq.setTenantId(tenantId);
+			} 
+			seq.setValue(seq.getValue() + increment);
+			store(seq);
+			flush();
+			return seq.getValue();
+		}
+	}
+
+	@Override
+	public synchronized long next(String sequence, long objectId, long tenantId) {
+		return this.next(sequence, objectId, tenantId, 1L);
+	}
+
+	@Override
+	public long getCurrentValue(String sequence, long objectId, long tenantId) {
+		flush();
+		Sequence seq = findByAlternateKey(sequence, objectId, tenantId);
+		if (seq == null)
+			return 0L;
+		else
+			return seq.getValue();
+	}
+
+	@Override
+	public List<Sequence> findByName(String name, long tenantId) {
+		String query = " _entity.tenantId=" + tenantId;
+		query += " and _entity.name like '" + SqlUtil.doubleQuotes(name) + "%' ";
+		return findByWhere(query, null, null);
+	}
+
+	@Override
+	public Sequence findByAlternateKey(String name, long objectId, long tenantId) {
+		String query = " _entity.tenantId=" + tenantId;
+		query += " and _entity.name = '" + SqlUtil.doubleQuotes(name) + "' ";
+		query += " and _entity.objectId = " + objectId;
+		List<Sequence> sequences = findByWhere(query, null, null);
+		if (sequences.isEmpty())
+			return null;
+		else
+			return sequences.get(0);
+	}
+}
