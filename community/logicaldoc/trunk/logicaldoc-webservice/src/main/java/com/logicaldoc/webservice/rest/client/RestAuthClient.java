ccbb1890db7cdbfdc912a2a038a1ba34c6fa6ae0
shatzing5 <shatzing5@bae09422-6297-422f-b3ee-419521344c47>
2016-04-20 09:00:42 +0000

    small improvement on resource usage

--- community/logicaldoc/trunk/logicaldoc-webservice/src/main/java/com/logicaldoc/webservice/rest/client/RestAuthClient.java
+++ community/logicaldoc/trunk/logicaldoc-webservice/src/main/java/com/logicaldoc/webservice/rest/client/RestAuthClient.java
@@ -16,33 +16,29 @@ import com.logicaldoc.webservice.rest.AuthService;
 public class RestAuthClient extends AbstractRestClient implements AuthService {
 	
 	protected static Logger log = LoggerFactory.getLogger(RestAuthClient.class);
+	
+	AuthService proxy = null;
 
 	public RestAuthClient(String endpoint) {
 		super(endpoint);
+		proxy = JAXRSClientFactory.create(endpoint, AuthService.class);
 	}
 	
 	public String login(String username, String password) throws Exception {
-		
-        System.out.println("Sending request to the service");
-        System.out.println("endpoint: " +endpoint);
-        AuthService proxy = JAXRSClientFactory.create(endpoint, AuthService.class);
 		return proxy.login(username, password);   
 	}	
 
 	public void logout(String sid) {
-        AuthService proxy = JAXRSClientFactory.create(endpoint, AuthService.class);
 		proxy.logout(sid);
 	}
 
 	@Override
 	public String loginPost(String username, String password) throws Exception {
-        AuthService proxy = JAXRSClientFactory.create(endpoint, AuthService.class);
 		return proxy.loginPost(username, password);
 	}
 
 	@Override
 	public String loginPostJSON(String jsonstr) throws Exception {
-        AuthService proxy = JAXRSClientFactory.create(endpoint, AuthService.class);
 		return proxy.loginPostJSON(jsonstr);
 	}
 
