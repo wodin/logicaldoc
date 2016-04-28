49612fa2ac4ec70449914a32e2c978b6e9333d58
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2016-04-28 15:05:38 +0000

    Renamed UserSession to Session

--- community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/security/SessionManager.java
+++ community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/security/SessionManager.java
@@ -1,15 +1,26 @@
 package com.logicaldoc.core.security;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import com.logicaldoc.core.security.spring.LDAuthenticationToken;
+import com.logicaldoc.core.security.spring.LDSecurityContextRepository;
 import com.logicaldoc.util.config.ContextProperties;
 import com.logicaldoc.util.time.TimeDiff;
 import com.logicaldoc.util.time.TimeDiff.TimeField;
@@ -21,6 +32,10 @@ import com.logicaldoc.util.time.TimeDiff.TimeField;
  * @since 4.6
  */
 public class SessionManager extends ConcurrentHashMap<String, Session> {
+
+	public static final String COOKIE_SID = "ldoc-sid";
+
+	public static final String PARAM_SID = "sid";
 
 	private static Logger log = LoggerFactory.getLogger(SessionManager.class);
 
@@ -213,5 +228,128 @@ public class SessionManager extends ConcurrentHashMap<String, Session> {
 		for (String sessionId : garbage) {
 			remove(sessionId);
 		}
+	}
+
+	/**
+	 * Gets the Session returned by <code>getSid(request)</code>
+	 */
+	public Session getSession(HttpServletRequest request) {
+		String sid = getSid(request);
+		if (sid == null)
+			return null;
+		if (isValid(sid))
+			return get(sid);
+		return null;
+	}
+
+	/**
+	 * Gets the Session ID specification from the current request following this
+	 * lookup strategy:
+	 * <ol>
+	 * <li>Session attribute <code>PARAM_SID</code></li>
+	 * <li>Request attribute <code>PARAM_SID</code></li>
+	 * <li>Request parameter <code>PARAM_SID</code></li>
+	 * <li>Cookie <code>COOKIE_SID</code></li>
+	 * <li>Spring SecurityContextHolder</li>
+	 * 
+	 * @param request The current request to inspect
+	 * @return The SID if any
+	 */
+	public String getSid(HttpServletRequest request) {
+		if (request.getSession(false) != null && request.getSession(false).getAttribute(PARAM_SID) != null)
+			return (String) request.getSession(false).getAttribute(PARAM_SID);
+		if (request.getAttribute(PARAM_SID) != null)
+			return (String) request.getAttribute(PARAM_SID);
+		if (request.getParameter(PARAM_SID) != null)
+			return (String) request.getParameter(PARAM_SID);
+
+		Cookie cookies[] = request.getCookies();
+		if (cookies != null)
+			for (Cookie cookie : cookies) {
+				if (COOKIE_SID.equals(cookie.getName()))
+					return cookie.getValue();
+			}
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null && auth instanceof LDAuthenticationToken)
+			return ((LDAuthenticationToken) auth).getSid();
+
+		String combinedUserId = getCombinedUserId(request);
+		if (combinedUserId != null)
+			for (Session session : SessionManager.get().getSessions()) {
+				try {
+					String[] userObject = (String[]) session.getUserObject();
+					if (userObject.length > 2 && userObject[2].equals(combinedUserId))
+						if (isValid(session.getId())) {
+							return session.getId();
+						}
+				} catch (Throwable t) {
+
+				}
+			}
+
+		return null;
+	}
+
+	/**
+	 * Saves the session identifier in the request and session attribute
+	 * <code>PARAM_SID</code> and Cookie <code>COOKIE_SID</code>
+	 * 
+	 * @param request
+	 * @param sid
+	 */
+	public void saveSid(HttpServletRequest request, HttpServletResponse response, String sid) {
+		request.setAttribute(PARAM_SID, sid);
+		if (request.getSession(false) != null)
+			request.getSession(false).setAttribute(PARAM_SID, sid);
+
+		Cookie sidCookie = new Cookie(COOKIE_SID, sid);
+		response.addCookie(sidCookie);
+	}
+
+	/**
+	 * Retrieves the session ID of the current thread execution
+	 */
+	public static String getCurrentSid() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth instanceof LDAuthenticationToken)
+			return ((LDAuthenticationToken) auth).getSid();
+		else
+			return null;
+	}
+
+	public HttpSession getServletSession(String sid) {
+		if (sid == null)
+			return null;
+		return LDSecurityContextRepository.getServletSession(sid);
+	}
+
+	/**
+	 * Create a pseudo session identifier, useful to handle session bindings in
+	 * basic authentication.
+	 * 
+	 * @param req The request to process
+	 * @return The combined user Id.
+	 */
+	public String getCombinedUserId(HttpServletRequest req) {
+		String[] credentials = getBasicCredentials(req);
+		if (credentials != null)
+			return String.format("%s-%s-%s", credentials[0], credentials[1] == null ? "0" : credentials[1].hashCode(),
+					req.getRemoteAddr());
+		else
+			return null;
+	}
+
+	private static String[] getBasicCredentials(HttpServletRequest req) {
+		final String authorization = req.getHeader("Authorization");
+		if (authorization != null && authorization.startsWith("Basic")) {
+			// Authorization: Basic base64credentials
+			String base64Credentials = authorization.substring("Basic".length()).trim();
+			String credentials = new String(Base64.getDecoder().decode(base64Credentials), Charset.forName("UTF-8"));
+
+			// credentials = username:password
+			return credentials.split(":", 2);
+		} else
+			return null;
 	}
 }
---
+++ community/logicaldoc/trunk/logicaldoc-core/src/main/java/com/logicaldoc/core/security/spring/LDSecurityContextRepository.java
@@ -0,0 +1,72 @@
+package com.logicaldoc.core.security.spring;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.web.context.HttpRequestResponseHolder;
+import org.springframework.security.web.context.SecurityContextRepository;
+
+import com.logicaldoc.core.security.Session;
+import com.logicaldoc.core.security.SessionManager;
+
+/**
+ * This repository avoid the use of sessions and simply use the current request
+ * to store and retrieve the session ID.
+ * 
+ * @author Marco Meschieri - LogicalDOC
+ * @since 7.5
+ */
+public class LDSecurityContextRepository implements SecurityContextRepository {
+
+	private static Map<String, HttpSession> servletSessionMapping = new HashMap<String, HttpSession>();
+
+	@Override
+	public boolean containsContext(HttpServletRequest request) {
+		String sid = SessionManager.get().getSid(request);
+		return sid != null;
+	}
+
+	@Override
+	public SecurityContext loadContext(HttpRequestResponseHolder request) {
+		String sid = SessionManager.get().getSid(request.getRequest());
+		if (sid == null || !SessionManager.get().isValid(sid))
+			sid = null;
+
+		if (sid == null)
+			return SecurityContextHolder.createEmptyContext();
+
+		Session session = SessionManager.get().get(sid);
+
+		LDAuthenticationToken token = new LDAuthenticationToken(session.getUserName(), "", null);
+		token.setSid(sid);
+
+		SecurityContextImpl context = new SecurityContextImpl();
+		context.setAuthentication(token);
+
+		return context;
+	}
+
+	@Override
+	public void saveContext(SecurityContext context, HttpServletRequest request, HttpServletResponse response) {
+		if (context.getAuthentication() == null)
+			return;
+
+		Object principal = context.getAuthentication().getPrincipal();
+
+		if (principal != null && principal instanceof LDAuthenticationToken) {
+			LDAuthenticationToken token = (LDAuthenticationToken) principal;
+			SessionManager.get().saveSid(request, response, token.getSid());
+		}
+	}
+
+	public static HttpSession getServletSession(String sid) {
+		return servletSessionMapping.get(sid);
+	}
+}
