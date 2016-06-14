b584265025f6a2516a6ed457f9c10b4827d4bea4
shatzing5 <shatzing5@bae09422-6297-422f-b3ee-419521344c47>
2016-06-14 10:00:07 +0000

    Created branche logicaldoc-7.5

---
+++ community/logicaldoc/branches/logicaldoc-7.5R02/logicaldoc-core/src/main/java/com/logicaldoc/core/security/SecurityManager.java
@@ -0,0 +1,134 @@
+package com.logicaldoc.core.security;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * Manager for security objects like users and groups
+ * 
+ * @author Marco Meschieri - Logical Objects
+ * @since 3.0
+ */
+public interface SecurityManager {
+
+	/**
+	 * Assign a collection of users to a group.
+	 * 
+	 * @param users The users collection
+	 * @param group The group
+	 */
+	public void assignUsersToGroup(Collection<User> users, Group group);
+
+	/**
+	 * Assign a user to a collection of groups.
+	 * 
+	 * @param user The user to be assigned
+	 * @param groups The groups collection
+	 */
+	public void assignUserToGroups(User user, Collection<Group> groups);
+
+	/**
+	 * Assign a user to a collection of groups.
+	 * 
+	 * @param user The user to be assigned
+	 * @param groupIds Array of group ids
+	 */
+	public void assignUserToGroups(User user, long[] groupIds);
+
+	/**
+	 * Assign a user to a group.
+	 * 
+	 * @param user The user to be assigned
+	 * @param group The group
+	 */
+	public void assignUserToGroup(User user, Group group);
+
+	/**
+	 * Removes the specified users from a group. Users will not removed from
+	 * their user's groups.
+	 * 
+	 * @param users The users to be removed
+	 * @param group The group to be updated
+	 */
+	public void removeUsersFromGroup(Collection<User> users, Group group);
+
+	/**
+	 * Removes the specified user from a group. If the specified group is the
+	 * user's group no changes are committed.
+	 * 
+	 * @param users The user to be removed
+	 * @param group The group to be updated
+	 */
+	public void removeUserFromGroup(User user, Group group);
+
+	/**
+	 * Removes the specified user from a all groups except it's user's group
+	 * 
+	 * @param user The user to be removed
+	 */
+	public void removeUserFromAllGroups(User user);
+
+	/**
+	 * Removes all users from group
+	 * 
+	 * @param group The group from which all users must be removed
+	 */
+	public void removeAllUsersFromGroup(Group group);
+
+	/**
+	 * Retrieves the collection of groups that can access the given menu
+	 * 
+	 * @param menuId The menu to consider
+	 * @return The collection of allowed groups
+	 */
+	public Set<Group> getAllowedGroups(long menuId);
+
+	/**
+	 * Checks if a given user is member of a particular group
+	 * 
+	 * @param userId The user identifier
+	 * @param groupId The group identifier
+	 * @return true only if the user belongs to the group
+	 */
+	public boolean isMemberOf(long userId, long groupId);
+
+	/**
+	 * Checks if a given user is member of a particular group
+	 * 
+	 * @param userId The user identifier
+	 * @param groupName The group name
+	 * @return true only if the user belongs to the group
+	 */
+	public boolean isMemberOf(long userId, String groupName);
+
+	/**
+	 * This method is looking up for writing rights for a folder and an user.
+	 * 
+	 * @param docId ID of the document
+	 * @param userId ID of the user
+	 */
+	public boolean isWriteEnabled(long docId, long userId);
+
+	public boolean isReadEnabled(long docId, long userId);
+
+	public boolean isDownloadEnabled(long docId, long userId);
+
+	/**
+	 * This method checks if the given permission is enabled for a document and
+	 * an user.
+	 * 
+	 * @param permission the permission to check
+	 * @param docId ID of the document
+	 * @param userId ID of the user
+	 */
+	public boolean isPermissionEnabled(Permission permission, long docId, long userId);
+
+	/**
+	 * Finds all permissions of a user enabled on the specified document
+	 * 
+	 * @param docId ID of the document
+	 * @param userId ID of the user
+	 * @return Collection of enabled permissions
+	 */
+	public Set<Permission> getEnabledPermissions(long docId, long userId);
+}
---
+++ community/logicaldoc/branches/logicaldoc-7.5R02/logicaldoc-webapp/src/main/java/com/logicaldoc/web/service/SearchEngineServiceImpl.java
@@ -0,0 +1,194 @@
+package com.logicaldoc.web.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.logicaldoc.core.document.dao.DocumentDAO;
+import com.logicaldoc.core.i18n.LanguageManager;
+import com.logicaldoc.core.parser.ParserFactory;
+import com.logicaldoc.core.searchengine.SearchEngine;
+import com.logicaldoc.core.security.Session;
+import com.logicaldoc.core.security.Tenant;
+import com.logicaldoc.gui.common.client.ServerException;
+import com.logicaldoc.gui.common.client.beans.GUISearchEngine;
+import com.logicaldoc.gui.frontend.client.services.SearchEngineService;
+import com.logicaldoc.util.Context;
+import com.logicaldoc.util.config.ContextProperties;
+import com.logicaldoc.web.util.ServiceUtil;
+
+/**
+ * Implementation of the SearchEngineService
+ * 
+ * @author Matteo Caruso - Logical Objects
+ * @since 6.0
+ */
+public class SearchEngineServiceImpl extends RemoteServiceServlet implements SearchEngineService {
+
+	private static final long serialVersionUID = 1L;
+
+	private static Logger log = LoggerFactory.getLogger(SearchEngineServiceImpl.class);
+
+	@Override
+	public GUISearchEngine getInfo() throws ServerException {
+		Session session = ServiceUtil.validateSession(getThreadLocalRequest());
+
+		try {
+			GUISearchEngine searchEngine = new GUISearchEngine();
+
+			SearchEngine indexer = (SearchEngine) Context.get().getBean(SearchEngine.class);
+			searchEngine.setLocked(indexer.isLocked());
+			searchEngine.setEntries(indexer.getCount());
+
+			ContextProperties conf = Context.get().getProperties();
+			searchEngine.setExcludePatters(conf.getProperty(session.getTenantName() + ".index.excludes"));
+			searchEngine.setIncludePatters(conf.getProperty(session.getTenantName() + ".index.includes"));
+			searchEngine.setDir(conf.getProperty("index.dir"));
+			searchEngine.setSubwords("true".equals(conf.getProperty("index.subwords")));
+
+			if (StringUtils.isNotEmpty(conf.getProperty("index.batch")))
+				searchEngine.setBatch(new Integer(conf.getProperty("index.batch")));
+			else
+				searchEngine.setBatch(0);
+
+			if (StringUtils.isNotEmpty(conf.getProperty("parser.timeout")))
+				searchEngine.setParsingTimeout(new Integer(conf.getProperty("parser.timeout")));
+			else
+				searchEngine.setParsingTimeout(0);
+
+			if (StringUtils.isNotEmpty(conf.getProperty("index.maxtext")))
+				searchEngine.setMaxText(new Integer(conf.getProperty("index.maxtext")));
+			else
+				searchEngine.setMaxText(0);
+
+			// Populate the list of supported languages
+			searchEngine.setLanguages("");
+			LanguageManager lm = LanguageManager.getInstance();
+			List<String> langs = lm.getLanguagesAsString(session.getTenantName());
+			for (String lang : langs) {
+				searchEngine.setLanguages(searchEngine.getLanguages() + "," + lang);
+			}
+			if (searchEngine.getLanguages().startsWith(","))
+				searchEngine.setLanguages(searchEngine.getLanguages().substring(1));
+
+			return searchEngine;
+		} catch (Exception t) {
+			return (GUISearchEngine) ServiceUtil.throwServerException(session, log, t);
+		}
+	}
+
+	@Override
+	public void rescheduleAll(final boolean dropIndex) throws ServerException {
+		final Session session = ServiceUtil.validateSession(getThreadLocalRequest());
+
+		if (dropIndex)
+			try {
+				SearchEngine indexer = (SearchEngine) Context.get().getBean(SearchEngine.class);
+				indexer.dropIndex();
+			} catch (Exception e) {
+				log.error(e.getMessage(), e);
+				throw new RuntimeException(e.getMessage(), e);
+			}
+
+		Runnable task = new Runnable() {
+			public void run() {
+				try {
+					DocumentDAO documentDao = (DocumentDAO) Context.get().getBean(DocumentDAO.class);
+					documentDao.bulkUpdate("set ld_indexed=0 where ld_indexed=1 "
+							+ (!dropIndex ? " and ld_tenantid=" + session.getTenantId() : ""), null);
+				} catch (Exception t) {
+					log.error(t.getMessage(), t);
+					throw new RuntimeException(t.getMessage(), t);
+				}
+			}
+		};
+
+		Thread recreateThread = new Thread(task);
+		recreateThread.start();
+
+		return;
+	}
+
+	@Override
+	public void unlocks() throws ServerException {
+		Session session = ServiceUtil.validateSession(getThreadLocalRequest());
+
+		try {
+			SearchEngine indexer = (SearchEngine) Context.get().getBean(SearchEngine.class);
+			indexer.unlock();
+		} catch (Exception t) {
+			ServiceUtil.throwServerException(session, log, t);
+		}
+	}
+
+	@Override
+	public String check() throws ServerException {
+		Session session = ServiceUtil.validateSession(getThreadLocalRequest());
+
+		try {
+			SearchEngine indexer = (SearchEngine) Context.get().getBean(SearchEngine.class);
+			return indexer.check();
+		} catch (Exception t) {
+			return (String) ServiceUtil.throwServerException(session, log, t);
+		}
+	}
+
+	@Override
+	public void save(GUISearchEngine searchEngine) throws ServerException {
+		Session session = ServiceUtil.validateSession(getThreadLocalRequest());
+
+		try {
+			ContextProperties conf = Context.get().getProperties();
+			conf.setProperty(session.getTenantName() + ".index.excludes",
+					searchEngine.getExcludePatters() != null ? searchEngine.getExcludePatters() : "");
+			conf.setProperty(session.getTenantName() + ".index.includes",
+					searchEngine.getIncludePatters() != null ? searchEngine.getIncludePatters() : "");
+
+			if (session.getTenantId() == Tenant.DEFAULT_ID) {
+				conf.setProperty("index.batch", Integer.toString(searchEngine.getBatch()));
+				conf.setProperty("index.maxtext", Integer.toString(searchEngine.getMaxText()));
+				conf.setProperty("parser.timeout", Integer.toString(searchEngine.getParsingTimeout()));
+				conf.setProperty("index.dir", searchEngine.getDir());
+				conf.setProperty("index.subwords", searchEngine.isSubwords() ? "true" : "false");
+			}
+
+			conf.write();
+		} catch (Exception t) {
+			ServiceUtil.throwServerException(session, log, t);
+		}
+	}
+
+	@Override
+	public void setLanguageStatus(String language, boolean active) throws ServerException {
+		Session session = ServiceUtil.validateSession(getThreadLocalRequest());
+
+		try {
+			ContextProperties conf = Context.get().getProperties();
+			conf.setProperty(session.getTenantName() + ".lang." + language, active ? "enabled" : "disabled");
+			conf.write();
+		} catch (Throwable t) {
+			ServiceUtil.throwServerException(session, log, t);
+		}
+	}
+
+	@Override
+	public void setAliases(String extension, String aliases) throws ServerException {
+		Session session = ServiceUtil.validateSession(getThreadLocalRequest());
+
+		try {
+			StringTokenizer st = new StringTokenizer(aliases, ",", false);
+			List<String> buf = new ArrayList<String>();
+			while (st.hasMoreElements())
+				buf.add(((String) st.nextElement()).trim());
+
+			ParserFactory.setAliases(extension, buf.toArray(new String[0]));
+		} catch (Throwable t) {
+			ServiceUtil.throwServerException(session, log, t);
+		}
+	}
+}
