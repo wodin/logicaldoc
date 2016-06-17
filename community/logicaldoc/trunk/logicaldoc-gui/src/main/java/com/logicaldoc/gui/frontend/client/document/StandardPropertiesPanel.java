7b57dbddd8154cbfe8dde0fad3b98865b9906d79
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2016-06-17 07:11:48 +0000

    Fix #346 Execute scripts in workflow task object

--- community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/document/StandardPropertiesPanel.java
+++ community/logicaldoc/trunk/logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/document/StandardPropertiesPanel.java
@@ -163,13 +163,13 @@ public class StandardPropertiesPanel extends DocumentDetailTab {
 
 		LinkItem folder = ItemFactory.newLinkItem("folder", Util.padLeft(document.getPathExtended(), 40));
 		folder.setTitle(I18N.message("folder"));
-		folder.setValue(Util.contextPath() + "?" + Constants.FOLDER_ID + "=" + document.getFolder().getId());
+		folder.setValue(Util.contextPath() + "frontend.jsp?" + Constants.FOLDER_ID + "=" + document.getFolder().getId());
 		folder.setTooltip(document.getPathExtended());
 		folder.setWrap(false);
 		folder.setWidth(DEFAULT_ITEM_WIDTH);
 
 		String downloadUrl = Util.downloadURL(document.getId());
-		String detailsUrl = Util.contextPath() + "/frontend.jsp?docId=" + document.getId();
+		String detailsUrl = Util.contextPath() + "frontend.jsp?docId=" + document.getId();
 		String perma = "<a href='" + downloadUrl + "'>" + I18N.message("download") + "</a> | " + "<a href='"
 				+ detailsUrl + "'>" + I18N.message("details") + "</a>";
 
