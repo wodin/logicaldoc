f33de4f21c5f4992d9f7c61bcb544e82a71730c3
car031 <car031@bae09422-6297-422f-b3ee-419521344c47>
2016-12-27 09:45:48 +0000

    added patch utility in the databaes loader

---
+++ community/modules/logicaldoc-installer/trunk/src/dist/loader/patch.bat
@@ -0,0 +1,2 @@
+set ANT_HOME=%{INSTALL_PATH}\ant
+"%{INSTALL_PATH}\ant\bin\ant" -f "%{INSTALL_PATH}\loader\build.xml" patch
---
+++ community/modules/logicaldoc-installer/trunk/src/dist/loader/patch.sh
@@ -0,0 +1,2 @@
+export ANT_HOME=%{INSTALL_PATH}/ant
+$ANT_HOME/bin/ant -f "%{INSTALL_PATH}/build.xml" patch
